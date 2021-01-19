337de33b173b46cd801da00b26f65699b2ca2dd3
David Robert-Ansart <david.robertansart@gmail.com>
2021-01-19 22:49:35 +0100

    :construction_worker: Add webpack plugins to analyse build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /* eslint-disable */
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
+
 module.exports = {
   distDir: "./dist/next",
   poweredByHeader: false,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+    // config.plugins.push(
+    //   new BundleAnalyzerPlugin({
+    //     analyzerMode: 'server',
+    //     analyzerPort: isServer ? 8888 : 8889,
+    //     openAnalyzer: true,
+    //   })
+    // );
+    config.plugins.push(new DuplicatePackageCheckerPlugin());
     return config;
   },
 };
