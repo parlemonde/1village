17b7d077f0bfdd3752fe0cfa8c221cc9af25a5d9
David Robert-Ansart <david.robertansart@gmail.com>
2021-01-26 19:23:06 +0100

    :sparkles: Add new pages

---
+++ src/pages/se-presenter/mascotte/1.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Base } from "src/components/Base";
+
+const MascotteStep1: React.FC = () => {
+  return (
+    <Base>
+      <h1>Ma mascotte</h1>
+    </Base>
+  );
+};
+
+export default MascotteStep1;
---
+++ src/pages/se-presenter/presentation/1.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Base } from "src/components/Base";
+
+const PresentationStep1: React.FC = () => {
+  return (
+    <Base>
+      <h1>Présentation thématique</h1>
+    </Base>
+  );
+};
+
+export default PresentationStep1;
