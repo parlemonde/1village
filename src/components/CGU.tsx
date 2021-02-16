25f47fc205e3bf244a689d6dea1229a3f6ae990e
David Robert-Ansart <david.robertansart@gmail.com>
2021-02-16 17:59:13 +0100

    :sparkles: Add CGU

---
+++ src/components/CGU.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export const CGU: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'justify' }}>
+      <div className="text-center" style={{ marginBottom: '1rem' }}>
+        <h2 className="text--primary">Conditions générales d’utilisation 1Village</h2>
+      </div>
+      <h3 className="text--orange-background">1. Respect de certaines dispositions sur la plateforme 1Village</h3>
+      <p>
+        En tant que professeur des écoles, utilisateur de la plateforme 1v.parlemonde.org, je m’engage à ne mettre en ligne aucun contenu qui
+        contreviendrait à la législation applicable, notamment tout message à caractère raciste, injurieux, diffamant, ou pornographique, quel que
+        soit le support utilisé (texte, photographie, vidéo, …).
+      </p>
+      <p>
+        <strong>Je m’engage notamment à respecter la législation relative au droit à l’image</strong>, en particulier, je m’engage à ne mettre en
+        ligne que des photos ou vidéos d’élèves pour lesquels je possède une autorisation valide et signée d’un parent ou du tuteur légal.
+      </p>
+      <p>
+        Je comprends et j’accepte que l’association Par Le Monde ne peut être considérée comme responsable des contenus illégaux que je mettrais en
+        ligne sur la plateforme 1v.parlemonde.org.
+      </p>
+
+      <br />
+      <h3 className="text--orange-background">2. Acceptation des mentions légales du site https://www.parlemonde.org</h3>
+      <p>
+        Je prends connaissance et j’accepte les mentions légales telles que précisées à l’adresse suivante :
+        <a href="https://www.parlemonde.org/mentions-legales/" target="_blank" rel="noreferrer">
+          https://www.parlemonde.org/mentions-legales/
+        </a>
+      </p>
+
+      <br />
+      <h3 className="text--orange-background">3. Adhésion à l’association Par Le Monde</h3>
+      <p>
+        Je prends connaissance des statuts et du règlement intérieur de l’association Par Le Monde et consens, conformément aux dispositions
+        statutaires (articles 5 et 11 des statuts, articles 1 et 2 du règlement intérieur), à devenir membre de droit de l’association pour l’année en
+        cours.
+      </p>
+      <p>Ce statut me donne un droit de vote aux assemblées générales de l’association Par Le Monde.</p>
+      <p>
+        Pour rappel : sont membres de droit les professeurs ou écoles participant à un programme porté par l’association tel que défini dans le
+        règlement intérieur, et ayant donné leur accord à devenir membres de droit. Tous les membres de droit ont le pouvoir de voter à l’assemblée
+        générale. Aucune cotisation n’est demandée à ce jour pour les professeurs participant à 1Village pour l’année 2020-2021, le projet étant dans
+        sa phase pilote.
+      </p>
+    </div>
+  );
+};
--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -16,6 +16,7 @@ import PelicoSearch from 'src/svg/pelico/pelico-search.svg';
 import { ActivityType } from 'types/activity.type';
 import { User, UserType } from 'types/user.type';
 
+import { CGU } from './CGU';
 import { Flag } from './Flag';
 import { ActivityCard } from './activities/ActivityCard';
 
@@ -132,24 +133,24 @@ export const WelcomeModal: React.FC = () => {
           <MobileStepper
             style={{ backgroundColor: 'unset' }}
             variant="dots"
-            steps={3}
+            steps={4}
             activeStep={currentStep}
             nextButton={
               <Button
                 size="small"
-                color={currentStep === 2 ? 'primary' : 'default'}
-                variant={currentStep === 2 ? 'contained' : 'text'}
-                disabled={currentStep === 2 && (!newUser.city || !newUser.address || !newUser.postalCode)}
+                color={currentStep >= 2 ? 'primary' : 'default'}
+                variant={currentStep >= 2 ? 'contained' : 'text'}
+                disabled={currentStep === 3 && (!newUser.city || !newUser.address || !newUser.postalCode)}
                 onClick={() => {
-                  if (currentStep !== 2) {
+                  if (currentStep !== 3) {
                     setCurrentStep(currentStep + 1);
                   } else {
                     updateUser();
                   }
                 }}
               >
-                {currentStep === 2 ? 'Terminer' : 'Suivant'}
-                {currentStep !== 2 && <KeyboardArrowRight />}
+                {currentStep === 2 ? 'Accepter' : currentStep === 3 ? 'Terminer' : 'Suivant'}
+                {currentStep < 2 && <KeyboardArrowRight />}
               </Button>
             }
             backButton={
@@ -196,6 +197,11 @@ export const WelcomeModal: React.FC = () => {
           </div>
         )}
         {currentStep === 2 && (
+          <div style={{ height: '20rem', overflow: 'scroll', maxWidth: '800px', margin: '0 auto' }}>
+            <CGU />
+          </div>
+        )}
+        {currentStep === 3 && (
           <>
             <div style={{ display: 'flex', alignItems: 'center', marginBottom: '2rem' }}>
               <PelicoSearch style={{ width: '4rem', height: 'auto', marginRight: '1rem' }} />
