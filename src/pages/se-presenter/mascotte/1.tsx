6ea42c3d7062add91b619686e6db99640ba2433b
olivier <olivier.sans@sciam.fr>
2021-02-18 15:44:19 +0100

    mascotte page 1

--- src/pages/se-presenter/mascotte/1.tsx
+++ src/pages/se-presenter/mascotte/1.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import React from 'react';
 
-import { Button, TextField } from '@material-ui/core';
+import { Button, TextField, Grid } from '@material-ui/core';
 
 import { Base } from 'src/components/Base';
 import { Steps } from 'src/components/Steps';
@@ -40,20 +40,23 @@ const MascotteStep1: React.FC = () => {
       <div style={{ width: '100%', padding: '0.5rem 1rem 1rem 1rem' }}>
         <BackButton href="/se-presenter" />
         <Steps steps={['Votre classe', 'Votre mascotte', 'Description de votre mascotte', 'Prévisualiser']} activeStep={0} />
-        <div style={{ margin: '0 auto 1rem auto', width: '100%', maxWidth: '900px' }}>
-          <h1>Qui est dans votre classe ?</h1>
-          <TextField variant="outlined" style={{ width: '100%' }} value={activity.data.totalStudent} onChange={dataChange('totalStudent')} />
-          <p className="text" style={{ fontSize: '1.1rem' }}>
-            Dans cette activité, nous vous proposons de faire une présentation générale aux Pélicopains sur le thème de votre choix.
-          </p>
-          <div style={{ width: '100%', textAlign: 'right', margin: '1rem 0' }}>
-            <Link href="/se-presenter/mascotte/2">
-              <Button component="a" href="/se-presenter/mascotte/2" variant="outlined" color="primary">
-                Étape suivante
-              </Button>
-            </Link>
-          </div>
+        <div style={{lineHeight:"70px"}}>
+
+            <h1>Qui est dans votre classe ?</h1>
+            <TextField variant="outlined" style={{ width: '100%' }} value={activity.data.presentation} disabled />
+            <span>Nous sommes </span> <TextField style={{ display:"inline"}} type="number" /> <span> élèves, dont </span> <TextField style={{display:"inline"}} type="number"/> <span> filles et </span> <TextField style={{display:"inline"}} type="number"/> <span> garçons.</span><br/>
+            <span>En moyenne, l’âge des élèves de notre classe est </span> <TextField style={{display:"inline"}} type="number"/> <span> ans.</span><br/>
+            <span>Nous avons </span> <TextField style={{display:"inline"}} type="number"/> <span> professeurs, dont </span> <TextField style={{display:"inline"}} type="number"/> <span> femmes et </span> <TextField style={{display:"inline"}} type="number"/> <span> hommes.</span> <br/>
+            <span>Dans notre école, il y a </span> <TextField style={{display:"inline"}} type="number"/> <span> classes et </span> <TextField style={{display:"inline"}} type="number"/> <span> élèves.</span>
+            <div style={{ width: '100%', textAlign: 'right', margin: '1rem 0' }}>
+                <Link href="/se-presenter/mascotte/2">
+                <Button component="a" href="/se-presenter/mascotte/2" variant="outlined" color="primary">
+                  Étape suivante
+                </Button>
+              </Link>
+            </div>
         </div>
+       
       </div>
     </Base>
   );
