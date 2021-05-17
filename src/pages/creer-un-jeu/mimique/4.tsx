9347af4a3cbc73be95e2e191ea00b4de0c292046
--global <--global>
2021-05-17 20:52:17 +0200

    create step4

--- src/pages/creer-un-jeu/mimique/4.tsx
+++ src/pages/creer-un-jeu/mimique/4.tsx
@@ -1,118 +1,65 @@
 import { useRouter } from 'next/router';
-import { useQueryCache } from 'react-query';
 import React from 'react';
 
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { Button, Grid, Backdrop, Box } from '@material-ui/core';
+import { TextField, Grid, Box ,Button,Radio ,RadioGroup,FormControlLabel} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
 
-import { isPresentation } from 'src/activity-types/anyActivity';
-import { getMascotteContent, isMascotte } from 'src/activity-types/presentation.const';
-import { MascotteData } from 'src/activity-types/presentation.types';
-import { AvatarImg } from 'src/components/Avatar';
+import { isGame, isPresentation } from 'src/activity-types/anyActivity';
+import { DEFAULT_MIMIQUE_DATA, isMimique, GAME } from 'src/activity-types/game.const';
+import { MimiqueData, MimiquesData } from 'src/activity-types/game.types';
 import { Base } from 'src/components/Base';
 import { StepsButton } from 'src/components/StepsButtons';
 import { Steps } from 'src/components/Steps';
-import { EditButton } from 'src/components/buttons/EditButton';
+import { BackButton } from 'src/components/buttons/BackButton';
 import { ActivityContext } from 'src/contexts/activityContext';
 import { UserContext } from 'src/contexts/userContext';
-import { useCountries } from 'src/services/useCountries';
-import { useCurrencies } from 'src/services/useCurrencies';
-import { useLanguages } from 'src/services/useLanguages';
-import { ActivityStatus } from 'types/activity.type';
-import { User } from 'types/user.type';
+import { getUserDisplayName, pluralS } from 'src/utils';
+import { ActivityType, ActivityStatus } from 'types/activity.type';
+import { EditButton } from 'src/components/buttons/EditButton';
+import { VideoModals } from 'src/components/activities/content/editors/VideoEditor/VideoModals';
+import { AvatarImg } from 'src/components/Avatar';
+import UploadIcon from 'src/svg/jeu/mimique.svg';
 
+
+const GreenRadio = withStyles({
+  root: {
+    color: green[400],
+    '&$checked': {
+      color: green[600],
+    },
+  },
+  checked: {},
+})((props: RadioProps) => <Radio color="default" {...props} />);
 const MimiqueStep4: React.FC = () => {
   const router = useRouter();
-  const queryCache = useQueryCache();
-  const { user, setUser, axiosLoggedRequest } = React.useContext(UserContext);
-  const { activity, save, updateActivity } = React.useContext(ActivityContext);
-  const { countries } = useCountries();
-  const { languages } = useLanguages();
-  const { currencies } = useCurrencies();
-  const [isLoading, setIsLoading] = React.useState(false);
-
-  const isEdit = activity !== null && activity.id !== 0 && activity.status !== ActivityStatus.DRAFT;
-  const data = (activity?.data as MascotteData) || null;
-
-  React.useEffect(() => {
-    if (activity === null && !('activity-id' in router.query) && !sessionStorage.getItem('activity')) {
-      router.push('/se-presenter');
-    } else if (activity && (!isPresentation(activity) || !isMascotte(activity))) {
-      router.push('/se-presenter');
-    }
-  }, [activity, router, updateActivity]);
-
-  const content = React.useMemo(() => (data === null ? ['', '', ''] : getMascotteContent(data, countries, currencies, languages)), [
-    data,
-    countries,
-    languages,
-    currencies,
-  ]);
-
-  const prevContent = React.useRef<string[] | null>(null);
-  React.useEffect(() => {
-    if (!activity || prevContent.current === content) {
-      return;
-    }
-    prevContent.current = content;
-    const newContent = [...activity.processedContent];
-    // 1
-    if (newContent.length > 0 && content.length > 0) {
-      newContent[0].value = content[0];
-    } else if (content.length > 0) {
-      newContent.push({
-        type: 'text',
-        value: content[0],
-        id: 1,
-      });
-    }
-
-    // 2
-    if (newContent.length > 1 && content.length > 1) {
-      newContent[1].value = content[1];
-    } else if (content.length > 1) {
-      newContent.push({
-        type: 'text',
-        value: content[1],
-        id: 2,
-      });
-    }
-
-    // 3
-    if (newContent.length > 2 && content.length > 2) {
-      newContent[2].value = content[2];
-    } else if (content.length > 2) {
-      newContent.push({
-        type: 'text',
-        value: content[2],
-        id: 3,
-      });
-    }
-    updateActivity({ processedContent: newContent });
-  }, [activity, content, updateActivity]);
-
-  const onPublish = async () => {
-    setIsLoading(true);
-    const success = await save(true);
-    if (success) {
-      const newUser: Partial<User> = {};
-      if ((activity.data as MascotteData).mascotteImage) {
-        newUser.avatar = (activity.data as MascotteData).mascotteImage;
+  const [isError, setIsError] = React.useState<boolean>(false);
+  const { activity, updateActivity, createActivityIfNotExist, save } = React.useContext(ActivityContext);
+  const { user } = React.useContext(UserContext);
+  const labelPresentation = getUserDisplayName(user, false);
+  const created = React.useRef(false);
+  
+React.useEffect(() => {
+    if (!created.current) {
+      if (!activity) {
+        created.current = true;
+        createActivityIfNotExist(ActivityType.GAME, GAME.MIMIQUE, {
+          ...DEFAULT_MIMIQUE_DATA,
+          presentation: labelPresentation,
+        }).catch(console.error);
+      } else if (activity && (!isGame(activity) || !isMimique(activity))) {
+        created.current = true;
+        createActivityIfNotExist(ActivityType.GAME, GAME.MIMIQUE, {
+          ...DEFAULT_MIMIQUE_DATA,
+          presentation: labelPresentation,
+        }).catch(console.error);
       }
-      newUser.displayName = (activity.data as MascotteData).presentation;
-      const response = await axiosLoggedRequest({
-        method: 'PUT',
-        url: `/users/${user?.id}`,
-        data: newUser,
-      });
-      if (!response.error) {
-        setUser({ ...user, ...newUser });
-        queryCache.invalidateQueries('users');
-        queryCache.invalidateQueries('village-users');
-      }
-      router.push('/se-presenter/success');
     }
-    setIsLoading(false);
+  }, [activity, labelPresentation, createActivityIfNotExist, router]);
+  const data = (activity?.data as MimiquesData) || null;
+  const [value, setValue] = React.useState(data.mimique1.signification);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
   };
 
   if (!activity) {
@@ -126,98 +73,107 @@ const MimiqueStep4: React.FC = () => {
   return (
     <Base>
       <div style={{ width: '100%', padding: '0.5rem 1rem 1rem 1rem' }}>
-        <Steps
-          steps={['Votre classe', 'Votre mascotte : ' + data.mascotteName ?? 'mascotteName', 'Description de votre mascotte', 'Prévisualiser']}
-          activeStep={3}
-        />
+      
+        <Steps steps={['1ère mimique', '2ème mimique', '3ème mimique', 'Prévisualiser']} activeStep={3} />
         <div className="width-900">
-          <h1>Pré-visualisez votre mascotte{!isEdit && ' et publiez la'}</h1>
-          <p className="text" style={{ fontSize: '1.1rem' }}>
-            Voici la pré-visualisation de votre présentation.
-            {isEdit
-              ? " Vous pouvez la modifier à l'étape précédente, et enregistrer vos changements ici."
-              : ' Vous pouvez la modifier, et quand vous êtes prêts : publiez-la dans votre village-monde !'}
-          </p>
-          {isEdit ? (
-            <div style={{ width: '100%', textAlign: 'right', margin: '1rem 0' }}>
-              <Button variant="outlined" color="primary" onClick={onPublish}>
-                Enregistrer les changements
-              </Button>
-            </div>
-          ) : (
-            <div style={{ width: '100%', textAlign: 'right', margin: '1rem 0' }}>
-              <Button variant="outlined" color="primary" onClick={onPublish}>
-                Publier
-              </Button>
-            </div>
-          )}
-          <div className="preview-block">
-            <EditButton
-              onClick={() => {
-                router.push('/se-presenter/mascotte/1');
-              }}
-              isGreen
-              style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
-            />
-            <div>
-              {content.length > 0 &&
-                content[0].split('\n').map((s, index) => (
-                  <p key={index} style={{ margin: '0.5rem 0' }}>
-                    {s}
-                  </p>
-                ))}
-            </div>
+          <h1>Pré-visualisez votre mimiques et publiez les !</h1>
+          <h6 style={{ width: '100%', textAlign: 'left', margin: '1rem 0' }}>
+            Vous pouvez modifier chaque mimique si vous le souhaitez. Quand vous êtes prêts :
+          </h6>
+          <div style={{ width: '100%', textAlign: 'right', margin: '1rem 0' }}>
+                <Button variant="outlined" color="primary">
+                Publier … et jouer à votre tour !
+                </Button>
           </div>
+          {/* Mimique 1 */}
           <div className="preview-block">
-            <EditButton
-              onClick={() => {
-                router.push('/se-presenter/mascotte/2');
-              }}
-              isGreen
-              style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
-            />
             <Grid container spacing={3}>
-              <Grid item xs={12} md={3}>
-                <Box display="flex" justifyContent="center" m={0}>
-                  <AvatarImg src={data.mascotteImage} noLink />
-                </Box>
-              </Grid>
-              <Grid item xs={12} md={9}>
-                <div>
-                  {content.length > 1 &&
-                    content[1].split('\n').map((s, index) => (
-                      <p key={index} style={{ margin: '0.5rem 0' }}>
-                        {s}
-                      </p>
-                    ))}
-                </div>
-              </Grid>
+              <Grid item xs={12} md={4}>
+                  <Box display="flex" justifyContent="right" m={1}>
+                  <Button name="video" style={{ width: '100%', height:'80%',background: data.mimique1.video ? 'lightgrey' : 'lightgrey' }}>
+                  {<UploadIcon style={{ fill: 'currentcolor', width: '3rem', height: '3rem', margin: '30px' }} />}
+              </Button>
+                  </Box>
+                </Grid>
+              <Grid item xs={12} md={6}>
+                <RadioGroup aria-label="signification" name="signification1" value={value} onChange={handleChange}>
+                  <FormControlLabel  value={data.mimique1.signification} control={<GreenRadio />} label={data.mimique1.signification} />
+                  <FormControlLabel  control={<Radio />} label={data.mimique1.fakeSignification1} />
+                  <FormControlLabel  control={<Radio />} label={data.mimique1.fakeSignification2} />
+                </RadioGroup>
+                </Grid>
+                <Grid item xs={12} md={2}>
+                <EditButton
+                  onClick={() => {
+                    router.push('/creer-un-jeu/mimique/1');
+                  }}
+                  isGreen
+                  style={{ position: 'absolute', top: '40%', right: '0.5rem'}}
+                />
+                </Grid>
             </Grid>
+            <h6 style={{ width: '100%', textAlign: 'left', margin: '1rem 0' }}>{data.mimique1.origine} </h6>
           </div>
+          {/* Mimique 2 */}
           <div className="preview-block">
-            <EditButton
-              onClick={() => {
-                router.push('/se-presenter/mascotte/3');
-              }}
-              isGreen
-              style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
-            />
-            <div>
-              {content.length > 2 &&
-                content[2].split('\n').map((s, index) => (
-                  <p key={index} style={{ margin: '0.5rem 0' }}>
-                    {s}
-                  </p>
-                ))}
-            </div>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={4}>
+                  <Box display="flex" justifyContent="center" m={0}>
+                  <Button name="video" style={{ width: '100%', height:'80%',background: data.mimique2.video ? 'lightgrey' : 'lightgrey' }}>
+                  {<UploadIcon style={{ fill: 'currentcolor', width: '3rem', height: '3rem', margin: '30px' }} />}
+              </Button>
+                  </Box>
+                </Grid>
+              <Grid item xs={12} md={6}>
+                <RadioGroup aria-label="signification" name="signification1" value={value} onChange={handleChange}>
+                  <FormControlLabel  value={data.mimique2.signification} control={<GreenRadio />} label={data.mimique2.signification} />
+                  <FormControlLabel  control={<Radio />} label={data.mimique2.fakeSignification1} />
+                  <FormControlLabel  control={<Radio />} label={data.mimique2.fakeSignification2} />
+                </RadioGroup>
+                </Grid>
+                <Grid item xs={12} md={2}>
+                <EditButton
+                  onClick={() => {
+                    router.push('/creer-un-jeu/mimique/2');
+                  }}
+                  isGreen
+                  style={{ position: 'absolute', top: '40%', right: '0.5rem' }}
+                />
+                </Grid>
+            </Grid>
+            <h6 style={{ width: '100%', textAlign: 'left', margin: '1rem 0' }}>{data.mimique2.origine} </h6>
           </div>
-
-          <StepsButton prev="/se-presenter/mascotte/3" />
-        </div>
+          {/* Mimique 3 */}
+          <div className="preview-block">
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={4}>
+                  <Box display="flex" justifyContent="center" m={0}>
+                  <Button name="video" style={{ width: '100%', height:'80%',background: data.mimique3.video ? 'lightgrey' : 'lightgrey' }}>
+                  {<UploadIcon style={{ fill: 'currentcolor', width: '3rem', height: '3rem', margin: '30px' }} />}
+              </Button>
+                  </Box>
+                </Grid>
+              <Grid item xs={12} md={6}>
+                <RadioGroup aria-label="signification" name="signification1" value={value} onChange={handleChange}>
+                  <FormControlLabel  value={data.mimique3.signification} control={<GreenRadio />} label={data.mimique3.signification} />
+                  <FormControlLabel  control={<Radio />} label={data.mimique3.fakeSignification1} />
+                  <FormControlLabel  control={<Radio />} label={data.mimique3.fakeSignification2} />
+                </RadioGroup>
+                </Grid>
+                <Grid item xs={12} md={2}>
+                <EditButton
+                  onClick={() => {
+                    router.push('/creer-un-jeu/mimique/3');
+                  }}
+                  isGreen
+                  style={{ position: 'absolute', top: '40%', right: '0.5rem' }}
+                />
+                </Grid>
+            </Grid>
+            <h6 style={{ width: '100%', textAlign: 'left', margin: '1rem 0' }}>{data.mimique3.origine} </h6>
+          </div>
+         </div>
       </div>
-      <Backdrop style={{ zIndex: 2000, color: 'white' }} open={isLoading}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
     </Base>
   );
 };
