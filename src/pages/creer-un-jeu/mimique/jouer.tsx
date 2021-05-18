b5f02e6da53f3a734bbb4746619b7e4791d2dc3e
Loïc Hermann <loic.hermann@sciam.fr>
2021-05-18 11:58:23 +0200

    load a mimique in play page

--- src/pages/creer-un-jeu/mimique/jouer.tsx
+++ src/pages/creer-un-jeu/mimique/jouer.tsx
@@ -1,33 +1,56 @@
-import { FormControlLabel, Grid, Radio, RadioGroup } from '@material-ui/core';
+import { Button, FormControlLabel, Radio, RadioGroup } from '@material-ui/core';
 import React from 'react';
 import ReactPlayer from 'react-player';
-import { StepsButton } from 'src/components/StepsButtons';
-
+import { UserContext } from 'src/contexts/userContext';
 import { Base } from 'src/components/Base';
+import { Mimique } from 'types/mimique.type';
 
 const PlayMimique: React.FC = () => {
-  const validate = () => {
-    console.log('toto');
-  };
+  const [mimique, setMimique] = React.useState<Mimique>(null);
+  const { axiosLoggedRequest } = React.useContext(UserContext);
 
+  React.useEffect(() => {
+    axiosLoggedRequest({
+      method: 'GET',
+      url: `/mimiques/play`,
+    }).then((response) => {
+      if (!response.error) {
+        setMimique(response.data as Mimique);
+      }
+    });
+  }, [setMimique]);
+
+  const validate = () => {};
+
+  if (!mimique) {
+    return (
+      <Base>
+        <div></div>
+      </Base>
+    );
+  }
   return (
     <Base>
       <div style={{ width: '100%', padding: '0.5rem 1rem 1rem 1rem' }}>
         <h1>Que signifie cette mimique ?</h1>
         <p>Une mimique proposée par la classe de CE2 à Ain El-Rihani</p>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
-            <ReactPlayer width="100%" height="100%" light url="" controls />
-            <RadioGroup aria-label="gender" name="gender1" value="">
-              <FormControlLabel value="0" control={<Radio />} label="toto1" />
-              <FormControlLabel value="1" control={<Radio />} label="toto2" />
-              <FormControlLabel value="2" control={<Radio />} label="toto3" />
-            </RadioGroup>
-          </Grid>
-          <Grid item xs={12} md={6}></Grid>
-        </Grid>
+        <ReactPlayer width="100%" height="100%" light url="" controls />
+        <RadioGroup aria-label="gender" name="gender1" value="">
+          <FormControlLabel value="0" control={<Radio />} label={mimique.signification} />
+          <FormControlLabel value="1" control={<Radio />} label={mimique.fakeSignification1} />
+          <FormControlLabel value="2" control={<Radio />} label={mimique.fakeSignification2} />
+        </RadioGroup>
+        <Button
+          style={{
+            float: 'right',
+          }}
+          variant="outlined"
+          color="primary"
+          onClick={validate}
+        >
+          Valider
+        </Button>
       </div>
-      <StepsButton next={validate} />
     </Base>
   );
 };
