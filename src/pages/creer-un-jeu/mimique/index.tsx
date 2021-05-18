14c18bab24d9416ec1aeb56e2c9679dae4b5d217
root <root@DESKTOP-D4VLFF7.localdomain>
2021-05-18 10:00:59 +0200

    enhance layout

--- src/pages/creer-un-jeu/mimique/index.tsx
+++ src/pages/creer-un-jeu/mimique/index.tsx
@@ -10,16 +10,16 @@ const Mimique: React.FC = () => {
 
   return (
     <Base>
-      <div style={{ width: '100%', padding: '0.5rem 1rem 1rem 1rem' }}>
+      <div style={{ width: '100%', padding: '0.5rem 1rem 1rem 1rem', marginTop: '2rem' }}>
         <h1>Qu’est-ce qu’une mimique ?</h1>
-        <p>
+        <p style={{ marginBottom: '2rem' }}>
           Une mimique est un geste du corps qui exprime une émotion ou une pensée, et que l’on fait dans certaine situation. Par exemple, un signe des
           mains pour dire bonjour ! Ou bien, un mouvement du visage pour exprimer la colère. Pelico aime beaucoup les mimiques, parce qu’elles sont
           très parlantes… sans que l’on ait besoin de parler ! Certaines sont les mêmes partout sur terre, quand d’autres ne sont compréhensible que
           par les habitants d’un seul pays… ou même d’une seule région !
         </p>
         <h1>Faites découvrir vos mimiques aux Pélicopains !</h1>
-        <p>
+        <p style={{ marginBottom: '2rem' }}>
           À vous de faire découvrir 3 mimiques que vous utilisez à vos Pélicopains ! À chaque étape, vous pourrez mettre en ligne une courte vidéo
           illustrant la mimique, une explication de sa signification et de son origine. Pour pimenter le jeu, à chaque étape vous devrez également
           inventer deux significations fausses … à vos Pélicopains de deviner ce qui signifie réellement vos mimiques !
@@ -33,20 +33,17 @@ const Mimique: React.FC = () => {
             }}
             href="/creer-un-jeu/mimique/1"
             color="primary"
-            className="navigation__button full-width"
             variant="outlined"
             style={{
-              justifyContent: 'flex-start',
-              paddingRight: '0.1rem',
-              marginBottom: '0.4rem',
+              float: 'right',
             }}
             disableElevation
           >
             Faire découvrir 3 mimiques
           </Button>
         </Link>
-        <h1>Découvrez les mimiques de vos Pélicopains !</h1>
-        <p>
+        <h1 style={{ marginTop: '6rem' }}>Découvrez les mimiques de vos Pélicopains !</h1>
+        <p style={{ marginBottom: '3rem' }}>
           Une fois que vous aurez décrit 3 mimiques, ça sera à vous de deviner ce que veulent dire les mimiques de vos Pélicopains français et
           lilbanais ! Il y a actuellement X nouvelles mimiques à découvrir :
         </p>
@@ -59,12 +56,9 @@ const Mimique: React.FC = () => {
             }}
             href="/creer-un-jeu/mimique/jouer"
             color="primary"
-            className="navigation__button full-width"
             variant="outlined"
             style={{
-              justifyContent: 'flex-start',
-              paddingRight: '0.1rem',
-              marginBottom: '0.4rem',
+              float: 'right',
             }}
             disableElevation
           >
