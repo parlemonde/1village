83bbee3aac67179fe1922e44714c4f99955835fb
Sylvain Coutrot <sylvain.coutrot@hotmail.fr>
2021-09-20 23:31:13 +0200

    refactor: changing activity links to one component & change link to only 'cliquez ici'

--- src/pages/se-presenter/thematique/4.tsx
+++ src/pages/se-presenter/thematique/4.tsx
@@ -12,6 +12,7 @@ import { Base } from 'src/components/Base';
 import { StepsButton } from 'src/components/StepsButtons';
 import { Steps } from 'src/components/Steps';
 import { Activities } from 'src/components/activities/List';
+import ActivityLink from 'src/components/activities/Link';
 import { ContentView } from 'src/components/activities/content/ContentView';
 import { EditButton } from 'src/components/buttons/EditButton';
 import { ActivityContext } from 'src/contexts/activityContext';
@@ -88,15 +89,9 @@ const PresentationStep4 = () => {
           )}
 
           {!isEdit && activity.responseActivityId === null && (
-            <div style={{ margin: '1rem 0' }}>
-              Votre présentation initie un nouvel échange avec les Pélicopains,{' '}
-              <Link href={`/se-presenter/thematique/1?edit=${activity.id}`}>
-                <a className="text text--primary" href={`/se-presenter/thematique/1?edit=${activity.id}`}>
-                  si vous souhaitez plutôt réagir à une activité déjà publiée, cliquez ici.
-                </a>
-              </Link>
-            </div>
+            <ActivityLink url={`/se-presenter/thematique/1?edit=${activity.id}`} />
           )}
+
           {responseActivity !== null && (
             <>
               <span className="text text--small text--success">Présentation en réaction à {REACTIONS[responseActivity.type]}</span>
