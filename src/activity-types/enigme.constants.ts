3abf0227cafabffa557d381d1a69320b77c89d04
Mikael P <mika.98@live.fr>
2021-09-21 10:20:17 +0200

    Add new enigma category

--- src/activity-types/enigme.constants.ts
+++ src/activity-types/enigme.constants.ts
@@ -4,6 +4,7 @@ export const ENIGME = {
   OBJET: 0,
   EVENEMENT: 1,
   PERSONALITE: 2,
+  LIEU: 3,
 };
 
 export const ENIGME_TYPES = [
@@ -36,6 +37,16 @@ export const ENIGME_TYPES = [
     titleStep2: 'Décrivez votre personalité mystère',
     titleStep2Short: 'personalité',
     description: `La classe choisit de présenter une "personalité mystère", à travers une énigme sous forme de texte, de son, d'image ou de vidéo. Cette personalité peut-être...`,
+  },
+  {
+    title: 'Lieu mystère',
+    title2: 'Le lieu mystère',
+    title3: 'un lieu mystère',
+    titleStep1: 'Choisissez votre lieu mystère',
+    step2: 'Description du lieu',
+    titleStep2: 'Décrivez votre lieu mystère',
+    titleStep2Short: 'lieu',
+    description: `La classe choisit de présenter un "lieu mystère", à travers une énigme sous forme de texte, de son, d'image ou de vidéo. Ce lieu peut-être...`,
   },
 ];
 
@@ -106,6 +117,7 @@ export const ENIGME_DATA = [
       step: 'Un personnage contemporain',
     },
   ],
+  [],
 ];
 
 export function getEnigmeTimeLeft(activity: EnigmeActivity): number {
