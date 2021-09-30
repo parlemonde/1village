b364a409d7a019cd6921e22776880fcc0c0241e3
Sylvain Coutrot <sylvain.coutrot@hotmail.fr>
2021-09-30 17:26:15 +0200

    delete verification logic to be able to pass steps

--- src/pages/indice-culturel/3.tsx
+++ src/pages/indice-culturel/3.tsx
@@ -18,17 +18,32 @@ import { EditButton } from 'src/components/buttons/EditButton';
 import { ActivityContext } from 'src/contexts/activityContext';
 import { useActivity } from 'src/services/useActivity';
 import { ActivityStatus } from 'types/activity.type';
+import { errorColor } from 'src/styles/variables.const';
 
 const IndiceStep3 = () => {
   const router = useRouter();
   const { activity, save } = React.useContext(ActivityContext);
   const { activity: responseActivity } = useActivity(activity?.responseActivityId ?? -1);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [errorSteps, setErrorSteps] = React.useState([]);
+  const [style, setStyle] = React.useState({});
 
   const data = (activity?.data as IndiceData) || null;
   const isEdit = activity !== null && activity.id !== 0 && activity.status !== ActivityStatus.DRAFT;
+ const isValid = () => {
+   let result = true;
+  activity?.processedContent?.map((content) => {
+    result = content.value === "" || content.value === "<p></p>\n" ? false : true
+  })
 
+  return result;
+ }
   React.useEffect(() => {
+    if (!isValid()) {
+      setErrorSteps([1]);
+      setStyle({ border: `1px dashed ${errorColor}` });
+    }
+
     if (activity === null && !('activity-id' in router.query) && !sessionStorage.getItem('activity')) {
       router.push('/indice-culturel');
     } else if (activity && !isIndice(activity)) {
@@ -37,6 +52,9 @@ const IndiceStep3 = () => {
   }, [activity, router]);
 
   const onPublish = async () => {
+    if (!isValid()) {
+      return;
+    }
     setIsLoading(true);
     const success = await save(true);
     if (success) {
@@ -52,7 +70,7 @@ const IndiceStep3 = () => {
   return (
     <Base>
       <div style={{ width: '100%', padding: '0.5rem 1rem 1rem 1rem' }}>
-        <Steps steps={[INDICE_TYPES[activity.subType].step1 ?? 'Indice', "Créer l'indice", 'Prévisualiser']} activeStep={2} />
+        <Steps steps={[INDICE_TYPES[activity.subType].step1 ?? 'Indice', "Créer l'indice", 'Prévisualiser']} activeStep={2} errorSteps={errorSteps} />
         <div className="width-900">
           <h1>Pré-visualisez votre publication{!isEdit && ' et publiez-la.'}</h1>
           <p className="text" style={{ fontSize: '1.1rem' }}>
@@ -82,14 +100,14 @@ const IndiceStep3 = () => {
 
           {responseActivity !== null && (
             <>
-              <span className="text text--small text--success">Présentation en réaction à l&apos;indice culturel</span>
-              <div className="preview-block">
+              <span className={'text text--small text--success'}>Présentation en réaction à l&apos;indice culturel</span>
+              <div className="preview-block" >
                 {!isEdit && (
                   <EditButton
                     onClick={() => {
                       router.push(`/indice-culturel/1?edit=${activity.id}`);
                     }}
-                    isGreen
+                    status={'success'}
                     style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
                   />
                 )}
@@ -98,25 +116,25 @@ const IndiceStep3 = () => {
             </>
           )}
 
-          <span className="text text--small text--success">Thème</span>
+          <span className={'text text--small text--success'}>Thème</span>
           <div className="preview-block">
             <EditButton
               onClick={() => {
                 router.push('/indice-culturel');
               }}
-              isGreen
+              status={'success'}
               style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
             />
             <p style={{ margin: '0.5rem 0' }}>{INDICE_TYPES[activity.subType].title}</p>
           </div>
 
-          <span className="text text--small text--success">Indice culturel</span>
-          <div className="preview-block">
+          <span className={`text text--small ${errorSteps.length > 0 ? 'text--alert' : 'text--success'}`}>Indice culturel</span>
+          <div className="preview-block" style={style}>
             <EditButton
               onClick={() => {
                 router.push('/indice-culturel/2');
               }}
-              isGreen
+              status={errorSteps.length > 0 ? 'error' : 'success'}
               style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
             />
             <ContentView content={activity.processedContent} />
@@ -128,7 +146,7 @@ const IndiceStep3 = () => {
       <Backdrop style={{ zIndex: 2000, color: 'white' }} open={isLoading}>
         <CircularProgress color="inherit" />
       </Backdrop>
-    </Base>
+    </Base >
   );
 };
 
