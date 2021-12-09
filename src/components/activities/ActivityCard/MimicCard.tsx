1bbe41c8e297ae7398cd8deeae8927d1f1019df4
yudino <yohane.udino@gmail.com>
2021-12-09 14:26:58 +0100

    Feature to make mimicCard unique on the timeline

--- src/components/activities/ActivityCard/MimicCard.tsx
+++ src/components/activities/ActivityCard/MimicCard.tsx
@@ -1,38 +1,49 @@
 import Link from 'next/link';
+import ReactPlayer from 'react-player';
 import React from 'react';
 
 import { Button, Grid } from '@mui/material';
 
 import { RedButton } from 'src/components/buttons/RedButton';
 import { UserContext } from 'src/contexts/userContext';
-import VideoPlaceholder from 'src/svg/jeu/video-placeholder.svg';
-import type { GameMimicActivity } from 'types/game.type';
+import { VillageContext } from 'src/contexts/villageContext';
+import { serializeToQueryUrl } from 'src/utils';
+import type { GameActivity, MimicsData } from 'types/game.type';
+import { GameType } from 'types/game.type';
 
 import { CommentIcon } from './CommentIcon';
 import type { ActivityCardProps } from './activity-card.types';
 
-export const MimiqueCard: React.FC<ActivityCardProps<GameMimicActivity>> = ({
-  activity,
-  isSelf,
-  noButtons,
-  isDraft,
-  showEditButtons,
-  onDelete,
-}: ActivityCardProps<GameMimicActivity>) => {
-  const [pictureUrl, setPictureUrl] = React.useState<string>();
-  const { axiosLoggedRequest } = React.useContext(UserContext);
+export const MimicCard = ({ activity, isSelf, noButtons, isDraft, showEditButtons, onDelete }: ActivityCardProps<GameActivity>) => {
+  const { village } = React.useContext(VillageContext);
+  const [count, setCount] = React.useState<number>(0);
 
-  React.useEffect(() => {
-    const videoUrl = activity?.data?.game1?.video;
-    const videoId = videoUrl?.split(/\//).pop();
-    if (!videoId) return;
-    axiosLoggedRequest({
-      method: 'GET',
-      url: `/videos/${videoId}/picture`,
-    }).then((response) => {
-      setPictureUrl(response.data);
-    });
-  }, [activity.data, activity.content]);
+  const { axiosLoggedRequest } = React.useContext(UserContext);
+  const activityMimic = activity.data as MimicsData;
+
+  const randomVideoLink = React.useMemo(() => {
+    const values = Object.values(activityMimic);
+    values.shift(); //First element drafturl remove
+    values.pop(); // Last element presentation remove
+    const rdmMimicPick = values[Math.floor(Math.random() * values.length)]; // random game picked
+    return rdmMimicPick.video;
+  }, [activityMimic]);
+
+  React.useMemo(() => {
+    if (village) {
+      axiosLoggedRequest({
+        method: 'GET',
+        url: `/games/ableToPlay${serializeToQueryUrl({
+          villageId: village.id,
+          type: GameType.MIMIC,
+        })}`,
+      }).then((response) => {
+        if (!response.error && response.data) {
+          setCount(response.data.count as number);
+        }
+      });
+    }
+  }, [axiosLoggedRequest, village]);
 
   return (
     <div
@@ -44,32 +55,12 @@ export const MimiqueCard: React.FC<ActivityCardProps<GameMimicActivity>> = ({
     >
       <div style={{ margin: '0.25rem', flex: 1, minWidth: 0 }}>
         <Grid container spacing={3} style={{ minHeight: '10rem' }}>
-          <Grid item xs={12} md={5}>
-            {pictureUrl ? (
-              <div
-                style={{
-                  height: '100%',
-                  width: '100%',
-                  backgroundImage: `url(${pictureUrl})`,
-                  backgroundSize: 'contain',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundPosition: 'center',
-                }}
-              />
-            ) : (
-              <VideoPlaceholder
-                style={{
-                  height: '50%',
-                  width: '50%',
-                  backgroundSize: 'contain',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundPosition: 'center',
-                }}
-              />
-            )}
+          <Grid item xs={12} md={3} style={{ marginTop: '2rem' }}>
+            {randomVideoLink && <ReactPlayer width="100%" height="100%" light url={randomVideoLink} />}
           </Grid>
-          <Grid item xs={12} md={7}>
-            <span>Nous avons avons ajouté 3 nouvelles mimiques au jeu !</span>
+          <Grid item xs={6} md={6} style={{ marginTop: '2rem' }}>
+            <p>Il y a actuellement {count} nouvelles mimiques à découvrir !</p>
+            <p>{activity.data.presentation} a relancé le jeu des mimiques</p>
           </Grid>
         </Grid>
         {noButtons || (
@@ -77,7 +68,7 @@ export const MimiqueCard: React.FC<ActivityCardProps<GameMimicActivity>> = ({
             {!showEditButtons && (
               <>
                 <CommentIcon count={activity.commentCount} activityId={activity.id} />
-                <Link href="/creer-un-jeu/mimique">
+                <Link href="/creer-un-jeu/mimique" passHref>
                   <Button component="a" color="primary" variant="outlined" href="/creer-un-jeu/mimique">
                     Jouer au jeu
                   </Button>
@@ -92,6 +83,7 @@ export const MimiqueCard: React.FC<ActivityCardProps<GameMimicActivity>> = ({
                       ? `${activity.data.draftUrl}?activity-id=${activity.id}`
                       : `/creer-un-jeu/mimique/4?activity-id=${activity.id}`
                   }
+                  passHref
                 >
                   <Button
                     component="a"
