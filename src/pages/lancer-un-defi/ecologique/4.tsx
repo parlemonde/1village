8105e172bb9e60021513927e0cc405b65271bc59
yudino <yohane.udino@gmail.com>
2021-12-16 16:39:25 +0100

    Publier disabled if previous steps in error

--- src/pages/lancer-un-defi/ecologique/4.tsx
+++ src/pages/lancer-un-defi/ecologique/4.tsx
@@ -95,9 +95,16 @@ const DefiEcoStep4 = () => {
             </div>
           ) : (
             <div style={{ width: '100%', textAlign: 'right', margin: '1rem 0' }}>
-              <Button variant="outlined" color="primary" onClick={onPublish}>
-                Publier
-              </Button>
+              {!isValid && (
+                <p>
+                  <b>Avant de publier votre présentation, il faut corriger les étapes incomplètes, marquées en orange.</b>
+                </p>
+              )}
+              <div style={{ width: '100%', textAlign: 'right', margin: '1rem 0' }}>
+                <Button variant="outlined" color="primary" onClick={onPublish} disabled={!isValid}>
+                  Publier
+                </Button>
+              </div>
             </div>
           )}
 
