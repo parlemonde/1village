159aab73d4e9f217edfa00acaf2766620f30ee50
David Robert-Ansart <david.robertansart@gmail.com>
2022-02-24 09:55:48 +0100

    Add nextJS mock for backend test

--- server/__tests__/server.test.ts
+++ server/__tests__/server.test.ts
@@ -1,33 +1,40 @@
+import type { Request as ExpressRequest, Response as ExpressResponse } from 'express';
+import path from 'path';
 import request from 'supertest';
 import { createConnection } from 'typeorm';
 
 import { getApp } from '../app';
 
-jest.mock('./../utils/database', () => {
-  const originalModule = jest.requireActual('./../utils/database');
+// Mock database to create a in-memory db for testing.
+jest.mock('../utils/database', () => ({
+  __esModule: true,
+  connectToDatabase: async () => {
+    return createConnection({
+      type: 'sqlite',
+      database: ':memory:',
+      dropSchema: true,
+      entities: [path.join(__dirname, '../entities/*.js')],
+      synchronize: true,
+      logging: false,
+    });
+  },
+}));
 
-  //Simule l'exportation par défaut et l'exportation nommée 'foo'
-  return {
-    __esModule: true,
-    ...originalModule,
-    connectToDatabase: async () => {
-      return createConnection({
-        type: 'sqlite',
-        database: ':memory:',
-        dropSchema: true,
-        // entities: [MyEntity],
-        synchronize: true,
-        logging: false,
-      });
+// Mock frontend NextJS library. We don't need it for testing.
+jest.mock('next', () => ({
+  __esModule: true,
+  default: () => ({
+    getRequestHandler: () => (_req: ExpressRequest, res: ExpressResponse) => {
+      res.sendJSON({ isFrontend: true });
     },
-  };
-});
+    prepare: () => Promise.resolve(),
+  }),
+}));
 
 describe('test entry point ', () => {
   it('should return 200', async () => {
     const app = await getApp();
     const response = await request(app).get('/api').set('Accept', 'application/json');
-   console.log(response);
     expect(response.status).toBe(200);
   });
 });
