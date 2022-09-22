d1e00f98e6117492b2a61669e325e98daffa707c
yudino <yohane.udino@gmail.com>
2022-09-22 14:07:01 +0200

    change(#429): Right menu change for Pelico user. Last Activity of Pelico is now visible

--- src/components/accueil/RightNavigation.tsx
+++ src/components/accueil/RightNavigation.tsx
@@ -61,26 +61,76 @@ export const RightNavigation = ({ activityUser, displayAsUser = false }: { activ
 
   if (isPelico) {
     return (
-      <div
-        className="bg-secondary vertical-bottom-margin with-sub-header-height"
-        style={{
-          borderRadius: '10px',
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          padding: '0 0.5rem',
-        }}
-      >
-        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', minWidth: 0 }}>
-          <span style={{ marginRight: '0.3rem', display: 'flex' }}>
-            <AvatarImg user={activityUser} size="extra-small" noLink displayAsUser={displayAsUser} onClick={onclick} style={{ cursor: 'pointer' }} />
-          </span>
-          <span className="text">
-            <strong>{getUserDisplayName(activityUser, false, displayAsUser)}</strong>
-          </span>
+      <>
+        <div
+          className="bg-secondary vertical-bottom-margin with-sub-header-height"
+          style={{
+            borderRadius: '10px',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: '0 0.5rem',
+          }}
+        >
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', minWidth: 0 }}>
+            <span style={{ marginRight: '0.3rem', display: 'flex' }}>
+              <AvatarImg
+                user={activityUser}
+                size="extra-small"
+                noLink
+                displayAsUser={displayAsUser}
+                onClick={onclick}
+                style={{ cursor: 'pointer' }}
+              />
+            </span>
+            <span className="text">
+              <strong>{getUserDisplayName(activityUser, false, displayAsUser)}</strong>
+            </span>
+          </div>
         </div>
-      </div>
+        <div
+          className="bg-secondary vertical-bottom-margin"
+          style={{ padding: '1rem', borderRadius: '10px', display: 'flex', justifyContent: 'center', flexDirection: 'column' }}
+        >
+          <h3>
+            <b>Dernières activités de Pelico</b>
+          </h3>
+          {activities.slice(0, 3).map((activity, index) => {
+            const ActivityIcon = icons[activity.type] || null;
+            return (
+              <div key={index}>
+                {activity.type !== ActivityType.GAME && (
+                  <>
+                    <div style={{ fontSize: 'smaller', paddingBottom: '1rem' }}>
+                      <strong>{DESC[activity.type]},&nbsp;</strong>
+                      le {toDate(activity.createDate as string)}
+                      {ActivityIcon && (
+                        <ActivityIcon
+                          style={{
+                            float: 'right',
+                            fill: primaryColor,
+                            margin: '1.5rem 0.65rem 0 0',
+                            width: '2rem',
+                            height: 'auto',
+                            alignSelf: 'center',
+                          }}
+                        />
+                      )}
+                    </div>
+                    <div style={{ paddingBottom: '1rem' }}>
+                      <CommentIcon count={activity.commentCount} activityId={activity.id} />
+                      <Button component="a" color="primary" variant="outlined" href={`/activite/${activity.id}`}>
+                        {"Voir l'activité"}
+                      </Button>
+                    </div>
+                  </>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </>
     );
   }
   return (
