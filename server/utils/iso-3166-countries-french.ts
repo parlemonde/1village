b8de572f0a3aa21684aa505ba8ceae69307d8aaa
charlinedev <charlinebarbelette@yahoo.fr>
2022-10-07 11:58:03 +0200

    update-countries-0710

--- server/utils/iso-3166-countries-french.ts
+++ server/utils/iso-3166-countries-french.ts
@@ -2,68 +2,76 @@ import type { Country } from '../../types/country.type';
 
 export const countries: Country[] = [
   {
-    isoCode: 'AD',
-    name: 'Andorre',
-  },
-  {
-    isoCode: 'AE',
-    name: '\u00c9mirats Arabes Unis',
-  },
-  {
     isoCode: 'AF',
     name: 'Afghanistan',
   },
   {
-    isoCode: 'AG',
-    name: 'Antigua-Et-Barbuda',
+    isoCode: 'ZA',
+    name: 'Afrique du Sud',
   },
   {
     isoCode: 'AL',
     name: 'Albanie',
   },
   {
-    isoCode: 'AM',
-    name: 'Arm\u00e9nie',
+    isoCode: 'DZ',
+    name: 'Alg\u00e9rie',
+  },
+  {
+    isoCode: 'DE',
+    name: 'Allemagne',
   },
   {
     isoCode: 'AO',
     name: 'Angola',
   },
   {
+    isoCode: 'AD',
+    name: 'Andorre',
+  },
+  {
+    isoCode: 'AG',
+    name: 'Antigua-Et-Barbuda',
+  },
+  {
+    isoCode: 'SA',
+    name: 'Arabie Saoudite',
+  },
+  {
     isoCode: 'AR',
     name: 'Argentine',
   },
   {
-    isoCode: 'WS',
-    name: 'Samoa',
-  },
-  {
-    isoCode: 'AT',
-    name: 'Autriche',
+    isoCode: 'AM',
+    name: 'Arm\u00e9nie',
   },
   {
     isoCode: 'AU',
     name: 'Australie',
   },
   {
-    isoCode: 'AW',
-    name: 'Aruba',
+    isoCode: 'AT',
+    name: 'Autriche',
   },
   {
     isoCode: 'AZ',
     name: 'Azerba\u00efdjan',
   },
   {
-    isoCode: 'BA',
-    name: 'Bosnie-Herz\u00e9govine',
+    isoCode: 'BS',
+    name: 'Bahamas',
   },
   {
-    isoCode: 'BB',
-    name: 'Barbad',
+    isoCode: 'BH',
+    name: 'Bahre\u00efn',
   },
   {
     isoCode: 'BD',
     name: 'Bangladesh',
+  },
+  {
+    isoCode: 'BB',
+    name: 'Barbade',
   },
   {
     isoCode: 'BE',
@@ -74,320 +82,104 @@ export const countries: Country[] = [
     name: 'Belize',
   },
   {
-    isoCode: 'BF',
-    name: 'Burkina Faso',
-  },
-  {
-    isoCode: 'BG',
-    name: 'Bulgarie',
-  },
-  {
-    isoCode: 'BH',
-    name: 'Bahre\u00efn',
-  },
-  {
-    isoCode: 'BI',
-    name: 'Burundi',
-  },
-  {
     isoCode: 'BJ',
     name: 'B\u00e9nin',
-  },
-  {
-    isoCode: 'BN',
-    name: 'Brunei Darussalam',
-  },
-  {
-    isoCode: 'BO',
-    name: '\u00c9tat Plurinational De Bolivie',
-  },
-  {
-    isoCode: 'BR',
-    name: 'Br\u00e9sil',
-  },
-  {
-    isoCode: 'BS',
-    name: 'Bahamas',
   },
   {
     isoCode: 'BT',
     name: 'Bhoutan',
   },
   {
-    isoCode: 'BW',
-    name: 'Botswana',
-  },
-  {
     isoCode: 'BY',
     name: 'Bi\u00e9lorussie',
   },
   {
-    isoCode: 'BZ',
-    name: 'Belize',
+    isoCode: 'MM',
+    name: 'Birmanie',
   },
   {
-    isoCode: 'CA',
-    name: 'Canada',
+    isoCode: 'BO',
+    name: '\u00c9tat Plurinational De Bolivie',
   },
   {
-    isoCode: 'CD',
-    name: 'R\u00e9publique D\u00e9mocratique Du Congo',
+    isoCode: 'BA',
+    name: 'Bosnie-Herz\u00e9govine',
   },
   {
-    isoCode: 'CF',
-    name: 'R\u00e9publique Centrafricaine',
+    isoCode: 'BW',
+    name: 'Botswana',
   },
   {
-    isoCode: 'CG',
-    name: 'Congo',
+    isoCode: 'BR',
+    name: 'Br\u00e9sil',
   },
   {
-    isoCode: 'CH',
-    name: 'Suisse',
+    isoCode: 'BN',
+    name: 'Brunei Darussalam',
   },
   {
-    isoCode: 'CI',
-    name: "C\u00f4te D'Ivoire",
+    isoCode: 'BG',
+    name: 'Bulgarie',
   },
   {
-    isoCode: 'CL',
-    name: 'Chili',
+    isoCode: 'BF',
+    name: 'Burkina Faso',
   },
   {
-    isoCode: 'CM',
-    name: 'Cameroun',
-  },
-  {
-    isoCode: 'CN',
-    name: 'Chine',
-  },
-  {
-    isoCode: 'CO',
-    name: 'Colombie',
-  },
-  {
-    isoCode: 'CR',
-    name: 'Costa Rica',
-  },
-  {
-    isoCode: 'CU',
-    name: 'Cuba',
-  },
-  {
-    isoCode: 'CV',
-    name: 'Cap-Vert',
-  },
-  {
-    isoCode: 'CW',
-    name: 'Cura\u00e7ao',
-  },
-  {
-    isoCode: 'CY',
-    name: 'Chypre',
-  },
-  {
-    isoCode: 'CZ',
-    name: 'R\u00e9publique Tch\u00e8que',
-  },
-  {
-    isoCode: 'DE',
-    name: 'Allemagne',
-  },
-  {
-    isoCode: 'DJ',
-    name: 'Djibouti',
-  },
-  {
-    isoCode: 'DK',
-    name: 'Denmark',
-  },
-  {
-    isoCode: 'DM',
-    name: 'Dominique',
-  },
-  {
-    isoCode: 'DO',
-    name: 'R\u00e9publique Dominicaine',
-  },
-  {
-    isoCode: 'DZ',
-    name: 'Alg\u00e9rie',
-  },
-  {
-    isoCode: 'EC',
-    name: '\u00c9quateur',
-  },
-  {
-    isoCode: 'EE',
-    name: 'Estonie',
-  },
-  {
-    isoCode: 'EG',
-    name: '\u00c9gypte',
-  },
-  {
-    isoCode: 'EH',
-    name: 'Sahara Occidental',
-  },
-  {
-    isoCode: 'ER',
-    name: '\u00c9rythr\u00e9e',
-  },
-  {
-    isoCode: 'ES',
-    name: 'Espagne',
-  },
-  {
-    isoCode: 'ET',
-    name: '\u00c9thiopie',
-  },
-  {
-    isoCode: 'FI',
-    name: 'Finlande',
-  },
-  {
-    isoCode: 'FJ',
-    name: 'Fidji',
-  },
-  {
-    isoCode: 'FM',
-    name: '\u00c9tats F\u00e9d\u00e9r\u00e9s De Micron\u00e9sie',
-  },
-  {
-    isoCode: 'FR',
-    name: 'France',
-  },
-  {
-    isoCode: 'GA',
-    name: 'Gabon',
-  },
-  {
-    isoCode: 'GB',
-    name: 'Royaume-Uni',
-  },
-  {
-    isoCode: 'GD',
-    name: 'Grenade',
-  },
-  {
-    isoCode: 'GE',
-    name: 'G\u00e9orgie',
-  },
-  {
-    isoCode: 'GH',
-    name: 'Ghana',
-  },
-  {
-    isoCode: 'GM',
-    name: 'Gambie',
-  },
-  {
-    isoCode: 'GN',
-    name: 'Guin\u00e9e',
-  },
-  {
-    isoCode: 'GQ',
-    name: 'Guin\u00e9e \u00c9quatoriale',
-  },
-  {
-    isoCode: 'GR',
-    name: 'Gr\u00e8ce',
-  },
-  {
-    isoCode: 'GT',
-    name: 'Guatemala',
-  },
-  {
-    isoCode: 'GW',
-    name: 'Guin\u00e9e-Bissau',
-  },
-  {
-    isoCode: 'GY',
-    name: 'Guyana',
-  },
-  {
-    isoCode: 'HN',
-    name: 'Honduras',
-  },
-  {
-    isoCode: 'HR',
-    name: 'Croatie',
-  },
-  {
-    isoCode: 'HT',
-    name: 'Ha\u00efti',
-  },
-  {
-    isoCode: 'HU',
-    name: 'Hongrie',
-  },
-  {
-    isoCode: 'ID',
-    name: 'Indon\u00e9sie',
-  },
-  {
-    isoCode: 'IE',
-    name: 'Irlande',
-  },
-  {
-    isoCode: 'IL',
-    name: 'Isra\u00ebl',
-  },
-  {
-    isoCode: 'IN',
-    name: 'Inde',
-  },
-  {
-    isoCode: 'IQ',
-    name: 'Irak',
-  },
-  {
-    isoCode: 'IR',
-    name: "R\u00e9publique Islamique D'Iran",
-  },
-  {
-    isoCode: 'IS',
-    name: 'Islande',
-  },
-  {
-    isoCode: 'IT',
-    name: 'Italie',
-  },
-  {
-    isoCode: 'JM',
-    name: 'Jama\u00efque',
-  },
-  {
-    isoCode: 'JO',
-    name: 'Jordanie',
-  },
-  {
-    isoCode: 'JP',
-    name: 'Japon',
-  },
-  {
-    isoCode: 'KE',
-    name: 'Kenya',
-  },
-  {
-    isoCode: 'KG',
-    name: 'Kirghizistan',
+    isoCode: 'BI',
+    name: 'Burundi',
   },
   {
     isoCode: 'KH',
     name: 'Cambodge',
   },
   {
-    isoCode: 'KI',
-    name: 'Kiribati',
+    isoCode: 'CM',
+    name: 'Cameroun',
+  },
+  {
+    isoCode: 'CA',
+    name: 'Canada',
+  },
+  {
+    isoCode: 'CV',
+    name: 'Cap-Vert',
+  },
+  {
+    isoCode: 'CF',
+    name: 'R\u00e9publique Centrafricaine',
+  },
+  {
+    isoCode: 'CL',
+    name: 'Chili',
+  },
+  {
+    isoCode: 'CN',
+    name: 'Chine',
+  },
+  {
+    isoCode: 'CY',
+    name: 'Chypre',
+  },
+  {
+    isoCode: 'CO',
+    name: 'Colombie',
   },
   {
     isoCode: 'KM',
     name: 'Comores',
   },
   {
-    isoCode: 'KN',
-    name: 'Saint-Christophe-et-Ni\u00e9v\u00e8s',
+    isoCode: 'CG',
+    name: 'Congo',
+  },
+  {
+    isoCode: 'CD',
+    name: 'R\u00e9publique D\u00e9mocratique Du Congo',
+  },
+  {
+    isoCode: 'CK',
+    name: '\u00celes Cook',
   },
   {
     isoCode: 'KP',
@@ -398,40 +190,232 @@ export const countries: Country[] = [
     name: 'R\u00e9publique De Cor\u00e9e',
   },
   {
-    isoCode: 'KW',
-    name: 'Kowe\u00eft',
+    isoCode: 'CR',
+    name: 'Costa Rica',
+  },
+  {
+    isoCode: 'CI',
+    name: "C\u00f4te D'Ivoire",
+  },
+  {
+    isoCode: 'HR',
+    name: 'Croatie',
+  },
+  {
+    isoCode: 'CU',
+    name: 'Cuba',
+  },
+  {
+    isoCode: 'DK',
+    name: 'Danemark',
+  },
+  {
+    isoCode: 'DJ',
+    name: 'Djibouti',
+  },
+  {
+    isoCode: 'DO',
+    name: 'R\u00e9publique Dominicaine',
+  },
+  {
+    isoCode: 'DM',
+    name: 'Dominique',
+  },
+  {
+    isoCode: 'EG',
+    name: '\u00c9gypte',
+  },
+  {
+    isoCode: 'AE',
+    name: '\u00c9mirats Arabes Unis',
+  },
+  {
+    isoCode: 'EC',
+    name: '\u00c9quateur',
+  },
+  {
+    isoCode: 'ER',
+    name: '\u00c9rythr\u00e9e',
+  },
+  {
+    isoCode: 'ES',
+    name: 'Espagne',
+  },
+  {
+    isoCode: 'EE',
+    name: 'Estonie',
+  },
+  {
+    isoCode: 'SZ',
+    name: 'Swaziland',
+  },
+  {
+    isoCode: 'US',
+    name: '\u00c9tats-Unis',
+  },
+  {
+    isoCode: 'ET',
+    name: '\u00c9thiopie',
+  },
+  {
+    isoCode: 'FJ',
+    name: 'Fidji',
+  },
+  {
+    isoCode: 'FI',
+    name: 'Finlande',
+  },
+  {
+    isoCode: 'FR',
+    name: 'France',
+  },
+  {
+    isoCode: 'GA',
+    name: 'Gabon',
+  },
+  {
+    isoCode: 'GM',
+    name: 'Gambie',
+  },
+  {
+    isoCode: 'GE',
+    name: 'G\u00e9orgie',
+  },
+  {
+    isoCode: 'GH',
+    name: 'Ghana',
+  },
+  {
+    isoCode: 'GR',
+    name: 'Gr\u00e8ce',
+  },
+  {
+    isoCode: 'GD',
+    name: 'Grenade',
+  },
+  {
+    isoCode: 'GT',
+    name: 'Guatemala',
+  },
+  {
+    isoCode: 'GN',
+    name: 'Guin\u00e9e',
+  },
+  {
+    isoCode: 'GQ',
+    name: 'Guin\u00e9e \u00c9quatoriale',
+  },
+  {
+    isoCode: 'GW',
+    name: 'Guin\u00e9e-Bissau',
+  },
+  {
+    isoCode: 'GY',
+    name: 'Guyana',
+  },
+  {
+    isoCode: 'HT',
+    name: 'Ha\u00efti',
+  },
+  {
+    isoCode: 'HN',
+    name: 'Honduras',
+  },
+  {
+    isoCode: 'HU',
+    name: 'Hongrie',
+  },
+  {
+    isoCode: 'IN',
+    name: 'Inde',
+  },
+  {
+    isoCode: 'ID',
+    name: 'Indon\u00e9sie',
+  },
+  {
+    isoCode: 'IQ',
+    name: 'Irak',
+  },
+  {
+    isoCode: 'IR',
+    name: "R\u00e9publique Islamique D'Iran",
+  },
+  {
+    isoCode: 'IE',
+    name: 'Irlande',
+  },
+  {
+    isoCode: 'IS',
+    name: 'Islande',
+  },
+  {
+    isoCode: 'IL',
+    name: 'Isra\u00ebl',
+  },
+  {
+    isoCode: 'IT',
+    name: 'Italie',
+  },
+  {
+    isoCode: 'JM',
+    name: 'Jama\u00efque',
+  },
+  {
+    isoCode: 'JP',
+    name: 'Japon',
+  },
+  {
+    isoCode: 'JO',
+    name: 'Jordanie',
+  },
+  {
+    isoCode: 'KG',
+    name: 'Kirghizistan',
+  },
+  {
+    isoCode: 'KI',
+    name: 'Kiribati',
   },
   {
     isoCode: 'KZ',
     name: 'Kazakhstan',
   },
   {
+    isoCode: 'KE',
+    name: 'Kenya',
+  },
+  {
+    isoCode: 'KW',
+    name: 'Kowe\u00eft',
+  },
+  {
     isoCode: 'LA',
     name: 'R\u00e9publique D\u00e9mocratique Populaire Lao',
+  },
+  {
+    isoCode: 'LS',
+    name: 'Lesotho',
+  },
+  {
+    isoCode: 'LV',
+    name: 'Lettonie',
   },
   {
     isoCode: 'LB',
     name: 'Liban',
   },
   {
-    isoCode: 'LC',
-    name: 'Sainte-Lucie',
-  },
-  {
-    isoCode: 'LI',
-    name: 'Liechtenstein',
-  },
-  {
-    isoCode: 'LK',
-    name: 'Sri Lanka',
-  },
-  {
     isoCode: 'LR',
     name: 'Liberia',
   },
   {
-    isoCode: 'LS',
-    name: 'Lesotho',
+    isoCode: 'LY',
+    name: 'Libye',
+  },
+  {
+    isoCode: 'LI',
+    name: 'Liechtenstein',
   },
   {
     isoCode: 'LT',
@@ -442,16 +426,56 @@ export const countries: Country[] = [
     name: 'Luxembourg',
   },
   {
-    isoCode: 'LV',
-    name: 'Lettonie',
+    isoCode: 'MG',
+    name: 'Madagascar',
   },
   {
-    isoCode: 'LY',
-    name: 'Libye',
+    isoCode: 'MK',
+    name: 'Mac\u00e9doine Du Nord',
+  },
+  {
+    isoCode: 'MY',
+    name: 'Malaisie',
+  },
+  {
+    isoCode: 'MW',
+    name: 'Malawi',
+  },
+  {
+    isoCode: 'MV',
+    name: 'Maldives',
+  },
+  {
+    isoCode: 'ML',
+    name: 'Mali',
+  },
+  {
+    isoCode: 'MT',
+    name: 'Malte',
   },
   {
     isoCode: 'MA',
     name: 'Maroc',
+  },
+  {
+    isoCode: 'MH',
+    name: 'Marshall',
+  },
+  {
+    isoCode: 'MU',
+    name: 'Maurice',
+  },
+  {
+    isoCode: 'MR',
+    name: 'Mauritanie',
+  },
+  {
+    isoCode: 'MX',
+    name: 'Mexique',
+  },
+  {
+    isoCode: 'FM',
+    name: '\u00c9tats F\u00e9d\u00e9r\u00e9s De Micron\u00e9sie',
   },
   {
     isoCode: 'MC',
@@ -466,52 +490,8 @@ export const countries: Country[] = [
     name: 'Mont\u00e9n\u00e9gro',
   },
   {
-    isoCode: 'MG',
-    name: 'Madagascar',
-  },
-  {
-    isoCode: 'MK',
-    name: 'Mac\u00e9doine Du Nord',
-  },
-  {
-    isoCode: 'ML',
-    name: 'Mali',
-  },
-  {
-    isoCode: 'MM',
-    name: 'Birmanie',
-  },
-  {
     isoCode: 'MN',
     name: 'Mongolie',
-  },
-  {
-    isoCode: 'MR',
-    name: 'Mauritanie',
-  },
-  {
-    isoCode: 'MT',
-    name: 'Malte',
-  },
-  {
-    isoCode: 'MU',
-    name: 'Maurice',
-  },
-  {
-    isoCode: 'MV',
-    name: 'Maldives',
-  },
-  {
-    isoCode: 'MW',
-    name: 'Malawi',
-  },
-  {
-    isoCode: 'MX',
-    name: 'Mexique',
-  },
-  {
-    isoCode: 'MY',
-    name: 'Malaisie',
   },
   {
     isoCode: 'MZ',
@@ -520,6 +500,14 @@ export const countries: Country[] = [
   {
     isoCode: 'NA',
     name: 'Namibie',
+  },
+  {
+    isoCode: 'NR',
+    name: 'Nauru',
+  },
+  {
+    isoCode: 'NP',
+    name: 'N\u00e9pal',
   },
   {
     isoCode: 'NE',
@@ -534,24 +522,12 @@ export const countries: Country[] = [
     name: 'Nicaragua',
   },
   {
-    isoCode: 'NL',
-    name: 'Pays-Bas',
+    isoCode: 'NU',
+    name: 'Niue',
   },
   {
     isoCode: 'NO',
     name: 'Norv\u00e8ge',
-  },
-  {
-    isoCode: 'NP',
-    name: 'N\u00e9pal',
-  },
-  {
-    isoCode: 'NR',
-    name: 'Nauru',
-  },
-  {
-    isoCode: 'NU',
-    name: 'Niue',
   },
   {
     isoCode: 'NZ',
@@ -562,40 +538,56 @@ export const countries: Country[] = [
     name: 'Oman',
   },
   {
-    isoCode: 'PA',
-    name: 'Panama',
+    isoCode: 'UG',
+    name: 'Ouganda',
   },
   {
-    isoCode: 'PE',
-    name: 'P\u00e9rou',
-  },
-  {
-    isoCode: 'PH',
-    name: 'Philippines',
+    isoCode: 'UZ',
+    name: 'Ouzb\u00e9kistan',
   },
   {
     isoCode: 'PK',
     name: 'Pakistan',
   },
   {
-    isoCode: 'PL',
-    name: 'Pologne',
+    isoCode: 'PW',
+    name: 'Palaos',
   },
   {
     isoCode: 'PS',
     name: 'Palestine',
   },
   {
-    isoCode: 'PT',
-    name: 'Portugal',
+    isoCode: 'PA',
+    name: 'Panama',
   },
   {
-    isoCode: 'PW',
-    name: 'Palaos',
+    isoCode: 'PG',
+    name: 'Papouasie-Nouvelle-Guinée',
   },
   {
     isoCode: 'PY',
     name: 'Paraguay',
+  },
+  {
+    isoCode: 'PE',
+    name: 'P\u00e9rou',
+  },
+  {
+    isoCode: 'NL',
+    name: 'Pays-Bas',
+  },
+  {
+    isoCode: 'PH',
+    name: 'Philippines',
+  },
+  {
+    isoCode: 'PL',
+    name: 'Pologne',
+  },
+  {
+    isoCode: 'PT',
+    name: 'Portugal',
   },
   {
     isoCode: 'QA',
@@ -606,8 +598,8 @@ export const countries: Country[] = [
     name: 'Roumanie',
   },
   {
-    isoCode: 'RS',
-    name: 'Serbie',
+    isoCode: 'GB',
+    name: 'Royaume-Uni',
   },
   {
     isoCode: 'RU',
@@ -618,156 +610,164 @@ export const countries: Country[] = [
     name: 'Rwanda',
   },
   {
-    isoCode: 'SA',
-    name: 'Arabie Saoudite',
+    isoCode: 'KN',
+    name: 'Saint-Christophe-et-Ni\u00e9v\u00e8s',
   },
   {
-    isoCode: 'SB',
-    name: '\u00celes Salomon',
-  },
-  {
-    isoCode: 'SC',
-    name: 'Seychelles',
-  },
-  {
-    isoCode: 'SD',
-    name: 'Soudan',
-  },
-  {
-    isoCode: 'SE',
-    name: 'Su\u00e8de',
-  },
-  {
-    isoCode: 'SG',
-    name: 'Singapour',
-  },
-  {
-    isoCode: 'SI',
-    name: 'Slov\u00e9nie',
-  },
-  {
-    isoCode: 'SK',
-    name: 'Slovaquie',
-  },
-  {
-    isoCode: 'SL',
-    name: 'Sierra Leone',
+    isoCode: 'LC',
+    name: 'Sainte-Lucie',
   },
   {
     isoCode: 'SM',
     name: 'Saint-Marin',
   },
   {
-    isoCode: 'SN',
-    name: 'S\u00e9n\u00e9gal',
+    isoCode: 'VC',
+    name: 'Saint-Vincent-Et-Les Grenadines',
   },
   {
-    isoCode: 'SO',
-    name: 'Somalie',
-  },
-  {
-    isoCode: 'SR',
-    name: 'Suriname',
-  },
-  {
-    isoCode: 'SS',
-    name: 'Soudan Du Sud',
-  },
-  {
-    isoCode: 'ST',
-    name: 'Sao Tom\u00e9-Et-Principe',
+    isoCode: 'SB',
+    name: '\u00celes Salomon',
   },
   {
     isoCode: 'SV',
     name: 'R\u00e9publique Du Salvador',
   },
   {
+    isoCode: 'WS',
+    name: 'Samoa',
+  },
+  {
+    isoCode: 'ST',
+    name: 'Sao Tom\u00e9-Et-Principe',
+  },
+  {
+    isoCode: 'SN',
+    name: 'S\u00e9n\u00e9gal',
+  },
+  {
+    isoCode: 'RS',
+    name: 'Serbie',
+  },
+  {
+    isoCode: 'SC',
+    name: 'Seychelles',
+  },
+  {
+    isoCode: 'SL',
+    name: 'Sierra Leone',
+  },
+  {
+    isoCode: 'SG',
+    name: 'Singapour',
+  },
+  {
+    isoCode: 'SK',
+    name: 'Slovaquie',
+  },
+  {
+    isoCode: 'SI',
+    name: 'Slov\u00e9nie',
+  },
+  {
+    isoCode: 'SO',
+    name: 'Somalie',
+  },
+  {
+    isoCode: 'SD',
+    name: 'Soudan',
+  },
+  {
+    isoCode: 'SS',
+    name: 'Soudan Du Sud',
+  },
+  {
+    isoCode: 'LK',
+    name: 'Sri Lanka',
+  },
+  {
+    isoCode: 'SE',
+    name: 'Su\u00e8de',
+  },
+  {
+    isoCode: 'CH',
+    name: 'Suisse',
+  },
+  {
+    isoCode: 'SR',
+    name: 'Suriname',
+  },
+  {
     isoCode: 'SY',
     name: 'R\u00e9publique Arabe Syrienne',
-  },
-  {
-    isoCode: 'SZ',
-    name: 'Swaziland',
-  },
-  {
-    isoCode: 'TD',
-    name: 'Tchad',
-  },
-  {
-    isoCode: 'TG',
-    name: 'Togo',
-  },
-  {
-    isoCode: 'TH',
-    name: 'Tha\u00eflande',
   },
   {
     isoCode: 'TJ',
     name: 'Tadjikistan',
   },
   {
+    isoCode: 'TZ',
+    name: 'R\u00e9publique-Unie De Tanzanie',
+  },
+  {
+    isoCode: 'TD',
+    name: 'Tchad',
+  },
+  {
+    isoCode: 'CZ',
+    name: 'R\u00e9publique Tch\u00e8que',
+  },
+  {
+    isoCode: 'TH',
+    name: 'Tha\u00eflande',
+  },
+  {
     isoCode: 'TL',
     name: 'Timor-Leste',
   },
   {
-    isoCode: 'TM',
-    name: 'Turkm\u00e9nistan',
-  },
-  {
-    isoCode: 'TN',
-    name: 'Tunisie',
+    isoCode: 'TG',
+    name: 'Togo',
   },
   {
     isoCode: 'TO',
     name: 'Tonga',
   },
   {
-    isoCode: 'TR',
-    name: 'Turquie',
-  },
-  {
     isoCode: 'TT',
     name: 'Trinit\u00e9-Et-Tobago',
+  },
+  {
+    isoCode: 'TN',
+    name: 'Tunisie',
+  },
+  {
+    isoCode: 'TM',
+    name: 'Turkm\u00e9nistan',
+  },
+  {
+    isoCode: 'TR',
+    name: 'Turquie',
   },
   {
     isoCode: 'TV',
     name: 'Tuvalu',
   },
   {
-    isoCode: 'TW',
-    name: 'Ta\u00efwan',
-  },
-  {
-    isoCode: 'TZ',
-    name: 'R\u00e9publique-Unie De Tanzanie',
-  },
-  {
     isoCode: 'UA',
     name: 'Ukraine',
-  },
-  {
-    isoCode: 'UG',
-    name: 'Ouganda',
-  },
-  {
-    isoCode: 'US',
-    name: '\u00c9tats-Unis',
   },
   {
     isoCode: 'UY',
     name: 'Uruguay',
   },
   {
-    isoCode: 'UZ',
-    name: 'Ouzb\u00e9kistan',
+    isoCode: 'VU',
+    name: 'Vanuatu',
   },
   {
     isoCode: 'VA',
     name: 'Saint-Si\u00e8ge (\u00c9tat De La Cit\u00e9 Du Vatican)',
-  },
-  {
-    isoCode: 'VC',
-    name: 'Saint-Vincent-Et-Les Grenadines',
   },
   {
     isoCode: 'VE',
@@ -778,20 +778,8 @@ export const countries: Country[] = [
     name: 'Viet Nam',
   },
   {
-    isoCode: 'VU',
-    name: 'Vanuatu',
-  },
-  {
-    isoCode: 'WS',
-    name: 'Samoa',
-  },
-  {
     isoCode: 'YE',
     name: 'Y\u00e9men',
-  },
-  {
-    isoCode: 'ZA',
-    name: 'Afrique Du Sud',
   },
   {
     isoCode: 'ZM',
