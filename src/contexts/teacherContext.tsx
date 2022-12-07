4c5a040840e368fdf56b4f291eb0e2b247970f77
yudino <yohane.udino@gmail.com>
2022-12-07 16:25:16 +0100

    feat(teacher): init dashboard teacher

---
+++ src/contexts/teacherContext.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface TeacherContextValue {}
+
+export const TeacherContext = React.createContext<TeacherContextValue>({});
+
+//TODO : il faut alimenter l'interface et le TeaCherContext avec les fonctions et les bon types, les fonctions sont à titre d'exemple
+
+export const TeacherContextProvider = ({ children }: React.PropsWithChildren<Record<string, unknown>>) => {
+  /**
+   * Set the list of students in the classrom
+   */
+  const setStudentList = React.useCallback(() => {}, []);
+
+  /**
+   * Get the list of students in the classroom
+   */
+  const getStudentList = React.useCallback(() => {}, []);
+
+  /**
+   * Delete an access for a relative's student
+   */
+  const deleteAccessTorRelatives = React.useCallback(() => {}, []);
+
+  const value = React.useMemo(
+    () => ({
+      setStudentList,
+      getStudentList,
+      deleteAccessTorRelatives,
+    }),
+    [deleteAccessTorRelatives, getStudentList, setStudentList],
+  );
+  return <TeacherContext.Provider value={value}>{children}</TeacherContext.Provider>;
+};
