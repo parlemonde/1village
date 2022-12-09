e49598110df81ff90a11c135ebd63afeca67155b
yudino <yohane.udino@gmail.com>
2022-12-09 17:02:07 +0100

    change(teacher page): page 1 still in progress

--- src/pages/familles/1.tsx
+++ src/pages/familles/1.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { FormControlLabel, Radio, RadioGroup, TextField } from '@mui/material';
 
@@ -7,8 +7,69 @@ import { Base } from 'src/components/Base';
 import { Steps } from 'src/components/Steps';
 import { StepsButton } from 'src/components/StepsButtons';
 
+type Props = {
+  text1: string;
+  text2?: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  value: number;
+};
+
+/**
+ * Container to display text and input inline
+ * @param onChange function to handle changes
+ * @param value value of the input
+ * @param object text object containing text to display
+ */
+const TextnInputContainer = ({ onChange, value, ...props }: Props) => {
+  const { text1, text2 } = props;
+  const spanStyle = { flexShrink: 0, marginRight: '0.5rem' };
+  return (
+    <div className="textnInputContainer__line" style={{ display: 'flex', alignItems: 'flex-start' }}>
+      <span style={spanStyle}>{text1}</span>
+      <TextField
+        className="textnInputContainer__textfield"
+        variant="standard"
+        type="number"
+        inputProps={{ min: 0 }}
+        // onFocus={onFocusInput('totalStudent')}
+        size="small"
+        value={value}
+        onChange={onChange}
+        sx={{
+          width: '2rem',
+          marginRight: '5px',
+        }}
+      />
+      <span style={spanStyle}>{text2}</span>
+    </div>
+  );
+};
+
+const content1 = {
+  text1: 'les familles peuvent voir toutes les activités publiées sur 1Village, mais',
+  text2: 'jours après leurs publication',
+};
+const content2 = {
+  text1: 'les familles peuvent voir toutes les activités publiées sur 1Village, mais',
+  text2: 'jours après leurs publication',
+};
+
 const ClassroomParamStep1 = () => {
   const router = useRouter();
+  const [daysDelay, setDaysDelay] = React.useState(0);
+  const [isDisabled, setIsDisabled] = React.useState(true);
+  const radioSelectedRef = useRef('default');
+
+  const handleDaysDelay = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDaysDelay(Number((event.target as HTMLInputElement).value));
+  };
+  const handleRadioSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    radioSelectedRef.current = event.target.value;
+  };
+  const toggle = (bool: boolean) => {
+    setIsDisabled(bool);
+  };
   const onNext = () => {
     router.push('/familles/2');
   };
@@ -26,44 +87,31 @@ const ClassroomParamStep1 = () => {
           Vous allez inviter les familles de vos élèves à se connecter à 1Village. Ainsi, elles pourront observer les échanges qui ont lieu en ligne.
           Vous avez la possibilité de définir ce que les familles voient sur la plateforme. Choisissez parmi ces 4 options :
         </p>
-        <RadioGroup aria-label="visibility" name="visibility" value={1}>
+        {/* <RadioGroup aria-label="visibility" value={radioSelectedRef} onChange={handleRadioSelect}> */}
+        <RadioGroup aria-label="visibility" onChange={handleRadioSelect} defaultValue={radioSelectedRef.current}>
           <FormControlLabel
-            value={1}
+            value="default"
             control={<Radio />}
             label="les familles peuvent voir toutes les activités publiées sur 1Village, dès leur publication"
-            style={{ maxWidth: '100%' }}
           />
           <FormControlLabel
+            value="timeDelay"
             control={<Radio />}
-            // label="les familles peuvent voir toutes les activités publiées sur 1Village, mais __ jours après leurs publication"
-            label={
-              <div className="se-presenter-step-one__line" style={{ display: 'flex', alignItems: 'flex-start', margin: '1.4rem 0' }}>
-                <span style={{ flexShrink: 0, marginRight: '0.5rem' }}>Nous sommes</span>
-                <TextField
-                  className="se-presenter-step-one__textfield se-presenter-step-one__textfield--full-width"
-                  variant="standard"
-                  style={{ flex: 1, minWidth: 0 }}
-                  fullWidth
-                  placeholder={'hello'}
-                  value={''}
-                  //   onChange={dataChange('presentation')}
-                  //   error={showError && !data.presentation}
-                  //   helperText={showError && !data.presentation ? 'Ce champ est obligatoire' : ''}
-                />
-              </div>
-            }
-            style={{ maxWidth: '100%' }}
+            label={<TextnInputContainer {...content1} onChange={handleDaysDelay} value={daysDelay} />}
+            onClick={() => toggle(false)}
+            disabled={isDisabled}
           />
           <FormControlLabel
+            value="ownClass"
             control={<Radio />}
             label="les familles peuvent voir toutes les activités publiées sur 1Village, dès leur publication, mais seulement celles publiées par notre classe"
-            style={{ maxWidth: '100%' }}
           />
           <FormControlLabel
+            value="ownClass&TimeDelay"
             control={<Radio />}
-            label="les familles peuvent voir toutes les activités publiées sur 1Village, mais seulement celles publiées par notre classe 
-          et __ jours après leurs publication"
-            style={{ maxWidth: '100%' }}
+            label={<TextnInputContainer {...content2} onChange={handleDaysDelay} value={daysDelay} />}
+            onClick={() => toggle(false)}
+            disabled={isDisabled}
           />
         </RadioGroup>
         <StepsButton next={onNext} />
