4843d18a496b8213980d201c2f287e0dc9c54e2b
Yan Labarthe <yan.labarthe@gmail.com>
2023-01-09 10:32:48 +0100

    i think it works !!!!

--- server/migrations/1672916589276-alterUsertable.ts
+++ server/migrations/1672916589276-alterUsertable.ts
@@ -1,33 +1,72 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
+import { TableColumn } from 'typeorm';
 
-export class UpdateUserType1593203874479 implements MigrationInterface {
+export class MyMigration1579323075000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-      UPDATE users
-      SET type = 
-        CASE
-          WHEN type = 'TEACHER' THEN '3'
-          WHEN type = 'OBSERVATOR' THEN '5'
-          WHEN type = 'MEDIATOR' THEN '2'
-          WHEN type = 'ADMIN' THEN '1'
-          WHEN type = 'SUPER_ADMIN' THEN '0'
-          WHEN type = 'FAMILY' THEN '4'
-        END
-    `);
+    await queryRunner.addColumn(
+      'user',
+      new TableColumn({
+        name: 'newtype',
+        type: 'tinyint',
+        isNullable: true,
+      }),
+    );
+
+    // Map the old enum values to the new tinyint values
+    const mapping = {
+      '0': 3, // TEACHER = 3
+      '1': 5, // OBSERVATOR = 5
+      '2': 2, // MEDIATOR = 2
+      '3': 1, // ADMIN = 1
+      '4': 0, // SUPER_ADMIN = 0
+    };
+
+    const queries = Object.entries(mapping).map(([enumValue, tinyintValue]) => {
+      return `UPDATE user SET newtype = ${tinyintValue} WHERE type = '${enumValue}'`;
+    });
+
+    for (const query of queries) {
+      await queryRunner.query(query);
+    }
+
+    await queryRunner.dropColumn('user', 'type');
+    await queryRunner.renameColumn('user', 'newtype', 'type');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-      UPDATE users
-      SET type = 
-        CASE
-          WHEN type = '3' THEN 'TEACHER'
-          WHEN type = '5' THEN 'OBSERVATOR'
-          WHEN type = '2' THEN 'MEDIATOR'
-          WHEN type = '1' THEN 'ADMIN'
-          WHEN type = '0' THEN 'SUPER_ADMIN'
-          WHEN type = '4' THEN 'FAMILY'
-        END
-    `);
+    /*     // Add the enum column back
+    await queryRunner.addColumn(
+      'user',
+      new TableColumn({
+        name: 'type',
+        type: 'enum',
+        enum: ['value1', 'value2', 'value3'],
+        isNullable: true,
+      }),
+    );
+
+    // Map the new tinyint values to the old enum values
+    const mapping = {
+      0: '4', // SUPER_ADMIN = "4"
+      1: '3', // ADMIN = "3"
+      2: '2', // MEDIATOR = "2"
+      3: '0', // TEACHER = "0"
+      4: '5', // FAMILY = "" (not present in the old enum)
+      5: '1', // OBSERVATOR = "1"
+    };
+
+    // Copy the data from the tinyint column back to the enum column, using the mapping
+    await queryRunner.query(
+      `
+      UPDATE user
+      SET type = ?
+      WHERE newtype = ?
+    `,
+      [Object.values(mapping), Object.keys(mapping)],
+    );
+
+    // Drop the tinyint column and rename the table back
+    await queryRunner.dropColumn('user', 'newtype');
+    await queryRunner.renameTable('user', 'newtype'); */
   }
 }
