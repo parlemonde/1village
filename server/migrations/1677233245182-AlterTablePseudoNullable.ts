aeddf14b2c576d327195f04a6cd43bf64e96260f
charlinedev <charline.barbelette@sciam.fr>
2023-02-24 11:41:10 +0100

    fix migration pseudo

--- server/migrations/1677233245182-AlterTablePseudoNullable.ts
+++ server/migrations/1677233245182-AlterTablePseudoNullable.ts
@@ -3,10 +3,10 @@ import type { MigrationInterface, QueryRunner } from 'typeorm';
 export class AlterTablePseudoNullable1677233245182 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE user ALTER COLUMN pseudo SET DEFAULT ''`);
-    await queryRunner.query(`ALTER TABLE user DROP CONSTRAINT "IDX_be726a825c7254f55be1540601"`);
+    await queryRunner.query(`ALTER TABLE user DROP INDEX IDX_be726a825c7254f55be1540601`);
   }
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE user ALTER COLUMN pseudo SET DEFAULT NULL`);
-    await queryRunner.query(`ALTER TABLE user ADD CONSTRAINT "IDX_be726a825c7254f55be1540601" UNIQUE(pseudo)`);
+    await queryRunner.query(`CREATE UNIQUE INDEX IDX_be726a825c7254f55be1540601 ON user (pseudo)`);
   }
 }
