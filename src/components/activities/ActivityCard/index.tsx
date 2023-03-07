72b61f9c5150501f6614ea909adb16103df50cf8
MarineChillaud <marine.marez@gmail.com>
2023-03-07 16:16:25 +0100

    fix: refacto-cards: index.tsx
    typescript errors

--- src/components/activities/ActivityCard/index.tsx
+++ src/components/activities/ActivityCard/index.tsx
@@ -19,6 +19,7 @@ import { StoryCard } from './StoryCard';
 import { SymbolCard } from './SymbolCard';
 import { VerseRecordCard } from './VerseRecordCard';
 import type { ActivityCardProps } from './activity-card.types';
+import { isEnigme, isReaction } from 'src/activity-types/anyActivity';
 import { getEnigmeTimeLeft } from 'src/activity-types/enigme.constants';
 import { AvatarImg } from 'src/components/Avatar';
 import { Flag } from 'src/components/Flag';
