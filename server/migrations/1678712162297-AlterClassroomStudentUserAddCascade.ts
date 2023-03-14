bf54f3ee1838ae64f1b9ba1049232788dce67e5f
Yan Labarthe <yan.labarthe@gmail.com>
2023-03-14 16:34:08 +0100

    newMigration

--- server/migrations/1678712162297-AlterClassroomStudentUserAddCascade.ts
+++ server/migrations/1678712162297-AlterClassroomStudentUserAddCascade.ts
@@ -1,26 +1,111 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
-import { Table } from 'typeorm';
+import { TableForeignKey } from 'typeorm';
 
+export async function dropForeignKeyIfExists(queryRunner: QueryRunner, tableName: string, columnName: string): Promise<void> {
+  const table = await queryRunner.getTable(tableName);
+  if (table) {
+    const foreignKey = table.foreignKeys.find((fk) => fk.columnNames.indexOf(columnName) !== -1);
+    if (foreignKey) {
+      await queryRunner.dropForeignKey(tableName, foreignKey);
+    }
+  }
+}
 export class AlterClassroomStudentUserAddCascade1678712162297 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
-    const table = await queryRunner.getTable('user');
-    const foreignKey = table.foreignKeys.find((fk) => fk.columnNames.indexOf('') !== -1);
-    await queryRunner.dropForeignKey('answer', foreignKey);
+    await dropForeignKeyIfExists(queryRunner, 'classroom', 'userId');
+    await dropForeignKeyIfExists(queryRunner, 'user_to_student', 'userId');
+    await dropForeignKeyIfExists(queryRunner, 'user_to_student', 'studentId');
+    await dropForeignKeyIfExists(queryRunner, 'student', 'UserStudent_UserFK');
 
     await queryRunner.createForeignKey(
-      'answer',
+      'classroom',
       new TableForeignKey({
-        columnNames: [''],
+        columnNames: ['userId'],
         referencedColumnNames: ['id'],
-        referencedTableName: '',
+        referencedTableName: 'user',
         onDelete: 'CASCADE',
+        name: 'ClassroomUserFK',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'user_to_student',
+      new TableForeignKey({
+        columnNames: ['userId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'user',
+        onDelete: 'CASCADE',
+        name: 'UserStudent_UserFK',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'user_to_student',
+      new TableForeignKey({
+        columnNames: ['studentId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'student',
+        onDelete: 'CASCADE',
+        name: 'UserStudent_StudentFK',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'student',
+      new TableForeignKey({
+        columnNames: ['classroomId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'classroom',
+        onDelete: 'CASCADE',
+        name: 'StudentClassroomFK',
       }),
     );
   }
 
   async down(queryRunner: QueryRunner): Promise<void> {
-    const table = await queryRunner.getTable('answer');
-    const foreignKey = table.foreignKeys.find((fk) => fk.columnNames.indexOf('questionId') !== -1);
-    await queryRunner.dropForeignKey('answer', foreignKey);
+    await dropForeignKeyIfExists(queryRunner, 'classroom', 'userId');
+    await dropForeignKeyIfExists(queryRunner, 'user_to_student', 'studentId'); // studentId
+    await dropForeignKeyIfExists(queryRunner, 'user_to_student', 'userId'); // userId
+    await dropForeignKeyIfExists(queryRunner, 'student', 'UserStudent_UserFK'); // classroomId
+
+    await queryRunner.createForeignKey(
+      'classroom',
+      new TableForeignKey({
+        columnNames: ['userId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'user',
+        name: 'FK_6f2187911a8faba7c91a83194d9',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'user_to_student',
+      new TableForeignKey({
+        columnNames: ['studentId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'student',
+        name: 'FK_39299bddd62684b6bf55a7a8aec',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'user_to_student',
+      new TableForeignKey({
+        columnNames: ['userId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'user',
+        name: 'FK_c4a67eb239df56e50679e238eec',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'student',
+      new TableForeignKey({
+        columnNames: ['classroomId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'classroom',
+        name: 'FK_426224f5597213259b1d58fc0f4',
+      }),
+    );
   }
 }
