2facb542a735ee630519937c9a7e6529f77ef7cd
Yan Labarthe <yan.labarthe@gmail.com>
2023-03-15 17:33:58 +0100

    css

--- src/pages/familles/1.tsx
+++ src/pages/familles/1.tsx
@@ -117,7 +117,7 @@ const ClassroomParamStep1Visibility = () => {
     }, {}),
     pelico: true,
   });
-  
+
   const { activities, refetch, isLoading } = useActivities({
     limit: 300,
     page: 0,
@@ -185,129 +185,133 @@ const ClassroomParamStep1Visibility = () => {
 
   return (
     <Base>
-      <BackButton href="/" />
-      <Steps
-        steps={['Visibilité', 'Identifiants', 'Communication', 'Gestion']}
-        urls={['/familles/1?edit', '/familles/2', '/familles/3', '/familles/4']}
-        activeStep={0}
-      />
+      <div style={{ width: '100%', padding: '0.5rem 1rem 1rem 1rem' }}>
+        <BackButton href="/" />
+        <Steps
+          steps={['Visibilité', 'Identifiants', 'Communication', 'Gestion']}
+          urls={['/familles/1?edit', '/familles/2', '/familles/3', '/familles/4']}
+          activeStep={0}
+        />
 
-      {/* Main  */}
-      <div className="width-900">
-        <h1>Choisissez ce que voient les familles</h1>
-        <p className="text">
-          Vous allez inviter les familles de vos élèves à se connecter à 1Village. Ainsi, elles pourront observer les échanges qui ont lieu en ligne.
-          Vous avez la possibilité de définir ce que les familles voient sur la plateforme. Choisissez parmi ces 4 options :
-        </p>
-        <FormControl>
-          <RadioGroup aria-label="visibility" onChange={handleRadioSelect} value={radioValue}>
-            <FormControlLabel
-              value="default"
-              name="default"
-              control={<Radio />}
-              label="les familles peuvent voir toutes les activités publiées sur 1Village, dès leur publication"
-              onFocus={() => handleSelectionVisibility('default')}
-            />
-            <FormControlLabel
-              value="timeDelay"
-              name="timeDelay"
-              control={<Radio />}
-              label={
-                <TextnInputContainer
-                  {...content1}
-                  onChange={(event) => handleDaysDelay('timeDelay', event)}
-                  onBlur={() => handleSelectionVisibility('timeDelay')}
-                  value={state.timeDelay.delayedDays}
-                  disabled={isDisabled.timeDelay}
-                />
-              }
-              onClick={() => toggleInput('timeDelay', false)}
-              disabled={isDisabled.timeDelay}
-            />
-            <FormControlLabel
-              value="ownClass"
-              name="ownClass"
-              control={<Radio />}
-              label="les familles peuvent voir toutes les activités publiées sur 1Village, dès leur publication, mais seulement celles publiées par notre classe"
-              onFocus={() => handleSelectionVisibility('ownClass')}
-            />
-            <FormControlLabel
-              value="ownClassTimeDelay"
-              name="ownClassTimeDelay"
-              control={<Radio />}
-              label={
-                <TextnInputContainer
-                  {...content2}
-                  onChange={(event) => handleDaysDelay('ownClassTimeDelay', event)}
-                  onBlur={() => handleSelectionVisibility('ownClassTimeDelay')}
-                  value={state.ownClassTimeDelay.delayedDays}
-                  disabled={isDisabled.ownClassTimeDelay}
-                />
-              }
-              onClick={() => toggleInput('ownClassTimeDelay', false)}
-              disabled={isDisabled.ownClassTimeDelay}
-            />
-          </RadioGroup>
-        </FormControl>
-        <div style={{ margin: '-1rem 0' }}>
-          <StepsButton next={onNext} />
+        {/* Main  */}
+        <div className="width-900">
+          <h1>Choisissez ce que voient les familles</h1>
+          <p className="text">
+            Vous allez inviter les familles de vos élèves à se connecter à 1Village. Ainsi, elles pourront observer les échanges qui ont lieu en
+            ligne. Vous avez la possibilité de définir ce que les familles voient sur la plateforme. Choisissez parmi ces 4 options :
+          </p>
+          <FormControl>
+            <RadioGroup aria-label="visibility" onChange={handleRadioSelect} value={radioValue}>
+              <FormControlLabel
+                value="default"
+                name="default"
+                control={<Radio />}
+                label="les familles peuvent voir toutes les activités publiées sur 1Village, dès leur publication"
+                onFocus={() => handleSelectionVisibility('default')}
+              />
+              <FormControlLabel
+                value="timeDelay"
+                name="timeDelay"
+                control={<Radio />}
+                label={
+                  <TextnInputContainer
+                    {...content1}
+                    onChange={(event) => handleDaysDelay('timeDelay', event)}
+                    onBlur={() => handleSelectionVisibility('timeDelay')}
+                    value={state.timeDelay.delayedDays}
+                    disabled={isDisabled.timeDelay}
+                  />
+                }
+                onClick={() => toggleInput('timeDelay', false)}
+                disabled={isDisabled.timeDelay}
+              />
+              <FormControlLabel
+                value="ownClass"
+                name="ownClass"
+                control={<Radio />}
+                label="les familles peuvent voir toutes les activités publiées sur 1Village, dès leur publication, mais seulement celles publiées par notre classe"
+                onFocus={() => handleSelectionVisibility('ownClass')}
+              />
+              <FormControlLabel
+                value="ownClassTimeDelay"
+                name="ownClassTimeDelay"
+                control={<Radio />}
+                label={
+                  <TextnInputContainer
+                    {...content2}
+                    onChange={(event) => handleDaysDelay('ownClassTimeDelay', event)}
+                    onBlur={() => handleSelectionVisibility('ownClassTimeDelay')}
+                    value={state.ownClassTimeDelay.delayedDays}
+                    disabled={isDisabled.ownClassTimeDelay}
+                  />
+                }
+                onClick={() => toggleInput('ownClassTimeDelay', false)}
+                disabled={isDisabled.ownClassTimeDelay}
+              />
+            </RadioGroup>
+          </FormControl>
+          <div style={{ margin: '-1rem 0' }}>
+            <StepsButton next={onNext} />
+          </div>
+
+          {/* Activity Container */}
+          <p className="text">
+            Indépendamment de ce réglage, vous pouvez réglez individuellement la visibilité des activités déjà publiées en ligne.
+          </p>
+          {/* phase is set to 4 to match the array with ALL activities */}
+          <Filters countries={filterCountries} filters={filters} onChange={setFilters} phase={4} isMesFamilles={true} />
+          <OverflowContainer
+            style={{
+              height: '30vh',
+              overflowY: 'scroll',
+              marginBottom: '3rem',
+              border: '1px solid rgba(76, 62, 217, 0.5)',
+              borderRadius: '3px',
+              padding: '1rem 0 1rem .5rem',
+            }}
+          >
+            {isLoading ? (
+              <div className="loading-spinner">
+                <CircularProgress />
+                <p>Loading activities...</p>
+              </div>
+            ) : (
+              <>
+                {activities.map((activity) => (
+                  <Button
+                    key={activity.id}
+                    sx={{
+                      display: 'flex',
+                      gap: '2rem',
+                      justifyContent: 'space-evenly',
+                      width: '99%',
+                      padding: '0 1rem',
+                      marginBottom: '1rem',
+                      filter: activity.isVisibleToParent ? 'grayscale(0)' : 'grayscale(1)',
+                      backgroundColor: activity.isVisibleToParent ? '' : 'rgba(76, 62, 217, 0.37)',
+                    }}
+                    onClick={() => handleActivityVisibility(activity.id)}
+                  >
+                    {/* UI logic for activity disable */}
+                    {activity.isVisibleToParent ? (
+                      <EyeVisibility style={{ width: '8%', height: 'auto' }} />
+                    ) : (
+                      <EyeClosed style={{ width: '8%', height: 'auto' }} />
+                    )}
+                    <div style={{ width: '100%' }}>
+                      <ActivityCard
+                        activity={activity}
+                        isSelf={user !== null && activity.userId === user.id}
+                        user={userMap[activity.userId] !== undefined ? users[userMap[activity.userId]] : undefined}
+                        noButtons={true}
+                      />
+                    </div>
+                  </Button>
+                ))}
+              </>
+            )}
+          </OverflowContainer>
         </div>
-
-        {/* Activity Container */}
-        <p className="text">Indépendamment de ce réglage, vous pouvez réglez individuellement la visibilité des activités déjà publiées en ligne.</p>
-        {/* phase is set to 4 to match the array with ALL activities */}
-        <Filters countries={filterCountries} filters={filters} onChange={setFilters} phase={4} isMesFamilles={true} />
-        <OverflowContainer
-          style={{
-            height: '30vh',
-            overflowY: 'scroll',
-            marginBottom: '3rem',
-            border: '1px solid rgba(76, 62, 217, 0.5)',
-            borderRadius: '3px',
-            padding: '1rem 0 1rem .5rem',
-          }}
-        >
-          {isLoading ? (
-            <div className="loading-spinner">
-              <CircularProgress />
-              <p>Loading activities...</p>
-            </div>
-          ) : (
-            <>
-              {activities.map((activity) => (
-                <Button
-                  key={activity.id}
-                  sx={{
-                    display: 'flex',
-                    gap: '2rem',
-                    justifyContent: 'space-evenly',
-                    width: '99%',
-                    padding: '0 1rem',
-                    marginBottom: '1rem',
-                    filter: activity.isVisibleToParent ? 'grayscale(0)' : 'grayscale(1)',
-                    backgroundColor: activity.isVisibleToParent ? '' : 'rgba(76, 62, 217, 0.37)',
-                  }}
-                  onClick={() => handleActivityVisibility(activity.id)}
-                >
-                  {/* UI logic for activity disable */}
-                  {activity.isVisibleToParent ? (
-                    <EyeVisibility style={{ width: '8%', height: 'auto' }} />
-                  ) : (
-                    <EyeClosed style={{ width: '8%', height: 'auto' }} />
-                  )}
-                  <div style={{ width: '100%' }}>
-                    <ActivityCard
-                      activity={activity}
-                      isSelf={user !== null && activity.userId === user.id}
-                      user={userMap[activity.userId] !== undefined ? users[userMap[activity.userId]] : undefined}
-                      noButtons={true}
-                    />
-                  </div>
-                </Button>
-              ))}
-            </>
-          )}
-        </OverflowContainer>
       </div>
     </Base>
   );
