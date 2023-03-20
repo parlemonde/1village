54547f2e322de9a42a88bccf03ea4067db93a1e3
charlinedev <charline.barbelette@sciam.fr>
2023-03-20 18:17:07 +0100

    remove unused useContext

--- src/components/activities/ActivityComments/index.tsx
+++ src/components/activities/ActivityComments/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import { ActivityCard } from '../ActivityCard';
 import StoriesDataCardView from '../ActivityView/StoriesDataCardView';
