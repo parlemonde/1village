a90460b21afa8e2d4c1d672f8a3452c10833d14e
charlinedev <charline.barbelette@sciam.fr>
2023-03-23 19:20:27 +0100

    change text

--- src/pages/creer-une-histoire/2.tsx
+++ src/pages/creer-une-histoire/2.tsx
@@ -74,7 +74,7 @@ const StoryStep2 = () => {
         <div className="width-900">
           <h1>Inventez et dessinez un objet magique</h1>
           <p className="text">
-            Cet objet, tout comme le lieu que vous choisirez à l’étape suivante, est magique ! Grâce à leurs pouvoirs, le village idéal a atteint
+            Cet objet, tout comme le lieu que vous choisirez à l’étape suivante, est magique ! Grâce à leurs pouvoirs, le village idéal va atteindre
             l’objectif du développement durable que vous avez choisi à l’étape précédente.
           </p>
           <Grid container spacing={3}>
--- src/pages/creer-une-histoire/3.tsx
+++ src/pages/creer-une-histoire/3.tsx
@@ -89,8 +89,8 @@ const StoryStep3 = () => {
         <div className="width-900">
           <h1>Inventez et dessinez un lieu extraordinaire</h1>
           <p className="text">
-            Ce lieu, tout comme l’objet que vous avez choisi à l’étape précédente, est extraodinaire ! Grâce à leurs pouvoirs, le village idéal a
-            atteint l’objectif du développement durable que vous choisirez en étape 3.
+            Ce lieu, tout comme l’objet que vous avez choisi à l’étape précédente, est extraodinaire ! Grâce à leurs pouvoirs, le village idéal va
+            atteindre l’objectif de développement durable que vous avez choisi.
           </p>
           <Grid container spacing={3}>
             <Grid item xs={12} md={6}>
