c0d0bdbb4cc718a9fc9b037a5b80d26a961fe098
charlinedev <charline.barbelette@sciam.fr>
2023-03-23 09:14:27 +0100

    fix bug audioLabels

--- src/pages/parametrer-hymne/1.tsx
+++ src/pages/parametrer-hymne/1.tsx
@@ -92,7 +92,7 @@ const AnthemStep1 = () => {
                 <React.Fragment key={idx}>
                   {idx === 1 && <div style={{ margin: '25px 0 25px' }}>Les différentes pistes sonores du couplet (utiles au mixage)</div>}
                   <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                    {React.createElement(musicIcons[idx], { key: `descimg--${idx}` })}
+                    <span title={audio.label}>{React.createElement(musicIcons[idx], { key: `descimg--${idx}` })}</span>
                     <div style={{ width: '200px', marginLeft: '10px' }} title={audio.label}>
                       {audio.label} :{' '}
                     </div>
@@ -101,7 +101,6 @@ const AnthemStep1 = () => {
                         <AnthemEditor
                           key={`anthem-edit--${idx}`}
                           value={audio.value}
-                          title={audio.label}
                           onChange={onUpdateVerseAudios(idx)}
                           onDelete={() => {
                             onUpdateVerseAudios(idx)('');
