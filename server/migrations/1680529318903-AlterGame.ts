c4f5a26884aea58d62ca2237e8a2a23f64d6f257
MarineChillaud <marine.marez@gmail.com>
2023-04-05 18:21:41 +0200

    feat: migration: alter table game

--- server/migrations/1680529318903-AlterGame.ts
+++ server/migrations/1680529318903-AlterGame.ts
@@ -1,29 +1,51 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AlterGame1680529318903 implements MigrationInterface {
+export class GameAddJsonColumns1622752906572 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-      UPDATE game
-      SET 
-        fakeSignification1 = JSON_VALUE(content, '$.fakeSignification1'),
-        fakeSignification2 = JSON_VALUE(content, '$.fakeSignification2'),
-        origine = JSON_VALUE(content, '$.origine'),
-        signification = JSON_VALUE(content, '$.signification'),
-        video = JSON_VALUE(content, '$.video')
-      WHERE id IN (1, 2, 3, 4, 5)
-    `);
+    // add 5 columns
+    await queryRunner.query(`ALTER TABLE game ADD fakeSignification1 TEXT`);
+    await queryRunner.query(`ALTER TABLE game ADD fakeSignification2 TEXT`);
+    await queryRunner.query(`ALTER TABLE game ADD origine TEXT`);
+    await queryRunner.query(`ALTER TABLE game ADD signification TEXT`);
+    await queryRunner.query(`ALTER TABLE game ADD video TEXT`);
+
+    // Retrieve all games with value in column "content" and update the 5 colums
+    const games = await queryRunner.query(`SELECT * FROM game WHERE content IS NOT NULL`);
+    for (const game of games) {
+      const { fakeSignification1, fakeSignification2, origine, signification, video } = JSON.parse(game.content);
+      await queryRunner.query(`UPDATE game SET fakeSignification1=?, fakeSignification2=?, origine=?, signification=?, video=? WHERE id=?`, [
+        fakeSignification1,
+        fakeSignification2,
+        origine,
+        signification,
+        video,
+        game.id,
+      ]);
+    }
+
+    // Delete column "content"
+    await queryRunner.query(`ALTER TABLE game DROP COLUMN content`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await Promise.all([
-      queryRunner.query(
-        "UPDATE game SET content = JSON_SET(content, '$.fakeSignification1', fakeSignification1, '$.fakeSignification2', fakeSignification2, '$.origine', origine, '$.signification', signification, '$.video', video) WHERE id IN (1, 2, 3, 4, 5)",
-      ),
-      queryRunner.dropColumn('game', 'fakeSignification1'),
-      queryRunner.dropColumn('game', 'fakeSignification2'),
-      queryRunner.dropColumn('game', 'origine'),
-      queryRunner.dropColumn('game', 'signification'),
-      queryRunner.dropColumn('game', 'video'),
-    ]);
+    // Add column "content"
+    await queryRunner.query(`ALTER TABLE game ADD content jsonb`);
+
+    // Retrieve all games with value for 5 columns and update the "content" column
+    const games = await queryRunner.query(
+      `SELECT * FROM game WHERE fakeSignification1 IS NOT NULL OR fakeSignification2 IS NOT NULL OR origine IS NOT NULL OR signification IS NOT NULL OR video IS NOT NULL`,
+    );
+    for (const game of games) {
+      const { fakeSignification1, fakeSignification2, origine, signification, video } = game;
+      const content = JSON.stringify({ fakeSignification1, fakeSignification2, origine, signification, video });
+      await queryRunner.query(`UPDATE game SET content=$1 WHERE id=$2`, [content, game.id]);
+    }
+
+    // Delete 5 columns
+    await queryRunner.query(`ALTER TABLE game DROP COLUMN fakeSignification1`);
+    await queryRunner.query(`ALTER TABLE game DROP COLUMN fakeSignification2`);
+    await queryRunner.query(`ALTER TABLE game DROP COLUMN origine`);
+    await queryRunner.query(`ALTER TABLE game DROP COLUMN signification`);
+    await queryRunner.query(`ALTER TABLE game DROP COLUMN video`);
   }
 }
