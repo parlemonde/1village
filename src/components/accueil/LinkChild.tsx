8831dded7ae1395e329d64a214b0255f8455dc92
charlinedev <charline.barbelette@sciam.fr>
2023-04-19 13:58:03 +0200

    update

--- src/components/accueil/LinkChild.tsx
+++ src/components/accueil/LinkChild.tsx
@@ -4,25 +4,25 @@ import React from 'react';
 
 import { Button, TextField } from '@mui/material';
 
-import { DeleteButton } from '../buttons/DeleteButton';
-import { ClassroomContext } from 'src/contexts/classroomContext';
+// import { DeleteButton } from '../buttons/DeleteButton';
+// import { ClassroomContext } from 'src/contexts/classroomContext';
 import { UserContext } from 'src/contexts/userContext';
-import { bgPage } from 'src/styles/variables.const';
-import type { User } from 'types/user.type';
+// import { bgPage } from 'src/styles/variables.const';
+// import type { User } from 'types/user.type';
+import type { Student } from 'types/student.type';
 
-type UserStudents = User & {
-  firstLetter: string;
-};
+// type UserStudents = User & {
+//   firstLetter: string;
+// };
 
 export const LinkChild = () => {
   const router = useRouter();
   const { linkStudent } = React.useContext(UserContext);
-  const [linkedStudents, setLinkedStudents] = React.useState<UserStudents[]>([]);
+  const [linkedStudents, setLinkedStudents] = React.useState<Student[]>([]);
   const { getLinkedStudentsToUser } = React.useContext(UserContext);
   const { enqueueSnackbar } = useSnackbar();
   const hashedCodeRef = React.useRef<HTMLInputElement>(null);
   // const { students } = React.useContext(ClassroomContext);
-
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -45,17 +45,17 @@ export const LinkChild = () => {
     }, 2 * 1000);
   };
 
-  React.useEffect(() => {
-    const fetchLinkedStudents = async () => {
-      const linkedStudentsData = await getLinkedStudentsToUser(userId);
-      const linkedStudents: UserStudents[] = linkedStudentsData.map((student) => ({
-        ...student,
-        firstLetter: /[0-9]/.test(student.firstname[0]) ? '0-9' : student.firstname[0].toUpperCase(),
-      }));
-      setLinkedStudents(linkedStudents);
-    };
-    fetchLinkedStudents();
-  }, [getLinkedStudentsToUser, userId]);
+  // React.useEffect(() => {
+  //   const fetchLinkedStudents = async () => {
+  //     const linkedStudentsData = await getLinkedStudentsToUser(userId);
+  //     const linkedStudents: UserStudents[] = linkedStudentsData.map((student) => ({
+  //       ...student,
+  //       firstLetter: /[0-9]/.test(student.firstname[0]) ? '0-9' : student.firstname[0].toUpperCase(),
+  //     }));
+  //     setLinkedStudents(linkedStudents);
+  //   };
+  //   fetchLinkedStudents();
+  // }, [getLinkedStudentsToUser, userId]);
 
   return (
     <div style={{ padding: '15px' }}>
@@ -80,13 +80,13 @@ export const LinkChild = () => {
         </Button>
       </form>
       <div className="students-list" style={{ display: 'flex', flexDirection: 'column', width: '72%', minWidth: '350px' }}>
-      linkedStudents
-            .map((student) => (
-        <span key={student.id} style={{ display: 'flex', alignItems: 'center', height: '40px' }}>
-          <p style={{ flex: 1 }}>
-            {student.hashedCode} {student.firstname} {student.lastname}
-          </p>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+        console.log('here I am');
+        {linkedStudents.map((student) => (
+          <span key={student.id} style={{ display: 'flex', alignItems: 'center', height: '40px' }}>
+            <p style={{ flex: 1 }}>
+              {student.hashedCode} {student.firstname} {student.lastname}
+            </p>
+            {/* <div style={{ display: 'flex', alignItems: 'center' }}>
             <DeleteButton
               onDelete={() => {
                 // deleteLinkedStudent(user.id);
@@ -95,10 +95,10 @@ export const LinkChild = () => {
               confirmTitle="Supprimer lien élève"
               style={{ backgroundColor: bgPage, marginLeft: '0.5rem' }}
             />
-          </div>
-        </span>
-        ))
-        .reverse()
+          </div> */}
+          </span>
+        ))}
+        {/* .reverse() */}
       </div>
     </div>
   );
