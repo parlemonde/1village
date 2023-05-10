12fd4f6d513799cc7aa9b3e74170ed04793640fc
Yan Labarthe <yan.labarthe@gmail.com>
2023-05-10 13:46:33 +0200

    bugFix %identifiant can't have additionnal letters after it

--- types/featureFlag.constant.ts
+++ types/featureFlag.constant.ts
@@ -1,3 +1,3 @@
-export const FEATURE_FLAGS_NAMES = ['id-family' as const, 'toto' as const];
+export const FEATURE_FLAGS_NAMES = ['id-family' as const];
 
 export type FeatureFlagsNames = typeof FEATURE_FLAGS_NAMES[number];
