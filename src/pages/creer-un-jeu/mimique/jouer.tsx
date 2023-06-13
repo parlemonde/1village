58d0972195898751168bfaa1d7621c7927660d35
MarineChillaud <marine.marez@gmail.com>
2023-06-13 10:08:18 +0200

    debug: add missing props

--- src/pages/creer-un-jeu/mimique/jouer.tsx
+++ src/pages/creer-un-jeu/mimique/jouer.tsx
@@ -273,7 +273,7 @@ const PlayMimique = () => {
                   {gameCreatorIsPelico ? (
                     <PelicoNeutre style={{ marginLeft: '0.6rem', height: '16px', width: 'auto' }} />
                   ) : (
-                    <Flag country={gameCreator.country.isoCode} size="small" style={{ marginLeft: '0.6rem' }} />
+                    gameCreator && gameCreator.country && <Flag country={gameCreator.country.isoCode} size="small" style={{ marginLeft: '0.6rem' }} />
                   )}
                 </p>
               )}
