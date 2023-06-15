6bae80b6f662f9de81352c3b7740bd33cbe9145d
MarineChillaud <marine.marez@gmail.com>
2023-06-15 15:37:34 +0200

    fix: change migration table game

--- server/migrations/1686660843347-alterGameColumnOrigine.ts
+++ server/migrations/1686660843347-alterGameColumnOrigine.ts
@@ -2,10 +2,10 @@ import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class alterGameColumnOrigine1686660843347 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE game MODIFY origine NOT NULL`);
+    await queryRunner.query(`ALTER TABLE game MODIFY origine text NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE game MODIFY origine NULL`);
+    await queryRunner.query(`ALTER TABLE game MODIFY origine text NOT NULL`);
   }
 }
