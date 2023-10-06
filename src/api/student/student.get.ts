2d60f813a80be198ab909152a999f7db1179f882
charlinedev <charline.barbelette@sciam.fr>
2023-10-06 11:40:03 +0200

    add student put get featureFlag p2

--- src/api/student/student.get.ts
+++ src/api/student/student.get.ts
@@ -27,3 +27,15 @@ export const getClassroomOfStudent = async (id: number) => {
   console.log('classroom query', response.data);
   return response.data;
 };
+
+export const getStudentsAndUsers = async (classroomId: number) => {
+  const response = await axiosRequest({
+    method: 'GET',
+    url: `/students/${classroomId}/get-students-and-users`,
+  });
+
+  if (response.error) {
+    throw response.error;
+  }
+  return response.data;
+};
