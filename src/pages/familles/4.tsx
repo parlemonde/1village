5626a5fd9ff8279b4935b9002916f3d45e907405
charlinedev <79838535+charlinedev@users.noreply.github.com>
2023-10-18 11:36:49 +0200

    Hide page4

--- src/pages/familles/4.tsx
+++ src/pages/familles/4.tsx
@@ -4,7 +4,7 @@ import AccessControl from 'src/components/AccessControl';
 import { Base } from 'src/components/Base';
 import { Steps } from 'src/components/Steps';
 import { StepsButton } from 'src/components/StepsButtons';
-import CollapsibleTable from 'src/components/table/CollapsibleTable';
+import PelicoReflechit from 'src/svg/pelico/pelico_reflechit.svg';
 
 const ClassroomParamStep4 = () => {
   return (
@@ -16,7 +16,14 @@ const ClassroomParamStep4 = () => {
             urls={['/familles/1?edit', '/familles/2', '/familles/3', '/familles/4']}
             activeStep={3}
           />
-          <div className="width-900">
+          <div className="width-900" style={{ marginTop: '20px', marginBottom: '50px' }}>
+            <h1> Vous pourrez bientôt visualiser sur cette page les familles qui ont activé leur compte. </h1>
+            <PelicoReflechit style={{ marginTop: '50px', width: '50%', height: 'auto', maxWidth: '360px' }} />
+            <p className="text"></p>
+          </div>
+          <StepsButton prev="/familles/3" />
+          </div>
+          {/* <div className="width-900">
             <h1> Gérer les identifiants des familles </h1>
             <span>
               Chaque ligne de ce tableau correspond à un élève de votre classe.
@@ -33,7 +40,7 @@ const ClassroomParamStep4 = () => {
             </span>
 
             <CollapsibleTable />
-          </div>
+          </div> */}
           <StepsButton prev="/familles/3" />
         </div>
       </AccessControl>
