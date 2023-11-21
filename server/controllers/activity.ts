b2a89ba8456924ea173d185d4e1d43c378663b97
Melissa Olas <83476211+MelissaOlas@users.noreply.github.com>
2023-11-21 16:43:32 +0100

    showing activities by created dates

--- server/controllers/activity.ts
+++ server/controllers/activity.ts
@@ -147,7 +147,7 @@ const getActivities = async ({
 
   const activities = await subQueryBuilder
     .orderBy('activity.isPinned', 'DESC')
-    .addOrderBy('activity.updateDate', 'DESC')
+    .addOrderBy('activity.createDate', 'DESC')
     .limit(limit)
     .offset(page * limit)
     .getMany();
