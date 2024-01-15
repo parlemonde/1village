9be2fff8e2f9b4a9fb6a54d03ac86adec4a5f46b
Melissa Olas <83476211+MelissaOlas@users.noreply.github.com>
2024-01-15 16:55:23 +0100

    migration file completed

--- server/migrations/1705329596198-PublishDate.ts
+++ server/migrations/1705329596198-PublishDate.ts
@@ -1,7 +1,19 @@
+import { TableColumn } from 'typeorm';
 import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class PublishDate1705329596198 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> { }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'activity',
+      new TableColumn({
+        name: 'publishDate',
+        type: 'date',
+        isNullable: true,
+      }),
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> { }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('activity', 'publishDate');
+  }
 }
