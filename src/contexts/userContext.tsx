5aa1a659951f992ee5e101ec8f09a00ef2682bbe
Simon Nedjari <simon.nedjari@outlook.com>
2024-02-16 16:16:27 +0100

    remove: unused dependencies

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { axiosRequest } from 'src/utils/axiosRequest';
 import type { Student } from 'types/student.type';
