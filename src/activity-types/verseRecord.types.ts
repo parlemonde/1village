f8008b45cef8de68697e0aff21ec894cb0df80f2
Simon Nedjari <simon.nedjari@outlook.com>
2024-02-21 10:16:31 +0100

    modify: VerseRecordData

--- src/activity-types/verseRecord.types.ts
+++ src/activity-types/verseRecord.types.ts
@@ -1,17 +1,12 @@
-import type { Syllable, Track } from 'src/activity-types/anthem.types';
+import type { Syllable } from 'src/activity-types/anthem.types';
 import type { Activity } from 'types/activity.type';
 
 export type VerseRecordData = {
-  tracks: Track[];
+  verseMixUrl: string;
+  verseRecordUrl: string;
+  verseFinalMixUrl: string;
   verseLyrics: Syllable[];
   chorusLyrics: Syllable[];
-  verse: string;
-  customizedMix: string;
-  customizedMixBlob?: Blob;
-  mixWithoutLyrics: string;
-  classRecord: string;
-  slicedRecord: string;
-  customizedMixWithVocals: string;
 };
 
 export type VerseRecordActivity = Activity<VerseRecordData>;
