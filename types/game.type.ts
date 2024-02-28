20e7fff8a2e9a7b5614d883bd272798913902b87
Guillaume <guillaumepages@outlook.com>
2024-02-28 15:34:06 +0100

    changement

--- types/game.type.ts
+++ types/game.type.ts
@@ -1,5 +1,5 @@
-import type { StepsType } from '../src/config/games/game';
 import type { Activity } from './activity.type';
+import type { StepsType } from 'src/config/games/game';
 
 export enum GameType {
   MIMIC = 0,
