4804a00aa4c85ff98adb0981f1d45a637c095a51
Melissa Olas <melissa.olas@gmail.com>
2024-03-07 11:12:50 +0100

    test log pour staging

--- src/components/activities/ActivityCard/index.tsx
+++ src/components/activities/ActivityCard/index.tsx
@@ -121,6 +121,10 @@ export const ActivityCard = ({
           </p>
           <div style={{ display: 'flex', alignItems: 'center' }}>
             <p className="text text--small">Publié le {toDate(activity.publishDate as string)} </p>
+            {
+              // eslint-disable-next-line no-console
+              console.log(activity)
+            }
             {userIsPelico ? (
               <Link href={`/pelico-profil`}>
                 <PelicoNeutre style={{ marginLeft: '0.6rem', height: '16px', width: 'auto', cursor: 'pointer' }} />
