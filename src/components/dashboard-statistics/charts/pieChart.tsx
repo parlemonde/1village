0260278f5449d53a17e871476a7da8448b53648e
Melissa Olas <melissa.olas@gmail.com>
2024-03-22 08:56:17 +0100

    changes on pieCharts using d3

--- src/components/dashboard-statistics/charts/pieChart.tsx
+++ src/components/dashboard-statistics/charts/pieChart.tsx
@@ -1,28 +1,79 @@
-import * as React from 'react';
+import * as d3 from 'd3';
+import React, { useEffect } from 'react';
 
-import { PieChart } from '@mui/x-charts/PieChart/PieChart';
+const data = [
+  { name: '<5', value: 19912018 },
+  { name: '5-9', value: 20501982 },
+  { name: '10-14', value: 20679786 },
+  { name: '15-19', value: 21354481 },
+  { name: '20-24', value: 22604232 },
+  { name: '25-29', value: 21698010 },
+  { name: '30-34', value: 21183639 },
+];
 
-import styles from './../styles/charts.module.css';
+function PieChart(props) {
+  const { data, outerRadius, innerRadius } = props;
 
-export default function PieChartComponent() {
-  const title = 'title';
-  const data = [
-    { id: 0, value: 10, label: 'series A' },
-    { id: 1, value: 15, label: 'series B' },
-    { id: 2, value: 20, label: 'series C' },
-  ];
-  return (
-    <div className={styles.pieChartContainer}>
-      <h1>{title}</h1>
-      <PieChart
-        series={[
-          {
-            data,
-          },
-        ]}
-        width={400}
-        height={200}
-      />
-    </div>
-  );
+  const margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+  };
+
+  const width = 2 * outerRadius + margin.left + margin.right;
+  const height = 2 * outerRadius + margin.top + margin.bottom;
+
+  const colorScale = d3.scaleSequential().interpolator(d3.interpolateCool).domain([0, data.length]);
+
+  useEffect(() => {
+    drawChart();
+  }, [data]);
+
+  function drawChart() {
+    // Remove the old svg
+    d3.select('#pie-container').select('svg').remove();
+
+    // Create new svg
+    const svg = d3
+      .select('#pie-container')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+    const arcGenerator = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+
+    const pieGenerator = d3
+      .pie()
+      .padAngle(0)
+      .value((d) => d.value);
+
+    const arc = svg.selectAll().data(pieGenerator(data)).enter();
+
+    // Append arcs
+    arc
+      .append('path')
+      .attr('d', arcGenerator)
+      .style('fill', (_, i) => colorScale(i))
+      .style('stroke', '#ffffff')
+      .style('stroke-width', 0);
+
+    // Append text labels
+    arc
+      .append('text')
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle')
+      .text((d) => d.data.label)
+      .style('fill', (_, i) => colorScale(data.length - i))
+      .attr('transform', (d) => {
+        const [x, y] = arcGenerator.centroid(d);
+        return `translate(${x}, ${y})`;
+      });
+  }
+
+  return <div id="pie-container" />;
 }
+
+export default PieChart;
