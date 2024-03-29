f45d6d74792bdf4bf764439dfb02b4fe7ce00c6b
Simon Nedjari <simon.nedjari@outlook.com>
2024-03-29 16:28:17 +0100

    add VolumeControls component

---
+++ src/components/audio/VolumeControls/VolumeControl.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react'
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+
+interface VolumeControlProps{
+    icon?: ReactElement;
+    handleVolumeChange: (value: number) => void;
+}
+
+const VolumeControl = ({icon, handleVolumeChange}: VolumeControlProps) => {
+    const [value, setValue] = React.useState<number>(30);
+
+    const handleChange = (_event: Event, value: number | number[]) => {
+        const formatedValue = value as number;
+        setValue(formatedValue);
+        handleVolumeChange(formatedValue)
+      };
+
+    return (
+    <Stack spacing={2} direction="row" sx={{ mb: 1, width: '240px' }} alignItems="center">
+        {icon && icon}
+        <Slider aria-label="Volume" onChange={handleChange} sx={{color: '#666666'}} min={0} max={10}/>
+        <VolumeUp sx={{color: '#666666'}}/>
+    </Stack>
+  )
+}
+
+export default VolumeControl
