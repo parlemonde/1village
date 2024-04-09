03ae671933edabf34a59cdf942f6518f05c3fbf1
Melissa Olas <melissa.olas@gmail.com>
2024-04-09 10:27:54 +0200

    adding charts

--- src/components/dashboard-statistics/charts/BarCharts.tsx
+++ src/components/dashboard-statistics/charts/BarCharts.tsx
@@ -1,8 +1,8 @@
 import type React from 'react';
 
-// import { BarChart } from '@mui/x-charts/BarChart';
+import { BarChart } from '@mui/x-charts/BarChart';
 
-// import styles from '../styles/charts.module.css';
+import styles from '../styles/charts.module.css';
 
 type BarChartDataItem = {
   data: number[];
@@ -19,9 +19,9 @@ interface Props {
 const BarCharts: React.FC<Props> = ({ barChartData }) => {
   const numericData = barChartData.dt.flatMap((item) => item.data);
   return null;
-  // return (
-  //   <BarChart xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]} series={[{ data: numericData }]} width={500} height={300} />
-  // );
+  return (
+    <BarChart xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]} series={[{ data: numericData }]} width={500} height={300} />
+  );
 };
 
 export default BarCharts;
