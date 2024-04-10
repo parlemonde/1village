6712a25bcaba3bbe58376c126b58297cbd2216e9
Simon Nedjari <simon.nedjari@outlook.com>
2024-04-10 16:37:47 +0200

    add test page

---
+++ src/pages/dashboard-test/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Base } from 'src/components/Base';
+
+const DashboardTest = () => {
+  return (
+    <Base>
+      <div>Dashboard Test</div>
+    </Base>
+  );
+};
+
+export default DashboardTest;
