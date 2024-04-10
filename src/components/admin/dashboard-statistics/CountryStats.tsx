147eb3f8f8205a5d37acff66c4e1480e96e38caf
Simon Nedjari <simon.nedjari@outlook.com>
2024-04-10 16:25:45 +0200

    add dashboard-statistics components

---
+++ src/components/admin/dashboard-statistics/CountryStats.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import PieCharts from './charts/PieCharts';
+
+const pieChartData = {
+  data: [
+    { id: 0, value: 10, label: 'series A' },
+    { id: 1, value: 15, label: 'series B' },
+    { id: 2, value: 20, label: 'series C' },
+  ],
+};
+
+const CountryStats = () => {
+  return (
+    <>
+      <h1>Pays</h1>
+      <PieCharts pieChartData={pieChartData} />
+    </>
+  );
+};
+
+export default CountryStats;
