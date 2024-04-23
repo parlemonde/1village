c34f097a0f8d4cb2d1729ae1831d5d23afffece0
Simon Nedjari <simon.nedjari@outlook.com>
2024-04-23 13:43:44 +0200

    add statistics.type.ts

---
+++ types/statistics.type.ts
@@ -0,0 +1,10 @@
+export interface ContributionStats {
+  phase: number;
+  activeClassrooms: string;
+}
+
+export interface StudentAccountsStats {
+  totalStudentAccounts: number;
+  classWithStudentAccounts: number;
+  connectedFamilies: number;
+}
