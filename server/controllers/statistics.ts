93dc5dcf2a4a7e392665ac9805cd7f571fd6fed6
Simon Nedjari <simon.nedjari@outlook.com>
2024-04-30 10:46:03 +0200

    add connection-times endpoint

--- server/controllers/statistics.ts
+++ server/controllers/statistics.ts
@@ -26,7 +26,7 @@ statisticsController.get({ path: '/contributions' }, async (_req, res) => {
   );
 });
 
-statisticsController.get({ path: '/classes-exchanges' }, async (_req, res) => {
+statisticsController.get({ path: '/classroom-exchanges' }, async (_req, res) => {
   const activitiesCount = await activityRepository.count({ where: { user: { type: UserType.TEACHER } } });
   const commentsCount = await commentRepository.count({ where: { user: { type: UserType.TEACHER } } });
   const videosCount = await AppDataSource.createQueryRunner().manager.query(
@@ -54,13 +54,27 @@ statisticsController.get({ path: '/student-accounts' }, async (_req, res) => {
   );
 });
 
-statisticsController.get({ path: '/connexion-times' }, async (_req, res) => {
-  res.sendJSON(
-    await analyticSessionRepository
-      .createQueryBuilder('analytic_session')
-      .select('MIN(duration) AS minDuration')
-      .addSelect('MAX(duration) AS maxDuration')
-      .addSelect('AVG(duration) AS averageDuration')
-      .getRawOne(),
+//penser à ajouter une condition pour ne sélectionner que les durées supérieur à 60s pour le AVG
+statisticsController.get({ path: '/connection-times' }, async (_req, res) => {
+  const durationThreshold = 60;
+
+  const baseConnectionTimesStats = await analyticSessionRepository
+    .createQueryBuilder('analytic_session')
+    .select('MIN(DISTINCT(duration)) AS minDuration')
+    .addSelect('MAX(DISTINCT(duration)) AS maxDuration')
+    .addSelect('ROUND(AVG(duration), 0) AS averageDuration')
+    .where('duration >= :minDuration', { minDuration: durationThreshold })
+    .getRawOne();
+
+  const medianConnectionTimesStats = await AppDataSource.createQueryRunner().manager.query(
+    `SELECT duration FROM (SELECT duration, ROW_NUMBER() OVER (ORDER BY duration) AS medianIdx FROM (SELECT DISTINCT duration FROM analytic_session WHERE duration IS NOT NULL AND duration >= ?) AS sub) AS d, (SELECT COUNT(DISTINCT duration) AS cnt FROM analytic_session WHERE duration IS NOT NULL AND duration >= ?) AS total_count WHERE d.medianIdx = (total_count.cnt DIV 2);`,
+    [durationThreshold, durationThreshold],
   );
+
+  res.sendJSON({
+    minDuration: baseConnectionTimesStats.minDuration,
+    maxDuration: baseConnectionTimesStats.maxDuration,
+    averageDuration: parseInt(baseConnectionTimesStats.averageDuration),
+    medianDuration: parseInt(medianConnectionTimesStats[0].duration),
+  });
 });
