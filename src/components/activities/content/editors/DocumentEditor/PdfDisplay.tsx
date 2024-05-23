e2e9f2f3da80407b5ee9ab5812455bbd66fb93df
Neo Ryo <marco.sch4064@gmail.com>
2024-05-23 16:28:26 +0200

    fix link display

--- src/components/activities/content/editors/DocumentEditor/PdfDisplay.tsx
+++ src/components/activities/content/editors/DocumentEditor/PdfDisplay.tsx
@@ -5,6 +5,11 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+const options = {
+  cMapUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/cmaps/`,
+};
+
 export default function PdfDisplay({ url }: { url: string }) {
   const [numPages, setNumPages] = useState<number>(1);
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
@@ -12,9 +17,17 @@ export default function PdfDisplay({ url }: { url: string }) {
   }
 
   return (
-    <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+    <Document options={options} file={url} onLoadSuccess={onDocumentLoadSuccess}>
       {Array.from(Array(numPages).keys()).map((v) => (
-        <Page key={v} width={380} pageNumber={v} />
+        <div
+          key={v}
+          style={{
+            marginTop: 10,
+            marginBottom: 10,
+          }}
+        >
+          <Page width={380} pageNumber={v + 1} />
+        </div>
       ))}
     </Document>
   );
