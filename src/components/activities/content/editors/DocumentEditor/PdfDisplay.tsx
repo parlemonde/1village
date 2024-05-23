7b1bf44c51f38baf232ac522d5e1be2fdcae7b39
Neo-Ryo <marco.sch4064@gmail.com>
2024-05-23 11:58:58 +0200

    handle pages with button

--- src/components/activities/content/editors/DocumentEditor/PdfDisplay.tsx
+++ src/components/activities/content/editors/DocumentEditor/PdfDisplay.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import { Button } from '@mui/material';
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 export default function PdfDisplay({ url }: { url: string }) {
+  const [numPages, setNumPages] = useState<number>(1);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+  // function handlePages(){
+  //   if()
+  // }
   return (
-    <Document file={url}>
-      <Page width={380} renderTextLayer={true} renderAnnotationLayer={true} renderMode={'svg'} pageNumber={1} />
+    <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+      <Page width={380} pageNumber={1} />
+      <p>
+        Page {pageNumber} sur {numPages}
+      </p>
+      <Button onClick={() => setPageNumber(pageNumber + 1)}>+</Button>
+      <Button onClick={() => setPageNumber(pageNumber - 1)}>-</Button>
     </Document>
   );
 }
