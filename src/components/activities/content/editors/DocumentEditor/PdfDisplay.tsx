14d8ce7b53f703949f2ed483da3574da09e20bfc
Neo Ryo <marco.sch4064@gmail.com>
2024-05-23 15:00:39 +0200

    clean pdf

--- src/components/activities/content/editors/DocumentEditor/PdfDisplay.tsx
+++ src/components/activities/content/editors/DocumentEditor/PdfDisplay.tsx
@@ -3,26 +3,19 @@ import { Document, Page, pdfjs } from 'react-pdf';
 
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import { Button } from '@mui/material';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 export default function PdfDisplay({ url }: { url: string }) {
   const [numPages, setNumPages] = useState<number>(1);
-  const [pageNumber, setPageNumber] = useState<number>(1);
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
   }
-  // function handlePages(){
-  //   if()
-  // }
+
   return (
     <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-      <Page width={380} pageNumber={1} />
-      <p>
-        Page {pageNumber} sur {numPages}
-      </p>
-      <Button onClick={() => setPageNumber(pageNumber + 1)}>+</Button>
-      <Button onClick={() => setPageNumber(pageNumber - 1)}>-</Button>
+      {Array.from(Array(numPages).keys()).map((v) => (
+        <Page key={v} width={380} pageNumber={v} />
+      ))}
     </Document>
   );
 }
