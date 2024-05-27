316978f499a948a1da9b807a420706e5549a19df
Simon Nedjari <simon.nedjari@outlook.com>
2024-05-27 12:02:24 +0200

    add Stats Card to Global page

--- src/components/admin/dashboard-statistics/GlobalStats.tsx
+++ src/components/admin/dashboard-statistics/GlobalStats.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import StatsCard from './cards/StatsCard';
 
 const GlobalStats = () => {
-  return <h1>1Village</h1>;
+  return (<div>
+    <StatsCard data={15}>Nombre de classes inscrites</StatsCard>
+    <StatsCard data={20}>Nombre de classes connectées</StatsCard>
+    <StatsCard data={10}>Nombre de classes contributrices</StatsCard>
+  </div>)
 };
 
 export default GlobalStats;
