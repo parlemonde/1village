53479f517a7d8bf51c4dff5cab2e3beb2de3b57e
Simon Nedjari <simon.nedjari@outlook.com>
2024-05-27 13:38:46 +0200

    Add icon import

--- src/components/admin/dashboard-statistics/GlobalStats.tsx
+++ src/components/admin/dashboard-statistics/GlobalStats.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
-import StatsCard from './cards/StatsCard/StatsCard';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
 import AverageStatsCard from './cards/AverageStatsCard/AverageStatsCard';
+import StatsCard from './cards/StatsCard/StatsCard';
 
 const GlobalStats = () => {
   return (
-    <div>
-      <StatsCard data={15}>Nombre de classes inscrites</StatsCard>
-      <StatsCard data={20}>Nombre de classes connectées</StatsCard>
-      <StatsCard data={24}>Nombre de classes contributrices</StatsCard>
-    </div>
-    <div>
-      <AverageStatsCard data={{ min: 1, max: 20, average: 15, median: 5 }} unit="min" icon={<AccessTimeIcon sx={{ fontSize: 'inherit' }} />}>
+    <>
+      <div>
+        <StatsCard data={15}>Nombre de classes inscrites</StatsCard>
+        <StatsCard data={20}>Nombre de classes connectées</StatsCard>
+        <StatsCard data={24}>Nombre de classes contributrices</StatsCard>
+      </div>
+      <div>
+        <AverageStatsCard data={{ min: 1, max: 20, average: 15, median: 5 }} unit="min" icon={<AccessTimeIcon sx={{ fontSize: 'inherit' }} />}>
           Temps de connexion moyen par classe
-      </AverageStatsCard>
-    </div>
+        </AverageStatsCard>
+      </div>
+    </>
   );
 };
 
