a60a6867a3ce2d42789ce5c979ac0e66c84179d5
Melissa Olas <melissa.olas@gmail.com>
2024-05-28 15:20:34 +0200

    add chart, modified css on bar charts

--- src/components/admin/dashboard-statistics/charts/BarCharts.tsx
+++ src/components/admin/dashboard-statistics/charts/BarCharts.tsx
@@ -12,19 +12,31 @@ interface Props {
   barChartData: BarChartDataItem[];
 }
 
+//add isSelected to change colors #4339F2 or #DAD7FE
+
 const BarCharts: React.FC<Props> = ({ barChartData }) => {
   const seriesData = barChartData.map((item) => ({ data: item.data }));
   return (
     <div className={styles.barContainer}>
       <div className={styles.title}>Evolution des connexions</div>
-      <BarChart 
-        xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]} 
+      <BarChart
+        xAxis={[
+          {
+            scaleType: 'band',
+            data: ['group A', 'group B', 'group C'],
+            colorMap: {
+              type: 'piecewise',
+              thresholds: [new Date(2021, 1, 1), new Date(2023, 1, 1)],
+              colors: ['#DAD7FE'],
+            },
+          },
+        ]}
         series={seriesData}
         slotProps={{
           bar: {
             clipPath: `inset(0px round 40px)`,
           },
-        }} 
+        }}
       />
     </div>
   );
