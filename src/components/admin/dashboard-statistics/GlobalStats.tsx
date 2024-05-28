257b4c8c27e3a078c6ac66136838cdf17dfff255
Simon Nedjari <simon.nedjari@outlook.com>
2024-05-28 12:18:43 +0200

    fetch data to components

--- src/components/admin/dashboard-statistics/GlobalStats.tsx
+++ src/components/admin/dashboard-statistics/GlobalStats.tsx
@@ -6,13 +6,21 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import AverageStatsCard from './cards/AverageStatsCard/AverageStatsCard';
 import ClassesExchangesCard from './cards/ClassesExchangesCard/ClassesExchangesCard';
 import StatsCard from './cards/StatsCard/StatsCard';
-import { useGetClassroomExchanges } from 'src/api/statistics/statistics.get';
+import { useGetClassroomExchanges, useGetConnectionTimes, useGetConnectionCounts } from 'src/api/statistics/statistics.get';
 
 const GlobalStats = () => {
   const classroomExchanges = useGetClassroomExchanges();
+  const connectionTimes = useGetConnectionTimes();
+  const connectionCounts = useGetConnectionCounts();
 
   if (classroomExchanges.isError) return <p>Error!</p>;
   if (classroomExchanges.isLoading || classroomExchanges.isIdle) return <p>Loading...</p>;
+
+  if (connectionTimes.isError) return <p>Error!</p>;
+  if (connectionTimes.isLoading || connectionTimes.isIdle) return <p>Loading...</p>;
+
+  if (connectionCounts.isError) return <p>Error!</p>;
+  if (connectionCounts.isLoading || connectionCounts.isIdle) return <p>Loading...</p>;
 
   return (
     <>
@@ -22,10 +30,27 @@ const GlobalStats = () => {
         <StatsCard data={24}>Nombre de classes contributrices</StatsCard>
       </div>
       <div>
-        <AverageStatsCard data={{ min: 1, max: 20, average: 15, median: 5 }} unit="min" icon={<AccessTimeIcon sx={{ fontSize: 'inherit' }} />}>
+        <AverageStatsCard
+          data={{
+            min: Math.floor(connectionTimes.data.minDuration / 60),
+            max: Math.floor(connectionTimes.data.maxDuration / 60),
+            average: Math.floor(connectionTimes.data.averageDuration / 60),
+            median: Math.floor(connectionTimes.data.medianDuration / 60),
+          }}
+          unit="min"
+          icon={<AccessTimeIcon sx={{ fontSize: 'inherit' }} />}
+        >
           Temps de connexion moyen par classe
         </AverageStatsCard>
-        <AverageStatsCard data={{ min: 1, max: 20, average: 15, median: 5 }} icon={<VisibilityIcon sx={{ fontSize: 'inherit' }} />}>
+        <AverageStatsCard
+          data={{
+            min: connectionCounts.data.minConnections,
+            max: connectionCounts.data.maxConnections,
+            average: connectionCounts.data.averageConnections,
+            median: connectionCounts.data.medianConnections,
+          }}
+          icon={<VisibilityIcon sx={{ fontSize: 'inherit' }} />}
+        >
           Nombre de connexions moyen par classe
         </AverageStatsCard>
       </div>
