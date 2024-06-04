fe3e8e7fcae219daf616db29a3a14e3744751319
guillaume-pages <guillaumepages@outlook.com>
2024-06-04 14:09:52 +0200

    pagination coté front

--- src/components/admin/mediatheque/CardMediatheque.tsx
+++ src/components/admin/mediatheque/CardMediatheque.tsx
@@ -1,50 +1,42 @@
 import React, { useContext } from 'react';
 
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
 import MediathequeContext from 'src/contexts/mediathequeContext';
 
-export default function MediaCard() {
-  const { filtered } = useContext(MediathequeContext);
+export default function MediaCard({ page }) {
   const { allFiltered } = useContext(MediathequeContext);
-  console.log('filtered');
 
-  console.log(filtered);
-  console.log('allFiltered');
+  console.log('page depuis media', page);
 
-  console.log(allFiltered);
+  const slicedData = allFiltered?.slice(page, page + 6); // Slice data based on current page and page size
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-      {filtered && filtered.length > 0 ? (
-        filtered.map((item, index) => (
-          <Card key={index} sx={{ width: '30%', marginBottom: '20px' }}>
-            {/* Utilisation de la première image du tableau medias */}
-            <CardMedia sx={{ height: 140 }} image={item.medias[0].value} title="Media" />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Media {item.id}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Type: {item.type}, Subtype: {item.subType}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                User ID: {item.userId}, Village ID: {item.villageId}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">En savoir plus</Button>
-            </CardActions>
-          </Card>
-        ))
-      ) : (
-        <p>Aucun média disponible</p>
-      )}
+    <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+        {allFiltered && allFiltered.length > 0
+          ? slicedData.map((item, index) => (
+              <Card key={index} sx={{ width: '30%', marginBottom: '20px' }}>
+                {/* Utilisation de la première image du tableau medias */}
+                <CardMedia sx={{ height: 140 }} image={item.medias[0].value} title="Media" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Media {item.id}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Type: {item.type}, Subtype: {item.subType}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    User ID: {item.userId}, Village ID: {item.villageId}
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))
+          : null}
+      </div>
     </div>
   );
 }
--- src/contexts/mediathequeContext.tsx
+++ src/contexts/mediathequeContext.tsx
@@ -31,23 +31,24 @@ const MediathequeContext: React.Context<MediathequeContextType> = createContext<
 export const MediathequeProvider: FC<MediathequeProviderProps> = ({ children }) => {
   const [filters, setFilters] = useState<Array<Filter[]>>([[]]);
   const [offset, setOffset] = useState<number>(0);
-  const { data } = useGetMediatheque(filters, offset, 6);
-  console.log(data);
-  const { data: allData } = useGetMediatheque(filters);
-  console.log(allData);
 
-  if (!data) {
+  // const { data } = useGetMediatheque(filters, offset, 6);
+  // console.log(data);
+  const { data: allData } = useGetMediatheque(filters);
+  console.log('allData : ', allData);
+
+  if (!allData) {
     return null;
   }
   return (
     <MediathequeContext.Provider
       value={{
         filters,
-        offset: data?.newOffset,
+        // offset: data?.offset,
         setFilters,
-        filtered: data?.activities,
+        // filtered: data?.activities,
         setOffset,
-        count: allData?.activities?.length,
+        // count: allData?.activities?.length,
         allFiltered: allData?.activities,
       }}
     >
