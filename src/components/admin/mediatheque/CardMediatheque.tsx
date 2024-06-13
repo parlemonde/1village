19b2930ea458acc196209758f436a92362d0d58e
Nathan <nathan.75@outlook.com>
2024-06-13 12:19:47 +0200

    Push for staging

--- src/components/admin/mediatheque/CardMediatheque.tsx
+++ src/components/admin/mediatheque/CardMediatheque.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { Grid, makeStyles } from '@mui/material';
+import { Grid } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
