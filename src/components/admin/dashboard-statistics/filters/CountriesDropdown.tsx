23e2750bb664184001a761d51abc0e4600891ad7
Melissa Olas <melissa.olas@gmail.com>
2024-06-26 11:06:49 +0200

    fixing lint errors

--- src/components/admin/dashboard-statistics/filters/CountriesDropdown.tsx
+++ src/components/admin/dashboard-statistics/filters/CountriesDropdown.tsx
@@ -1,10 +1,11 @@
+import * as React from 'react';
+
 import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import type { SelectChangeEvent } from '@mui/material/Select';
 import Select from '@mui/material/Select';
-import * as React from 'react';
 
 export default function CountriesDropdown() {
   const [country, setCountry] = React.useState('');
