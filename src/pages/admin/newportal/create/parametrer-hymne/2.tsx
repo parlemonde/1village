fe6c5d53e0b526e1190f42263e64dd73dd683208
Gaspard Rivoire <36168128+GaspardRivoire@users.noreply.github.com>
2024-07-19 16:14:58 +0200

    Ajout de l'hymne dans le portail admin

---
+++ src/pages/admin/newportal/create/parametrer-hymne/2.tsx
@@ -0,0 +1,171 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import styles from '../../../../../styles/parametrer-hymne.module.css';
+import { postMixAudio } from 'src/api/audio/audio-mix.post';
+import { deleteAudio } from 'src/api/audio/audio.delete';
+import { PageLayout } from 'src/components/PageLayout';
+import { Steps } from 'src/components/Steps';
+import { StepsButton } from 'src/components/StepsButtons';
+import AnthemTrack from 'src/components/activities/anthem/AnthemTrack/AnthemTrack';
+import { InstrumentSvg } from 'src/components/activities/anthem/InstrumentSvg/InstrumentSvg';
+import { getErrorSteps } from 'src/components/activities/anthemChecks';
+import { ActivityContext } from 'src/contexts/activityContext';
+import Vocal from 'src/svg/anthem/vocal.svg';
+import { getLongestVerseSampleDuration } from 'src/utils/audios';
+import instruments from 'src/utils/instruments';
+import { toTime } from 'src/utils/toTime';
+import { ActivityStatus } from 'types/activity.type';
+import { TrackType } from 'types/anthem.type';
+import type { AnthemData, Track } from 'types/anthem.type';
+
+const AnthemStep2 = () => {
+  const router = useRouter();
+
+  const { activity, updateActivity, save } = React.useContext(ActivityContext);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const data = (activity?.data as AnthemData) || null;
+
+  const errorSteps = React.useMemo(() => {
+    if (data !== null) {
+      return getErrorSteps(data, 1);
+    }
+    return [];
+  }, [data]);
+
+  const buildFullMixUrl = async () => {
+    // -- clean previous mix
+    if (data.fullMixUrl && activity?.status === ActivityStatus.DRAFT) {
+      await deleteAudio(data.fullMixUrl).catch(console.error);
+    }
+
+    const verseTracks = data.tracks.filter((t) => t.type !== TrackType.INTRO_CHORUS && t.type !== TrackType.OUTRO && t.sampleUrl !== '');
+    const intro = data.tracks.find((t) => t.type === TrackType.INTRO_CHORUS && t.sampleUrl !== '');
+    const outro = data.tracks.find((t) => t.type === TrackType.OUTRO && t.sampleUrl !== '');
+    const fullTracks = [];
+    if (intro && intro.sampleUrl) {
+      fullTracks.push(intro);
+      fullTracks.push(...verseTracks.map((t) => ({ ...t, sampleStartTime: (t.sampleStartTime || 0) + (intro.sampleDuration || 0) })));
+    } else {
+      fullTracks.push(...verseTracks);
+    }
+    if (outro && outro.sampleUrl) {
+      fullTracks.push({ ...outro, sampleStartTime: Math.max(...fullTracks.map((t) => (t.sampleStartTime || 0) + (t.sampleDuration || 0))) });
+    }
+    const fullMixUrl = fullTracks.length > 0 ? await postMixAudio(fullTracks) : '';
+    updateActivity({ data: { ...data, fullMixUrl } });
+  };
+
+  const onNext = async () => {
+    setIsLoading(true);
+    // [1] Build the full url
+    await buildFullMixUrl();
+
+    // [2] Update the activity
+    save().catch(console.error);
+    setIsLoading(false);
+    router.push('/admin/newportal/create/parametrer-hymne/3');
+  };
+
+  const displayableInstruments = React.useMemo(() => {
+    return instruments.map((instrument) => {
+      return { ...instrument, svg: <InstrumentSvg instrumentName={instrument.value} /> };
+    });
+  }, []);
+
+  const updateTrackInActivity = (updatedTrack: Track) => {
+    const tracks = [...data.tracks].map((track) => (track.type === updatedTrack.type ? updatedTrack : track));
+    updateActivity({ data: { ...data, tracks } });
+  };
+
+  if (!activity || !data) {
+    return <div></div>;
+  }
+
+  const introChorus = data.tracks.find((t) => t.type === TrackType.INTRO_CHORUS);
+  const introChorusDuration = introChorus?.sampleDuration || 0;
+  const outro = data.tracks.find((t) => t.type === TrackType.OUTRO);
+  const outroDuration = outro?.sampleDuration || 0;
+
+  return (
+    <>
+      <PageLayout>
+        <Steps
+          steps={['Mix Couplet', 'Intro Outro', 'Couplet', 'Refrain', 'Prévisualiser']}
+          errorSteps={errorSteps}
+          activeStep={1}
+          urls={[
+            '/admin/newportal/create/parametrer-hymne/1?edit',
+            '/admin/newportal/create/parametrer-hymne/2',
+            '/admin/newportal/create/parametrer-hymne/3',
+            '/admin/newportal/create/parametrer-hymne/4',
+            '/admin/newportal/create/parametrer-hymne/5',
+          ]}
+          onBeforeLeavePage={buildFullMixUrl}
+        />
+        <div className={styles.contentContainer}>
+          <h1>Mettre en ligne les pistes sonores de l&apos;hymne</h1>
+          <div className={styles.anthemStructureContainer}>
+            <p>
+              Pour mémoire voici la structure de l&apos;hymne
+              {introChorusDuration > 0 && outroDuration > 0 && (
+                <b> ({toTime(introChorusDuration + outroDuration + getLongestVerseSampleDuration(data.tracks))})</b>
+              )}
+              :
+            </p>
+            <div className={styles.anthemStructureVocalContainer}>
+              <span>Intro : {<b>{toTime(introChorusDuration)}</b>}</span>
+              <span>Couplet : {<b>{toTime(getLongestVerseSampleDuration(data.tracks))}</b>}</span>
+              <span>Outro : {<b>{toTime(outroDuration)}</b>}</span>
+            </div>
+            <Vocal className={styles.anthemStructureVocal} />
+          </div>
+
+          <div className={styles.trackSelectionContainer}>
+            <p className={styles.trackSelectionTitle}>Mettre en ligne le fichier son de (intro + refrain chanté)</p>
+            <AnthemTrack
+              track={
+                introChorus || {
+                  type: TrackType.INTRO_CHORUS,
+                  label: 'Piste intro + refrain chanté',
+                  sampleUrl: '',
+                  sampleDuration: 0,
+                  iconUrl: 'accordion',
+                  sampleStartTime: 0,
+                  sampleVolume: 0.5,
+                }
+              }
+              handleTrackUpdate={updateTrackInActivity}
+              instruments={displayableInstruments}
+            ></AnthemTrack>
+            <p className={styles.trackSelectionTitle}>Mettre en ligne le fichier son de l&apos;outro</p>
+            <AnthemTrack
+              track={
+                outro || {
+                  type: TrackType.OUTRO,
+                  label: 'Piste outro',
+                  sampleUrl: '',
+                  sampleDuration: 0,
+                  iconUrl: 'accordion',
+                  sampleStartTime: 0,
+                  sampleVolume: 0.5,
+                }
+              }
+              handleTrackUpdate={updateTrackInActivity}
+              instruments={displayableInstruments}
+            ></AnthemTrack>
+          </div>
+        </div>
+      </PageLayout>
+      <StepsButton prev="/admin/newportal/create/parametrer-hymne/1?edit" next={onNext} />
+      <Backdrop className={styles.trackSelectionBackdrop} open={isLoading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
+  );
+};
+
+export default AnthemStep2;
---
+++ src/pages/admin/newportal/create/parametrer-hymne/5.tsx
@@ -0,0 +1,185 @@
+import classNames from 'classnames';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import Backdrop from '@mui/material/Backdrop';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import { isAnthem } from 'src/activity-types/anyActivity';
+import { PageLayout } from 'src/components/PageLayout';
+import { Steps } from 'src/components/Steps';
+import { StepsButton } from 'src/components/StepsButtons';
+import { getErrorSteps } from 'src/components/activities/anthemChecks';
+import { AudioPlayer } from 'src/components/audio/AudioPlayer';
+import { EditButton } from 'src/components/buttons/EditButton';
+import { ActivityContext } from 'src/contexts/activityContext';
+import { VillageContext } from 'src/contexts/villageContext';
+import { useVillageRequests } from 'src/services/useVillages';
+import { ActivityStatus } from 'types/activity.type';
+import type { AnthemData } from 'types/anthem.type';
+
+const AnthemStep5 = () => {
+  const router = useRouter();
+  const { activity, save } = React.useContext(ActivityContext);
+  const { village } = React.useContext(VillageContext);
+  const { editVillage } = useVillageRequests();
+  const [isLoading, setIsLoading] = React.useState(false);
+  const data = (activity?.data as AnthemData) || null;
+
+  const errorSteps = React.useMemo(() => {
+    if (data !== null) {
+      return getErrorSteps(data, 4);
+    }
+    return [];
+  }, [data]);
+
+  const isEdit = activity !== null && activity.id !== 0 && activity.status !== ActivityStatus.DRAFT;
+
+  React.useEffect(() => {
+    if (activity === null && !('activity-id' in router.query) && !sessionStorage.getItem('activity')) {
+      router.push('/admin/newportal/create/parametrer-hymne');
+    } else if (activity && !isAnthem(activity)) {
+      router.push('/admin/newportal/create/parametrer-hymne');
+    }
+  }, [activity, router, village]);
+
+  const onPublish = async () => {
+    setIsLoading(true);
+    const response = await save(true);
+    if (response.success) {
+      if (village !== null) {
+        await editVillage({ id: village.id, anthemId: response.activity.id });
+      }
+      window.location.assign('/admin/newportal/create/parametrer-hymne/success'); // use window location assign to refresh the page and get an updated village.
+    }
+    setIsLoading(false);
+  };
+
+  if (data === null || activity === null || !isAnthem(activity)) {
+    return <div></div>;
+  }
+
+  return (
+    <>
+      <PageLayout>
+        <Steps
+          steps={['Mix Couplet', 'Intro Outro', 'Couplet', 'Refrain', 'Prévisualiser']}
+          errorSteps={errorSteps}
+          activeStep={4}
+          urls={[
+            '/admin/newportal/create/parametrer-hymne/1?edit',
+            '/admin/newportal/create/parametrer-hymne/2',
+            '/admin/newportal/create/parametrer-hymne/3',
+            '/admin/newportal/create/parametrer-hymne/4',
+            '/admin/newportal/create/parametrer-hymne/5',
+          ]}
+        />
+        <div className="width-900">
+          <h1>Pré-visualisez votre paramétrage et activez l&apos;hymne</h1>
+          <p className="text" style={{ fontSize: '1.1rem' }}>
+            Voici la pré-visualisation de votre paramétrage.
+            {isEdit
+              ? " Vous pouvez la modifier à l'étape précédente, et enregistrer vos changements ici."
+              : ' Vous pouvez la modifier, et quand vous êtes prêts : activez ce paramétrage dans ce village-monde !'}
+          </p>
+          {isEdit ? (
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', margin: '1rem 0' }}>
+              <Link href="/admin/newportal/create/parametrer-hymne/4" passHref>
+                <Button component="a" color="secondary" variant="contained" href="/admin/newportal/create/parametrer-hymne/4">
+                  {"Modifier à l'étape précédente"}
+                </Button>
+              </Link>
+              <Button variant="outlined" color="primary" onClick={onPublish}>
+                Enregistrer les changements
+              </Button>
+            </div>
+          ) : (
+            <div style={{ width: '100%', textAlign: 'right', margin: '1rem 0' }}>
+              <Button variant="outlined" color="primary" onClick={onPublish} disabled={errorSteps.length !== 0}>
+                Paramétrer
+              </Button>
+            </div>
+          )}
+
+          <div className={classNames('preview-block', { 'preview-block--warning': errorSteps.includes(0) })}>
+            <EditButton
+              onClick={() => {
+                router.push('/admin/newportal/create/parametrer-hymne/1');
+              }}
+              status={errorSteps.includes(0) ? 'warning' : 'success'}
+              style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+            />
+
+            <AudioPlayer src={data.mixUrl} isBuildingAudio />
+            <p style={{ margin: '0.5rem 0' }}>Écoutez le mix par défaut du couplet (les 7 pistes mélangées)</p>
+          </div>
+
+          <div className={classNames('preview-block', { 'preview-block--warning': errorSteps.includes(1) })}>
+            <EditButton
+              onClick={() => {
+                router.push('/admin/newportal/create/parametrer-hymne/2');
+              }}
+              status={errorSteps.includes(1) ? 'warning' : 'success'}
+              style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+            />
+            <AudioPlayer src={data.fullMixUrl} isBuildingAudio />
+            <p style={{ margin: '0.5rem 0' }}>Écoutez le mix de l&apos;hymne (intro + refrain + couplet mixé + outro)</p>
+          </div>
+
+          <div className={classNames('preview-block', { 'preview-block--warning': errorSteps.includes(2) })}>
+            <EditButton
+              onClick={() => {
+                router.push('/admin/newportal/create/parametrer-hymne/3');
+              }}
+              status={errorSteps.includes(2) ? 'warning' : 'success'}
+              style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+            />
+            <p>Voilà la structure du couplet, découpé en syllabes :</p>
+            <p>
+              {data.verseLyrics.map((syllable, index) =>
+                syllable.back ? (
+                  <React.Fragment key={index}>
+                    <br /> {syllable.value}{' '}
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment key={index}>{syllable.value} </React.Fragment>
+                ),
+              )}
+            </p>
+          </div>
+
+          <div className={classNames('preview-block', { 'preview-block--warning': errorSteps.includes(3) })}>
+            <EditButton
+              onClick={() => {
+                router.push('/admin/newportal/create/parametrer-hymne/4');
+              }}
+              status={errorSteps.includes(3) ? 'warning' : 'success'}
+              style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+            />
+            <p>Voilà le refrain découpé en syllabes :</p>
+            <p>
+              {data.chorusLyrics.map((syllable, index) =>
+                syllable.back ? (
+                  <React.Fragment key={index}>
+                    <br /> {syllable.value}{' '}
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment key={index}>{syllable.value} </React.Fragment>
+                ),
+              )}
+            </p>
+          </div>
+
+          <StepsButton prev="/admin/newportal/create/parametrer-hymne/4" />
+        </div>
+      </PageLayout>
+      <Backdrop style={{ zIndex: 2000, color: 'white' }} open={isLoading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
+  );
+};
+
+export default AnthemStep5;
