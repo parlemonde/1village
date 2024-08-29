62624eb3ae06faa303631dc84be40be226a1c732
David Robert-Ansart <david.robertansart@gmail.com>
2024-08-29 18:03:02 +0200

    Reset database migrations and add initial file

--- server/migrations/1720537502381-AlterAnalyticSessionTable.ts
+++
@@ -1,7 +0,0 @@
-// import type { MigrationInterface, QueryRunner } from 'typeorm';
-
-// export class AlterAnalyticSessionTable1720537502381 implements MigrationInterface {
-//   public async up(queryRunner: QueryRunner): Promise<void> { }
-
-//   public async down(queryRunner: QueryRunner): Promise<void> { }
-// }
