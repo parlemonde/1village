062c66a3f236152454afaffacb67ab8954de80f2
Simon Nedjari <simon.nedjari@outlook.com>
2024-09-03 09:34:25 +0200

    add AnaltyicSession migration

---
+++ server/migrations/1725348767523-AlterAnalyticSessionsByAddPhaseAndUser.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AlterAnalyticSessionsByAddPhaseAndUSer$1725348767523 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
