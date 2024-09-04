577256f6a7d2806c16e672fa84bff95c30d4c4b2
Melissa Olas <melissa.olas@gmail.com>
2024-09-04 15:27:42 +0200

    add Pelico file for no data

--- src/components/admin/dashboard-statistics/pelico-card.tsx
+++ src/components/admin/dashboard-statistics/pelico-card.tsx
@@ -5,12 +5,14 @@ import React from 'react';
 //   message: string;
 // }
 
-export const PelicoCard = (message: { message }) => {
+// add message props en fonction de l'onglet
+export const PelicoCard = () => {
   return (
     <>
-      <div style={{ width: '100%', maxWidth: '20rem', margin: '4rem auto', backgroundColor: bgPage, padding: '1rem', borderRadius: '10px' }}>
+      <div style={{ width: '100%', maxWidth: '20rem', margin: '4rem auto', backgroundColor: 'grey', padding: '1rem', borderRadius: '10px' }}>
         <p style={{ textAlign: 'center' }} className="text">
-          {message}
+          message de Pelico
+          {/* {message} */}
         </p>
         <Image src="/pelico-question.png" alt="image" style={{ width: '60%', height: 'auto', margin: '0 20%' }} />
         <p className="text" style={{ textDecorationLine: 'underline', margin: '0 25%' }}></p>
