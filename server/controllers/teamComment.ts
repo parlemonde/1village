ed755c3e884df113fc02ebb1ac309046ec34f694
Simon Nedjari <simon.nedjari@outlook.com>
2024-09-09 13:06:24 +0200

    add teamComment controller

---
+++ server/controllers/teamComment.ts
@@ -0,0 +1,34 @@
+import type { NextFunction, Request, Response } from 'express';
+
+import { Comment } from '../entities/comment';
+import { TeamComment } from '../entities/teamComment';
+import { UserType } from '../entities/user';
+import { AppDataSource } from '../utils/data-source';
+import { Controller } from './controller';
+
+const commentController = new Controller('/team-comments');
+
+// --- Get all comments. ---
+commentController.get({ path: '', userType: UserType.ADMIN }, async (req: Request, res: Response) => {
+  const teamComments = await AppDataSource.getRepository(TeamComment).find();
+  res.sendJSON(teamComments);
+});
+
+// --- Edit one comment. ---
+commentController.put({ path: '/:commentId', userType: UserType.ADMIN }, async (req: Request, res: Response, next: NextFunction) => {
+  const data = req.body;
+  const id = parseInt(req.params.commentId, 10) ?? 0;
+  const teamComment = await AppDataSource.getRepository(TeamComment).findOne({ where: { id } });
+  if (!teamComment) {
+    next();
+    return;
+  }
+
+  const updatedTeamComment = new TeamComment();
+  updatedTeamComment.id = id;
+  updatedTeamComment.text = data.text;
+  await AppDataSource.getRepository(TeamComment).save(updatedTeamComment);
+  res.sendJSON(updatedTeamComment);
+});
+
+export { commentController };
