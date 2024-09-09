4473428e69b323af65353842cf43ebb884c12682
Simon Nedjari <simon.nedjari@outlook.com>
2024-09-09 11:46:14 +0200

    add teamComment and typeCommentType

---
+++ server/entities/teamComment.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+import type { TeamCommentInterface, TeamCommentType } from '../../types/teamComment.type';
+
+@Entity()
+export class TeamComment implements TeamCommentInterface {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({ type: 'tinyint' })
+  type: TeamCommentType;
+
+  @CreateDateColumn()
+  public createDate: Date;
+
+  @UpdateDateColumn()
+  public updateDate: Date;
+
+  @Column({ type: 'text' })
+  public text: string;
+}
