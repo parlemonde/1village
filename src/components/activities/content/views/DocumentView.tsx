3828a0ebe03110599e993b01a35a012dc9b6d4ea
amineouladakdim <ouladakdim.amine@gmail.com>
2024-09-17 14:31:12 +0200

    Aggrandissement PDF

--- src/components/activities/content/views/DocumentView.tsx
+++ src/components/activities/content/views/DocumentView.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import { LightBox } from 'src/components/lightbox/Lightbox';
 import type { ViewProps } from '../content.types';
 import PdfDisplay from '../editors/DocumentEditor/PdfDisplay';
 
 export const DocumentView = ({ value = '' }: ViewProps) => {
   return (
     <div style={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
-      <PdfDisplay url={value} />
+
+      <LightBox url={value} isImage={false}>
+        <PdfDisplay url={value} />
+      </LightBox>
+
+
     </div>
   );
 };
--- src/components/lightbox/Lightbox.tsx
+++ src/components/lightbox/Lightbox.tsx
@@ -1,16 +1,25 @@
 import Image from 'next/image';
-import * as React from 'react';
-
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 import { Modal } from '../Modal';
 
 type LightBoxProps = {
   url: string;
   children: JSX.Element;
+  isImage: boolean
 };
 
-export const LightBox = ({ url, children }: LightBoxProps) => {
+export const LightBox = ({ url, children, isImage }: LightBoxProps) => {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
 
+  const options = {
+    cMapUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/cmaps/`,
+  };
+
+  const [numPages, setNumPages] = useState<number>(1);
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
   return (
     <>
       <div onClick={() => setIsModalOpen(true)} style={{ cursor: 'pointer' }}>
@@ -27,7 +36,21 @@ export const LightBox = ({ url, children }: LightBoxProps) => {
         ariaLabelledBy="lightBox-modal-title"
         ariaDescribedBy="lightBox-modal-description"
       >
-        <Image layout="responsive" objectFit="contain" width="100%" height="70%" alt="" unoptimized src={url} />
+        {isImage == false && (
+          <Document options={options} file={url} onLoadSuccess={onDocumentLoadSuccess} >
+            {Array.from(Array(numPages).keys()).map((v) => (
+              <div
+                key={v}
+                style={{
+                  marginTop: 10,
+                  marginBottom: 10,
+                }}
+              >
+                <Page width={380} scale={2.0} pageNumber={v + 1} />
+              </div>
+            ))}
+          </Document>)}
+        {isImage == true && (<Image layout="responsive" objectFit="contain" width="100%" height="70%" alt="" unoptimized src={url} />)}
       </Modal>
     </>
   );
