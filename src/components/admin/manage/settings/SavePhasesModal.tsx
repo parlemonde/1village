5c2b63087d7ae67beaf6a4a7f7880fa710a89a47
Benjamin Ramet <ramet.benj@gmail.com>
2024-11-07 14:59:04 +0100

    Add filtering queries and make sure the admin cannot go back to a past phase

--- src/components/admin/manage/settings/SavePhasesModal.tsx
+++ src/components/admin/manage/settings/SavePhasesModal.tsx
@@ -83,10 +83,7 @@ export function SavePhasesModal({ villagesToUpdate, isModalOpen, setIsModalOpen 
       ariaDescribedBy="Modal de validation des phases"
     >
       <div id="brouillon-desc" style={{ padding: '0.5rem' }}>
-        <p>
-          Les modifications que tu souhaites apporter vont modifier les phases actives. <br />
-          Attention, faire passer un village à l&apos;étape suivante est un choix définitif.
-        </p>
+        <p>Les modifications que tu souhaites apporter vont modifier les phases actives.</p>
       </div>
     </Modal>
   );
