fa9db91c77428e4efd29ad31b2f18c1898245a9d
lukas <lambry@wedigital.garden>
2024-11-13 16:10:21 +0100

    feat(VIL-622): adding access rights to archives to admin, s-admin and mediator
    fix: lint
    
    lint
    
    fix: staging build failed
    
    adding mediator account to test on staging
    
    restoring initial state after testing
    
    fixing ts errors
    
    revert changes to signatures

--- server/controllers/archive.ts
+++ server/controllers/archive.ts
@@ -8,15 +8,18 @@ import { Controller } from './controller';
 const archiveController = new Controller('/archives');
 
 // get file
-archiveController.get({ path: '/*', userType: UserType.ADMIN }, async (req: Request, res: Response, next: NextFunction) => {
-  const url = decodeURI(req.url);
-  const key = `archives${url}${url.split('/').length === 2 ? '/index.html' : url.indexOf('.') === -1 ? '.html' : ''}`;
-  try {
-    streamFile(key, req, res, next);
-  } catch {
-    next();
-  }
-});
+archiveController.get(
+  { path: '/*', userType: [UserType.SUPER_ADMIN, UserType.ADMIN, UserType.MEDIATOR] },
+  async (req: Request, res: Response, next: NextFunction) => {
+    const url = decodeURI(req.url);
+    const key = `archives${url}${url.split('/').length === 2 ? '/index.html' : url.indexOf('.') === -1 ? '.html' : ''}`;
+    try {
+      streamFile(key, req, res, next);
+    } catch {
+      next();
+    }
+  },
+);
 
 /**
  * Liste les dossiers dans un préfixe S3 spécifié.
--- src/api/archive/archive.get.ts
+++ src/api/archive/archive.get.ts
@@ -1,12 +1,26 @@
 import axios from 'axios';
+import React from 'react';
 import { useQuery } from 'react-query';
+
+import { UserType } from '../../../types/user.type';
+import { UserContext } from '../../contexts/userContext';
 
 const BASE_URL = '/api/archives';
 
 // Récupérer la liste des années déjà archivées
 export const useListArchives = () => {
-  return useQuery(['archives'], async () => {
-    const { data } = await axios.get(`${BASE_URL}`);
-    return data;
-  });
+  const { user } = React.useContext(UserContext);
+
+  const hasAccess = user?.type === UserType.SUPER_ADMIN || user?.type === UserType.ADMIN || user?.type === UserType.MEDIATOR;
+
+  return useQuery(
+    ['archives'],
+    async () => {
+      const { data } = await axios.get(`${BASE_URL}`);
+      return data;
+    },
+    {
+      enabled: hasAccess,
+    },
+  );
 };
