a316f5076e91eebaede90d84a5d47d4028a3497c
Thibault Guillou <tguillou@laprovence.com>
2024-12-12 16:04:39 +0100

    conflict server

--- server/controllers/statistics.ts
+++ server/controllers/statistics.ts
@@ -2,12 +2,20 @@ import type { Request } from 'express';
 
 import { Classroom } from '../entities/classroom';
 import {
-  getClassroomsInfos,
   getConnectedClassroomsCount,
-  getContributedClassroomsCount,
   getRegisteredClassroomsCount,
+  getChildrenCodesCountForClassroom,
+  getConnectedFamiliesCountForClassroom,
+  getFamiliesWithoutAccountForClassroom,
   normalizeForCountry,
 } from '../stats/classroomStats';
+import {
+  getChildrenCodesCountForGlobal,
+  getConnectedFamiliesCountForGlobal,
+  getFamiliesWithoutAccountForGlobal,
+  getFamilyAccountsCountForGlobal,
+  getFloatingAccountsForGlobal,
+} from '../stats/globalStats';
 import {
   getAverageConnections,
   getAverageDuration,
@@ -19,6 +27,13 @@ import {
   getMinDuration,
   getUserConnectionsList,
 } from '../stats/sessionStats';
+import {
+  getChildrenCodesCountForVillage,
+  getConnectedFamiliesCountForVillage,
+  getFamiliesWithoutAccountForVillage,
+  getFamilyAccountsCountForVillage,
+  getFloatingAccountsForVillage,
+} from '../stats/villageStats';
 import { AppDataSource } from '../utils/data-source';
 import { Controller } from './controller';
 
@@ -64,6 +79,24 @@ statisticsController.get({ path: '/sessions' }, async (req: Request, res) => {
     console.error('Error fetching statistics:', error);
     return res.status(500).json({ message: 'Internal server error' });
   }
+});
+
+statisticsController.get({ path: '/sessions/:phase' }, async (req: Request, res) => {
+  // const phase = req.params.phase ? parseInt(req.params.phase) : null;
+
+  res.sendJSON({
+    minDuration: await getMinDuration(), // TODO - add phase
+    maxDuration: await getMaxDuration(), // TODO - add phase
+    averageDuration: await getAverageDuration(), // TODO - add phase
+    medianDuration: await getMedianDuration(), // TODO - add phase
+    minConnections: await getMinConnections(), // TODO - add phase
+    maxConnections: await getMaxConnections(), // TODO - add phase
+    averageConnections: await getAverageConnections(), // TODO - add phase
+    medianConnections: await getMedianConnections(), // TODO - add phase
+    registeredClassroomsCount: await getRegisteredClassroomsCount(),
+    connectedClassroomsCount: await getConnectedClassroomsCount(), // TODO - add phase
+    // contributedClassroomsCount: await getContributedClassroomsCount(phase),
+  });
 });
 
 statisticsController.get({ path: '/classrooms' }, async (req, res) => {
@@ -180,3 +213,35 @@ statisticsController.get({ path: '/classrooms' }, async (req, res) => {
 //     classrooms: await getClassroomsInfos(),
 //   });
 // });
+
+statisticsController.get({ path: '/onevillage' }, async (_req, res) => {
+  res.sendJSON({
+    familyAccountsCount: await getFamilyAccountsCountForGlobal(),
+    childrenCodesCount: await getChildrenCodesCountForGlobal(),
+    connectedFamiliesCount: await getConnectedFamiliesCountForGlobal(),
+    familiesWithoutAccount: await getFamiliesWithoutAccountForGlobal(),
+    floatingAccounts: await getFloatingAccountsForGlobal(),
+  });
+});
+
+statisticsController.get({ path: '/villages/:villageId' }, async (_req, res) => {
+  const villageId = parseInt(_req.params.villageId);
+  const phase = _req.query.phase as unknown as number;
+  res.sendJSON({
+    familyAccountsCount: await getFamilyAccountsCountForVillage(villageId, phase),
+    childrenCodesCount: await getChildrenCodesCountForVillage(villageId, phase),
+    connectedFamiliesCount: await getConnectedFamiliesCountForVillage(villageId, phase),
+    familiesWithoutAccount: await getFamiliesWithoutAccountForVillage(villageId),
+    floatingAccounts: await getFloatingAccountsForVillage(villageId),
+  });
+});
+
+statisticsController.get({ path: '/classrooms/:classroomId' }, async (_req, res) => {
+  const classroomId = parseInt(_req.params.classroomId);
+  const phase = _req.query.phase as unknown as number;
+  res.sendJSON({
+    childrenCodesCount: await getChildrenCodesCountForClassroom(classroomId, phase),
+    connectedFamiliesCount: await getConnectedFamiliesCountForClassroom(classroomId, phase),
+    familiesWithoutAccount: await getFamiliesWithoutAccountForClassroom(classroomId),
+  });
+});
