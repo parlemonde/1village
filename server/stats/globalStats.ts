1119fcde25c1595d01bcd76d3c5bd41e4d7a1b8e
Thibault Guillou <tguillou@laprovence.com>
2024-12-12 15:47:13 +0100

    conflict server

---
+++ server/stats/globalStats.ts
@@ -0,0 +1,31 @@
+import {
+  generateEmptyFilterParams,
+  getChildrenCodesCount,
+  getConnectedFamiliesCount,
+  getFamiliesWithoutAccount,
+  getFamilyAccountsCount,
+  getFloatingAccounts,
+} from './queryStatsByFilters';
+
+export const getFamiliesWithoutAccountForGlobal = async () => {
+  return getFamiliesWithoutAccount();
+};
+
+export const getConnectedFamiliesCountForGlobal = async () => {
+  const filterParams = generateEmptyFilterParams();
+  return getConnectedFamiliesCount(filterParams);
+};
+
+export const getChildrenCodesCountForGlobal = async () => {
+  const filterParams = generateEmptyFilterParams();
+  return await getChildrenCodesCount(filterParams);
+};
+export const getFloatingAccountsForGlobal = async () => {
+  const filterParams = generateEmptyFilterParams();
+  return await getFloatingAccounts(filterParams);
+};
+
+export const getFamilyAccountsCountForGlobal = async () => {
+  const filterParams = generateEmptyFilterParams();
+  return await getFamilyAccountsCount(filterParams);
+};
