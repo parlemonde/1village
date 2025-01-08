55866fe10adba6ecce284e29ab0e5be53e52e61d
Simon Nedjari <simon.nedjari@outlook.com>
2025-01-08 15:55:14 +0100

    add type and utilitary functions for manage user section

---
+++ src/components/admin/manage/utils/tableHeaders.ts
@@ -0,0 +1,9 @@
+export const ManageUsersHeaders = [
+  { key: 'firstname', label: 'Prénom', sortable: true },
+  { key: 'lastname', label: 'Nom', sortable: true },
+  { key: 'email', label: 'Mail', sortable: true },
+  { key: 'school', label: 'École', sortable: true },
+  { key: 'village', label: 'Village Monde', sortable: true },
+  { key: 'country', label: 'Pays', sortable: true },
+  { key: 'role', label: 'Rôle', sortable: true },
+];
---
+++ types/manage.type.ts
@@ -0,0 +1,20 @@
+export interface UserRow {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  school_name: string;
+  village_name: string;
+  country: string;
+  role: string;
+}
+
+interface UserFilterProps {
+  fullname: string;
+  email: string;
+  villageName: string;
+  countryIsoCode: string;
+  type: string;
+}
+
+export type UserFilter = Partial<UserFilterProps>;
