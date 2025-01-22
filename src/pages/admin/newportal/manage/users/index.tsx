849d040e28217d8872454a34b4d0b439ef3b98b3
Simon Nedjari <simon.nedjari@outlook.com>
2025-01-22 16:08:40 +0100

    clean manage/users

--- src/pages/admin/newportal/manage/users/index.tsx
+++ src/pages/admin/newportal/manage/users/index.tsx
@@ -1,26 +1,16 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import type { SetStateAction } from 'react';
-import React, { useCallback, useMemo, useState } from 'react';
-import { UpdateFilter } from 'typeorm';
+import React, { useMemo, useState } from 'react';
 
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import DeleteIcon from '@mui/icons-material/Delete';
-import DownloadIcon from '@mui/icons-material/Download';
-import EditIcon from '@mui/icons-material/Edit';
-import type { SelectChangeEvent } from '@mui/material';
-import { Box, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+import { Box, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import Chip from '@mui/material/Chip';
-import IconButton from '@mui/material/IconButton';
 import NoSsr from '@mui/material/NoSsr';
-import Tooltip from '@mui/material/Tooltip';
 
 import { useUsers } from 'src/api/user/user.list';
 import { Modal } from 'src/components/Modal';
-import { AdminTile } from 'src/components/admin/AdminTile';
 import { OneVillageTable } from 'src/components/admin/OneVillageTable';
-import { createUsersRows } from 'src/components/admin/manage/utils/tableCreator';
 import { ManageUsersHeaders } from 'src/components/admin/manage/utils/tableHeaders';
 import { UserContext } from 'src/contexts/userContext';
 import { useUserRequests } from 'src/services/useUsers';
@@ -30,16 +20,15 @@ import BackArrow from 'src/svg/back-arrow.svg';
 import { countryToFlag } from 'src/utils';
 import { exportJsonToCsv } from 'src/utils/csv-export';
 import type { UserFilter } from 'types/manage.type';
-import type { OneVillageTableRow } from 'types/statistics.type';
-import { userTypeNames } from 'types/user.type';
+import { UserType, userTypeNames } from 'types/user.type';
 import type { Village } from 'types/village.type';
 
 const Users = () => {
   const router = useRouter();
   const { user } = React.useContext(UserContext);
   const { data, isLoading } = useUsers();
-  const [usersRows, setUsersRows] = React.useState<Array<OneVillageTableRow>>([]);
   const users = React.useMemo(() => data || [], [data]);
+  const [filters, setFilters] = useState<UserFilter>({});
   const { villages } = useVillages();
   const villageMap = villages.reduce<{ [key: number]: Village }>((acc, village) => {
     acc[village.id] = village;
@@ -47,39 +36,25 @@ const Users = () => {
   }, {});
   const { deleteUser } = useUserRequests();
   const [deleteIndex, setDeleteIndex] = React.useState(-1);
-  const [search, setSearch] = useState('');
-  const [userTypeFilter, setUserTypeFilter] = useState('');
-  const [countrySearch, setCountrySearch] = useState('');
-  const [filters, setFilters] = useState<UserFilter>({});
 
-  React.useEffect(() => {
-    if (data) {
-      setUsersRows([]);
-      setUsersRows(createUsersRows(data));
-    }
-  }, [data]);
+  const TABLE_ENTRIES_BY_PAGE = 6;
 
   const filteredUsers = useMemo(
     () =>
       users.filter((u) => {
-        console.log('FLT COUNTRY', filters.country);
-        console.log('USER COUNTRY', u.country?.name);
         if (filters.fullname)
           return [(u.firstname, u.lastname)].some((field) => filters.fullname && field?.toLowerCase().includes(filters.fullname.toLowerCase()));
         if (filters.email) return u.email.toLowerCase().includes(filters.email.toLowerCase());
         if (filters.villageName && u.villageId) return villageMap[u.villageId].name.toLowerCase().includes(filters.villageName.toLowerCase());
         if (filters.country) return u.country?.name.toLowerCase().includes(filters.country.toLowerCase());
-        if (filters.type) return u.type === parseInt(filters.type);
+        if (filters.type || (filters.type && parseInt(filters.type) === 0)) return u.type === parseInt(filters.type);
 
         return true;
       }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [users, filters],
+    [users, filters.type, filters.fullname, filters.email, filters.villageName, filters.country, villageMap],
   );
 
   const tableData = useMemo(() => {
-    // console.log('FILTER', filters);
-    // console.log('FILTERD USERS', filteredUsers);
     return filteredUsers.map((u) => ({
       ...u,
       country: u.country ? `${countryToFlag(u.country?.isoCode)} ${u.country?.name}` : <span style={{ color: 'grey' }}>Non renseignée</span>,
@@ -91,7 +66,7 @@ const Users = () => {
       type: <Chip size="small" label={userTypeNames[u.type]} />,
       position: null,
     }));
-  }, [filteredUsers, filters, villageMap]);
+  }, [filteredUsers, villageMap]);
 
   const handleChange = (updatedFilter: SetStateAction<UserFilter>) => {
     setFilters({ ...filters, ...updatedFilter });
@@ -119,7 +94,7 @@ const Users = () => {
     let userLabel = 'liste-utilisateurs-';
 
     for (const [key, value] of Object.entries(userTypeNames)) {
-      if (key === userTypeFilter) {
+      if (key === filters.type) {
         userLabel = 'liste-' + value.toLowerCase().replaceAll(' ', '-') + 's-';
       }
     }
@@ -131,28 +106,28 @@ const Users = () => {
 
   const actions = (id: number) => (
     <>
-      <Tooltip title="Modifier">
-        <IconButton
-          aria-label="edit"
-          onClick={() => {
-            router.push(`/admin/newportal/manage/users/edit/${id}`);
-          }}
-        >
-          <EditIcon />
-        </IconButton>
-      </Tooltip>
-      {user && user.id !== id && (
-        <Tooltip title="Supprimer">
-          <IconButton
-            aria-label="delete"
-            onClick={() => {
-              setDeleteIndex(users.findIndex((u) => u.id === id));
-            }}
-          >
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      )}
+      <MenuItem
+        aria-label="edit"
+        onClick={() => {
+          router.push(`/admin/newportal/manage/users/edit/${id}`);
+        }}
+      >
+        Modifier
+      </MenuItem>
+      <MenuItem aria-label="analyse" onClick={() => {}} disabled>
+        Analyser
+      </MenuItem>
+      <MenuItem aria-label="unblock" onClick={() => {}} disabled>
+        Débloquer
+      </MenuItem>
+      <MenuItem
+        aria-label="delete"
+        onClick={() => {
+          setDeleteIndex(users.findIndex((u) => u.id === id));
+        }}
+      >
+        Supprimer
+      </MenuItem>
     </>
   );
 
@@ -167,6 +142,8 @@ const Users = () => {
 
       <p>Il y a ici la liste complète des utilisateurs de 1Vilage</p>
 
+      {/* {user?.type === UserType.SUPER_ADMIN ||
+        (user?.type === UserType.ADMIN && ( */}
       <div style={{ display: 'flex', gap: '12px', justifyContent: 'flex-end', margin: '16px 0' }}>
         <Button
           className="like-button blue"
@@ -183,8 +160,19 @@ const Users = () => {
           </Button>
         </Link>
       </div>
+      {/* ))} */}
 
-      <div style={{ display: 'flex', gap: '12px', width: '75%', margin: '16px 0' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '12px',
+          width: '75%',
+          margin: '16px 0',
+          '& > *': {
+            flex: 1,
+          },
+        }}
+      >
         <TextField
           label="Prénom et nom"
           value={filters.fullname}
@@ -201,131 +189,61 @@ const Users = () => {
           size="small"
         />
         <TextField label="Pays" value={filters.country} onChange={(e) => handleChange({ country: e.target.value })} variant="outlined" size="small" />
-        <TextField label="Rôle" value={filters.type} onChange={(e) => handleChange({ type: e.target.value })} variant="outlined" size="small" />
-      </div>
+        <FormControl size="small">
+          <InputLabel id="demo-simple-select-label">Rôle</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            variant="outlined"
+            value={filters.type}
+            onChange={(e) => {
+              handleChange({ type: e.target.value });
+            }}
+            displayEmpty
+          >
+            <MenuItem value={''}>Tous</MenuItem>
+            {[UserType.SUPER_ADMIN, UserType.ADMIN, UserType.MEDIATOR, UserType.TEACHER, UserType.FAMILY, UserType.OBSERVATOR].map((type) => (
+              <MenuItem key={type} value={type}>
+                {userTypeNames[type]}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
 
-      <OneVillageTable admin={true} emptyPlaceholder={undefined} data={tableData} columns={ManageUsersHeaders} actions={actions} />
+      <OneVillageTable
+        admin={true}
+        emptyPlaceholder={undefined}
+        data={tableData}
+        columns={ManageUsersHeaders}
+        actions={actions}
+        usePagination={tableData.length > TABLE_ENTRIES_BY_PAGE}
+        footerElementsLabel="utilisateurs"
+      />
+      <NoSsr>
+        <Modal
+          title="Confirmer la suppression"
+          open={deleteIndex !== -1}
+          onClose={() => {
+            setDeleteIndex(-1);
+          }}
+          onConfirm={async () => {
+            await deleteUser(users[deleteIndex]?.id || -1);
+            setDeleteIndex(-1);
+          }}
+          fullWidth
+          maxWidth="sm"
+          ariaLabelledBy="delete-village-id"
+          ariaDescribedBy="delete-village-desc"
+          error
+        >
+          <div id="delete-village-desc">
+            {"Voulez vous vraiment supprimer l'utilisateur "}
+            <strong>{users[deleteIndex]?.pseudo}</strong> ?
+          </div>
+        </Modal>
+      </NoSsr>
     </div>
-    // <div>
-    //   <Link href="/admin/newportal/manage">
-    //     <div style={{ cursor: 'pointer', display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
-    //       <BackArrow />
-    //       <h1 style={{ marginLeft: '10px' }}>Utilisateurs</h1>
-    //     </div>
-    //   </Link>
-    //   <AdminTile
-    //     title="Liste des utilisateurs"
-    //     toolbarButton={
-    // <Link href="/admin/newportal/manage/users/new" passHref>
-    //   <Button
-    //     color="inherit"
-    //     sx={defaultContainedButtonStyle}
-    //     component="a"
-    //     href="/admin/newportal/manage/users/new"
-    //     variant="contained"
-    //     style={{ flexShrink: 0 }}
-    //     startIcon={<AddCircleIcon />}
-    //   >
-    //     Ajouter un utilisateur
-    //   </Button>
-    // </Link>
-    //     }
-    //   >
-    //     {/* //// */}
-    //     <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', marginBottom: '1rem', marginTop: '1rem', gap: '5px' }}>
-    // <TextField
-    //   label="Rechercher par email ou pseudo"
-    //   value={search}
-    //   onChange={handleChange}
-    //   variant="outlined"
-    //   size="small"
-    //   style={{ marginRight: '1rem' }}
-    // />
-    // <TextField
-    //   label="Rechercher par code pays"
-    //   value={countrySearch}
-    //   onChange={handleCountryChange}
-    //   variant="outlined"
-    //   size="small"
-    //   style={{ marginRight: '1rem' }}
-    // />
-    //       <FormControl variant="outlined" size="small" sx={{ minWidth: '10rem' }}>
-    //         <InputLabel htmlFor="user-type-filter">Filtrer par rôle</InputLabel>
-    //         <Select
-    //           label="Filtrer par rôle"
-    //           value={userTypeFilter}
-    //           onChange={handleSelect}
-    //           inputProps={{
-    //             name: 'user-type-filter',
-    //             id: 'user-type-filter',
-    //           }}
-    //         >
-    //           <MenuItem value="">
-    //             <em>Tous les rôles</em>
-    //           </MenuItem>
-    //           {Object.entries(userTypeNames).map(([value, label]) => (
-    //             <MenuItem key={value} value={value}>
-    //               {label}
-    //             </MenuItem>
-    //           ))}
-    //         </Select>
-    //       </FormControl>
-    // <Button
-    //   className="like-button blue"
-    //   sx={defaultContainedButtonStyle}
-    //   component="a"
-    //   onClick={handleExportToCSV}
-    //   style={{ flexShrink: 0, marginLeft: '1rem' }}
-    //   startIcon={<DownloadIcon />}
-    //   disabled={filteredUsers.length === 0}
-    // >
-    //   Exporter en CSV
-    // </Button>
-    //     </div>
-    //     {/* //// */}
-
-    //     <Box sx={{ overflow: 'auto', border: '1px solid black' }}>
-    //       <OneVillageTable
-    //         admin
-    //         emptyPlaceholder={isLoading ? 'Chargement...' : "Vous n'avez pas encore d'utilisateur !"}
-    //         data={tableData.map((data) => ({ ...data, position: null }))} // remove position attribute from data
-    //         columns={[
-    //           { key: 'firstname', label: 'Prénom', sortable: true },
-    //           { key: 'lastname', label: 'Nom', sortable: true },
-    //           { key: 'email', label: 'Email', sortable: true },
-    //           { key: 'school', label: 'École', sortable: true },
-    //           { key: 'village', label: 'Village', sortable: true },
-    //           { key: 'country', label: 'Pays', sortable: true },
-    //           { key: 'type', label: 'Rôle', sortable: true },
-    //         ]}
-    //         actions={actions}
-    //       />
-    //     </Box>
-    //   </AdminTile>
-    //   <NoSsr>
-    //     <Modal
-    //       title="Confirmer la suppression"
-    //       open={deleteIndex !== -1}
-    //       onClose={() => {
-    //         setDeleteIndex(-1);
-    //       }}
-    //       onConfirm={async () => {
-    //         await deleteUser(users[deleteIndex]?.id || -1);
-    //         setDeleteIndex(-1);
-    //       }}
-    //       fullWidth
-    //       maxWidth="sm"
-    //       ariaLabelledBy="delete-village-id"
-    //       ariaDescribedBy="delete-village-desc"
-    //       error
-    //     >
-    //       <div id="delete-village-desc">
-    //         {"Voulez vous vraiment supprimer l'utilisateur "}
-    //         <strong>{users[deleteIndex]?.pseudo}</strong> ?
-    //       </div>
-    //     </Modal>
-    //   </NoSsr>
-    // </div>
   );
 };
 
