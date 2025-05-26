1dee2d42b735291d482a56e495fddace5ab7daba
keyopto <keyopto@gmail.com>
2025-05-26 16:19:20 +0200

    feat: country list on analyse only displaying contries that have a village

--- server/controllers/countries.ts
+++ server/controllers/countries.ts
@@ -1,4 +1,5 @@
 import { UserType } from '../entities/user';
+import { getCountriesWithVillages } from '../features/countries/countries.service';
 import { countries } from '../utils/iso-3166-countries-french';
 import { Controller } from './controller';
 
@@ -7,6 +8,11 @@ const countryController = new Controller('/countries');
 //--- Get all countries ---
 countryController.get({ path: '', userType: UserType.TEACHER }, async (_req, res) => {
   res.sendJSON(countries);
+});
+
+//--- Get countries that have at least one village ---
+countryController.get({ path: '/present', userType: UserType.ADMIN }, async (_req, res) => {
+  res.sendJSON(await getCountriesWithVillages());
 });
 
 export { countryController };
