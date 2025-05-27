14027b9d4698135d72cb9039f3661111cdfca945
keyopto <151933299+keyopto@users.noreply.github.com>
2025-05-27 11:24:03 +0200

    fix: phase filter reappear on country stats (#1041)

--- src/components/admin/dashboard-statistics/CountryStats.tsx
+++ src/components/admin/dashboard-statistics/CountryStats.tsx
@@ -4,43 +4,46 @@ import VillageListCard from './cards/VillageListCard/VillageListCard';
 import HorizontalBarsChart from './charts/HorizontalChart';
 import DashboardSummary from './dashboard-summary/DashboardSummary';
 import CountriesDropdown from './filters/CountriesDropdown';
+import PhaseDropdown from './filters/PhaseDropdown';
 import { mockDataByMonth } from './mocks/mocks';
 import { PelicoCard } from './pelico-card';
 import styles from './styles/charts.module.css';
+import { useGetCountriesStats } from 'src/api/statistics/statistics.get';
 import { useCountries } from 'src/services/useCountries';
 import { useStatisticsClassrooms, useStatisticsSessions } from 'src/services/useStatistics';
-import type { ClassroomsStats, SessionsStats, VillageStats } from 'types/statistics.type';
-
-const mockStatisticsFamily: VillageStats = {
-  floatingAccounts: [],
-  familiesWithoutAccount: [],
-  childrenCodesCount: 0,
-  connectedFamiliesCount: 0,
-  familyAccountsCount: 0,
-};
+import type { ClassroomsStats, SessionsStats } from 'types/statistics.type';
 
 const CountryStats = () => {
+  const [selectedPhase, setSelectedPhase] = React.useState<number>(0);
   const [selectedCountry, setSelectedCountry] = useState<string>('');
   const pelicoMessage = 'Merci de sélectionner un pays pour analyser ses statistiques ';
   const statisticsClassrooms = useStatisticsClassrooms(null, selectedCountry, null) as ClassroomsStats;
   const statisticsSessions: SessionsStats = useStatisticsSessions(null, selectedCountry, null) as SessionsStats;
+
+  const statisticsFamily = useGetCountriesStats(selectedCountry, selectedPhase);
 
   const { countries } = useCountries();
   const handleCountryChange = (country: string) => {
     setSelectedCountry(country);
   };
 
+  const handlePhaseChange = (phase: string) => {
+    setSelectedPhase(+phase);
+  };
+
   return (
     <>
       <div className={styles.filtersContainer}>
-        <div className={styles.smallFilter}></div>
+        <div className={styles.smallFilter}>
+          <PhaseDropdown onPhaseChange={handlePhaseChange} />
+        </div>
         <div className={styles.medFilter}>
           <CountriesDropdown countries={countries} onCountryChange={handleCountryChange} />
         </div>
         <div className={styles.medFilter} />
         <div className={styles.medFilter} />
       </div>
-      {!selectedCountry ? (
+      {!selectedCountry || !statisticsFamily.data ? (
         <PelicoCard message={pelicoMessage} />
       ) : (
         <>
@@ -48,7 +51,7 @@ const CountryStats = () => {
             <HorizontalBarsChart highlightCountry="FR"></HorizontalBarsChart>
           </div>
           <VillageListCard></VillageListCard>
-          <DashboardSummary data={{ ...statisticsSessions, ...statisticsClassrooms, ...mockStatisticsFamily, barChartData: mockDataByMonth }} />
+          <DashboardSummary data={{ ...statisticsSessions, ...statisticsClassrooms, ...statisticsFamily.data, barChartData: mockDataByMonth }} />
         </>
       )}
     </>
