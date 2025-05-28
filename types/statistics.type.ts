6c2cf71a3b1cb4a6867a3ed60fd90c4e6828acd6
keyopto <keyopto@gmail.com>
2025-05-28 13:05:40 +0200

    feat: add in family global stats the duration stats
    feat: add frontend for connections stats for families
    
    refactor: for durations and connections, put the filters in the same
    object
    
    feat: refactor and add filter with groupType (classroom or family)
    
    refactor: delete DurationStatsFilter and groupType to StatsFilter
    
    fix: fix sql request with countryCode
    
    feat: refactor and link frontend to backend
    
    fix: fix typescript problems due to changes on VillageStats
    
    fix: fix typescript error

--- types/statistics.type.ts
+++ types/statistics.type.ts
@@ -44,11 +44,23 @@ export interface SessionsStats {
 }
 
 export interface VillageStats {
-  childrenCodesCount: number;
-  familyAccountsCount: number;
-  connectedFamiliesCount: number;
-  familiesWithoutAccount: FamiliesWithoutAccount[];
-  floatingAccounts: FloatingAccount[];
+  family: {
+    minDuration: number;
+    maxDuration: number;
+    medianDuration: number;
+    averageDuration: number;
+
+    minConnections: number;
+    maxConnections: number;
+    averageConnections: number;
+    medianConnections: number;
+
+    childrenCodesCount: number;
+    familyAccountsCount: number;
+    connectedFamiliesCount: number;
+    familiesWithoutAccount: FamiliesWithoutAccount[];
+    floatingAccounts: FloatingAccount[];
+  };
   exchanges: Exchanges;
 }
 
@@ -83,13 +95,20 @@ export interface OneVillageTableRow {
 }
 
 export type StatsFilterParams = {
-  villageId: number | undefined;
-  classroomId: number | undefined;
-  countryId: string | undefined;
-  phase: VillagePhase | undefined;
+  villageId?: number;
+  classroomId?: number;
+  countryId?: string;
+  phase?: VillagePhase;
+  groupType?: GroupType;
 };
 
 export type WhereClause = {
   clause: string;
   value: object;
 };
+
+export enum GroupType {
+  CLASSROOM,
+  FAMILY,
+  All,
+}
