b18cc0e63a803866febda798df534fbcb6f7b8b3
Thomas Le Clec'h <thomas.leclech01@gmail.com>
2025-06-11 20:44:45 +0200

    fix:refactoring

--- src/components/admin/OneVillageTable.tsx
+++ src/components/admin/OneVillageTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { isValidElement, useMemo, useState } from 'react';
 
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import { Box, Paper, TableContainer, TableSortLabel, useTheme } from '@mui/material';
@@ -52,9 +52,8 @@ export const OneVillageTable = ({
   minTableHeightInPx = 240,
 }: OneVillageTableProps) => {
   const theme = useTheme();
-  // const color = admin ? 'white' : 'black';
   const backgroundColor = admin ? theme.palette.secondary.main : primaryColorLight;
-  const [options, setTableOptions] = React.useState<TableOptions>({
+  const [options, setTableOptions] = useState<TableOptions>({
     page: 1,
     limit: 10,
     sort: 'asc',
@@ -92,31 +91,27 @@ export const OneVillageTable = ({
   };
 
   const usePagination = usePaginationProp !== undefined ? usePaginationProp : options.page !== undefined && options.limit !== undefined;
-  const displayedData = React.useMemo(() => {
+  const displayedData = useMemo(() => {
     const useSort = options.sort !== undefined && options.order !== undefined;
     const sortedData = useSort
       ? [...data].sort((a, b) => {
-          // Trouver la clé exacte dans l'objet en ignorant la casse
           const exactKey = Object.keys(a).find((key) => key.toLowerCase() === (options.order || '').toLowerCase()) || '';
           let aValue = a[exactKey] || '';
           let bValue = b[exactKey] || '';
 
-          // Gestion des villages
           if (options.order?.toLowerCase() === 'country' || options.order?.toLowerCase() === 'countries') {
             aValue = removeCountryFlagFromText(aValue as string);
             bValue = removeCountryFlagFromText(bValue as string);
           }
 
-          // Gestion des chaînes de caractères
           if (typeof aValue === 'string') aValue = normalizeString(aValue.toLowerCase());
           if (typeof bValue === 'string') bValue = normalizeString(bValue.toLowerCase());
 
-          // Gestion des React nodes
-          if (React.isValidElement(aValue)) {
+          if (isValidElement(aValue)) {
             const key = aValue.key?.toString() || '';
             aValue = key.toLowerCase();
           }
-          if (React.isValidElement(bValue)) {
+          if (isValidElement(bValue)) {
             const key = bValue.key?.toString() || '';
             bValue = key.toLowerCase();
           }
