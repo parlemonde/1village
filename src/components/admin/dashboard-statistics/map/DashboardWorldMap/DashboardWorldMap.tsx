076ff1496b9c79e3eecbaedcd6854e4529a7e9a5
Thomas Le Clec'h <thomas.leclech01@gmail.com>
2025-06-18 16:09:34 +0200

    fix: eslint

--- src/components/admin/dashboard-statistics/map/DashboardWorldMap/DashboardWorldMap.tsx
+++ src/components/admin/dashboard-statistics/map/DashboardWorldMap/DashboardWorldMap.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-
 import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
 
 import CircleIcon from '@mui/icons-material/Circle';
 import TooltipMouseTracker from '../TooltipMouseTracker/TooltipMouseTracker';
 import styles from './DashboardWorldMap.module.css';
+
 import { countryToFlag } from 'src/utils';
 
 type CountryStatus = 'active' | 'observer' | 'ghost' | 'absent';
