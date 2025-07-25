59a27cb53d1f54bf2c42e9eb65f025c00599c4d8
Samuel Marques Antunes <samuel.mantunes@gmail.com>
2025-07-25 15:08:41 +0200

    feat(VIL-747): change links between phases and year dates (#1074)

--- src/components/WelcomeModal/SecondPhase.tsx
+++ src/components/WelcomeModal/SecondPhase.tsx
@@ -1,5 +1,5 @@
 import { useSnackbar } from 'notistack';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 import Button from '@mui/material/Button';
 
@@ -9,11 +9,12 @@ import { VillageContext } from 'src/contexts/villageContext';
 import { axiosRequest } from 'src/utils/axiosRequest';
 
 export const SecondPhase = () => {
-  const { setSelectedPhase } = React.useContext(VillageContext);
-  const { user, setUser } = React.useContext(UserContext);
+  const { setSelectedPhase } = useContext(VillageContext);
+  const { user, setUser } = useContext(UserContext);
   const { enqueueSnackbar } = useSnackbar();
-  const [isModalOpen, setIsModalOpen] = React.useState(true);
-  const urlForm = 'https://docs.google.com/forms/d/e/1FAIpQLSf-BttQrXpk8558Uw9BGaS8sZ1qiR0v2LmA2be_wDLi5y3rLA/viewform';
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const urlForm =
+    'https://docs.google.com/forms/d/e/1FAIpQLSfqJKB8UnFZ14DM1tISHlODWd5qOS2MygGHusfJUo5UjxKdtg/viewform?usp=sharing&ouid=101262587132272655135';
 
   if (!user) {
     return null;
@@ -46,64 +47,59 @@ export const SecondPhase = () => {
         ariaDescribedBy="missing-step-desc"
         ariaLabelledBy="missing-step-title"
         actions={
-          <>
-            <div
-              id="new-user-desc"
-              style={{ minHeight: '15rem', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}
-            >
+          <div id="new-user-desc" style={{ minHeight: '15rem', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+            <div>
+              <p>Avant de passer à la phase suivante, prenez 5 minutes pour nous faire vos retours sur la phase 1 :</p>
+              <a href={urlForm} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: '#4c3ed9' }}>
+                Vos retours sur la phase 1 d&apos;1Village 2025/26
+              </a>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '2rem', marginTop: '2rem' }}>
               <div>
-                <p>Avant de passer à la phase suivante, prenez 5 minutes pour nous faire vos retours sur la phase 1 :</p>
-                <a href={urlForm} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: '#4c3ed9' }}>
-                  Vos retours sur la phase 1 d&apos;1Village 2024/25
-                </a>
+                <p>Si vous n&apos;avez pas encore résolu l&apos;énigme avec votre classe, retournez sur la phase 1.</p>
+                <Button
+                  component="a"
+                  onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+                    event.preventDefault();
+                    setSelectedPhase(1);
+                    setIsModalOpen(false);
+                  }}
+                  href="/"
+                  color="primary"
+                  variant="outlined"
+                  className="navigation__button full-width"
+                  style={{
+                    justifyContent: 'flex-start',
+                    width: 'auto',
+                  }}
+                >
+                  Retourner sur la phase 1
+                </Button>
               </div>
-              <div style={{ display: 'flex', justifyContent: 'center', gap: '2rem', marginTop: '2rem' }}>
-                <div>
-                  <p>Si vous n&apos;avez pas encore résolu l&apos;énigme avec votre classe, retournez sur la phase 1.</p>
-                  <Button
-                    component="a"
-                    onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-                      event.preventDefault();
-                      setSelectedPhase(1);
-                      setIsModalOpen(false);
-                    }}
-                    href={'/'}
-                    color="primary"
-                    variant={'outlined'}
-                    className="navigation__button full-width"
-                    style={{
-                      justifyContent: 'flex-start',
-                      width: 'auto',
-                    }}
-                  >
-                    Retourner sur la phase 1
-                  </Button>
-                </div>
-                <div>
-                  <p>Si vous souhaitez débuter les échanges avec vos pélicopains, poursuivez sur la phase 2.</p>
-                  <Button
-                    component="a"
-                    onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-                      event.preventDefault();
-                      updateUser();
-                      setSelectedPhase(2);
-                      setIsModalOpen(false);
-                    }}
-                    href={'/'}
-                    color="primary"
-                    variant={'outlined'}
-                    className="navigation__button full-width"
-                    style={{
-                      justifyContent: 'flex-start',
-                      width: 'auto',
-                    }}
-                  >
-                    Poursuivre sur la phase 2
-                  </Button>
-                </div>
+              <div>
+                <p>Si vous souhaitez débuter les échanges avec vos pélicopains, poursuivez sur la phase 2.</p>
+                <Button
+                  component="a"
+                  onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+                    event.preventDefault();
+                    updateUser();
+                    setSelectedPhase(2);
+                    setIsModalOpen(false);
+                  }}
+                  href="/"
+                  color="primary"
+                  variant="outlined"
+                  className="navigation__button full-width"
+                  style={{
+                    justifyContent: 'flex-start',
+                    width: 'auto',
+                  }}
+                >
+                  Poursuivre sur la phase 2
+                </Button>
               </div>
             </div>
-          </>
+          </div>
         }
       />
     </div>
