3792f40f98aea90fc07e783b5f5f11ae82c02bed
Carla Faboumy <carla.faboumy@carbon-it.com>
2025-08-11 10:44:09 +0200

    feat(VIL-794): déplace les données mockées de la carte dans GlobalStats et ajoute les loaders

---
+++ types/analytics/village-interactions-activity.ts
@@ -0,0 +1,16 @@
+import type { Country } from '../country.type';
+
+export interface VillageInteractionsActivity {
+  id: number;
+  countries: Country[];
+  totalConnections: number;
+  totalActivities: number;
+  status: VillageInteractionsStatus;
+}
+
+export enum VillageInteractionsStatus {
+  ACTIVE = 'active',
+  OBSERVER = 'observer',
+  GHOST = 'ghost',
+  ABSENT = 'absent',
+}
