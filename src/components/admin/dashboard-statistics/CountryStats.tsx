4f069ad5beaea79e73b484a54f29a39d67fa0aab
Carla Faboumy <carla.faboumy@carbon-it.com>
2025-08-11 11:46:10 +0200

    feat(VIL-797): déplace les données mockées de la carte dans CountryStats et ajoute le loader de la partie haute

--- src/components/admin/dashboard-statistics/CountryStats.tsx
+++ src/components/admin/dashboard-statistics/CountryStats.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Box from '@mui/material/Box';
 
-import { TeamCommentType } from '../../../../types/teamComment.type';
+import LoaderComponent, { AnalyticsDataType } from './Loader';
 import TeamCommentCard from './TeamCommentCard';
 import VillageListCard from './cards/VillageListCard/VillageListCard';
 import HorizontalBarsChart from './charts/HorizontalChart';
@@ -13,29 +13,84 @@ import { PelicoCard } from './pelico-card';
 import styles from './styles/charts.module.css';
 import { useGetCountriesStats } from 'src/api/statistics/statistics.get';
 import { useStatisticsClassrooms, useStatisticsSessions } from 'src/services/useStatistics';
+import type { CountryStat } from 'types/analytics/country-stat';
+import type { VillageListItem } from 'types/analytics/village-list-item';
 import type { ClassroomsStats, SessionsStats } from 'types/statistics.type';
+import { TeamCommentType } from 'types/teamComment.type';
 
 const CountryStats = () => {
   const [selectedPhase, setSelectedPhase] = useState<number>();
   const [selectedCountry, setSelectedCountry] = useState<string>();
-  const pelicoMessage = 'Merci de sélectionner un pays pour analyser ses statistiques ';
+
+  const [highlightedCountry, setHighlightedCountry] = useState<string>('');
+  const [loadingHighlightedCountry, setLoadingHighlightedCountry] = useState<boolean>(true);
+  const [barsChartData, setBarsChartData] = useState<CountryStat[]>([]);
+  const [loadingBarsChartData, setLoadingBarsChartData] = useState<boolean>(true);
+  const [villageList, setVillageList] = useState<VillageListItem[]>([]);
+  const [loadingVillageList, setLoadingVillageList] = useState<boolean>(true);
 
   const statisticsClassrooms = useStatisticsClassrooms(null, selectedCountry, null) as ClassroomsStats;
   const statisticsSessions: SessionsStats = useStatisticsSessions(null, selectedCountry, null, selectedPhase) as SessionsStats;
   const statisticsFamily = useGetCountriesStats(selectedCountry, selectedPhase);
+
+  // On mocke l'asynchronisme en attendant d'avoir l'appel serveur censé retourner les interactions des villages-mondes
+  // A refacto lors de l'implémentation des tickets VIL-407 et VIL-63
+  useEffect(() => {
+    setTimeout(() => {
+      const fakeHighlightedCountry: string = 'FR';
+      const fakeBarsChartData = [
+        { country: 'FR', total: 80 },
+        { country: 'CA', total: 70 },
+        { country: 'PT', total: 60 },
+        { country: 'Grèce', total: 50 },
+        { country: 'Maroc', total: 40 },
+        { country: 'Tunisie', total: 30 },
+        { country: 'Belgique', total: 20 },
+        { country: 'Roumanie', total: 10 },
+      ];
+      const fakeVillageListData = [
+        { name: 'Village France - Canada', color: 'green' },
+        { name: 'Village France - Liban', color: 'orange' },
+        { name: 'Village France - Italie', color: 'gold' },
+        { name: 'Village France - Canada', color: 'yellow' },
+        { name: 'Village France - Liban', color: 'green' },
+        { name: 'Village France - Italie', color: 'limegreen' },
+        { name: 'Village France - Canada', color: 'blue' },
+        { name: 'Village France - Liban', color: 'red' },
+        { name: 'Village France - Italie', color: 'orange' },
+      ];
+
+      setHighlightedCountry(fakeHighlightedCountry);
+      setLoadingHighlightedCountry(false);
+      setBarsChartData(fakeBarsChartData);
+      setLoadingBarsChartData(false);
+      setVillageList(fakeVillageListData);
+      setLoadingVillageList(false);
+    }, 5000);
+  }, []);
 
   return (
     <>
       <TeamCommentCard type={TeamCommentType.COUNTRY} />
       <StatisticFilters onPhaseChange={setSelectedPhase} onCountryChange={setSelectedCountry} />
       {!selectedCountry || !statisticsFamily.data ? (
-        <PelicoCard message={pelicoMessage} />
+        <PelicoCard message={'Merci de sélectionner un pays pour analyser ses statistiques'} />
       ) : (
         <Box mt={2}>
-          <div className={styles.simpleContainer}>
-            <HorizontalBarsChart highlightCountry="FR" />
-          </div>
-          <VillageListCard />
+          {loadingHighlightedCountry || loadingBarsChartData || loadingVillageList ? (
+            <LoaderComponent analyticsDataType={AnalyticsDataType.GRAPHS} />
+          ) : (
+            highlightedCountry &&
+            barsChartData &&
+            villageList && (
+              <>
+                <div className={styles.simpleContainer}>
+                  <HorizontalBarsChart highlightedCountry={highlightedCountry} barsChartData={barsChartData} />
+                </div>
+                <VillageListCard villageList={villageList} />
+              </>
+            )
+          )}
           <DashboardSummary
             data={{ ...statisticsClassrooms, ...statisticsSessions, ...statisticsFamily.data, barChartData: mockDataByMonth }}
             selectedCountry={selectedCountry}
