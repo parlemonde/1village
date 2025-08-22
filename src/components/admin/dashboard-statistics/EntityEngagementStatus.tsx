27fffcd2c7dcaede9e6e1a3daae223895f3457f8
Carla Faboumy <carla.faboumy@carbon-it.com>
2025-08-22 17:09:51 +0200

    feat(VIL-809): ajout du composant statut d'entite (classe, pays ou village)

---
+++ src/components/admin/dashboard-statistics/EntityEngagementStatus.tsx
@@ -0,0 +1,65 @@
+import CircleIcon from '@mui/icons-material/Circle';
+import { Box } from '@mui/material';
+
+import { EngagementStatusColor, EngagementStatus } from '../../../../types/statistics.type';
+
+const entityStatusLabel: Record<EngagementStatus, (entityType: EntityType) => EngagementStatusLabel> = {
+  [EngagementStatus.ABSENT]: (entityType) => (entityType === EntityType.CLASSROOM ? EngagementStatusLabel.ABSENTE : EngagementStatusLabel.ABSENT),
+  [EngagementStatus.ACTIVE]: (entityType) => (entityType === EntityType.CLASSROOM ? EngagementStatusLabel.ACTIVE : EngagementStatusLabel.ACTIF),
+  [EngagementStatus.GHOST]: () => EngagementStatusLabel.FANTOME,
+  [EngagementStatus.OBSERVER]: (entityType) =>
+    entityType === EntityType.CLASSROOM ? EngagementStatusLabel.OBSERVATRICE : EngagementStatusLabel.OBSERVATEUR,
+};
+
+const entityStatusColor: Record<EngagementStatus, EngagementStatusColor> = {
+  [EngagementStatus.ABSENT]: EngagementStatusColor.ABSENT,
+  [EngagementStatus.ACTIVE]: EngagementStatusColor.ACTIVE,
+  [EngagementStatus.GHOST]: EngagementStatusColor.GHOST,
+  [EngagementStatus.OBSERVER]: EngagementStatusColor.OBSERVER,
+};
+
+export enum EntityType {
+  CLASSROOM = 'Classroom',
+  COUNTRY = 'Country',
+  VILLAGE = 'Village',
+}
+
+enum EngagementStatusLabel {
+  ABSENT = 'Absent',
+  ABSENTE = 'Absente',
+  ACTIF = 'Actif',
+  ACTIVE = 'Active',
+  FANTOME = 'Fantôme',
+  OBSERVATEUR = 'Observateur',
+  OBSERVATRICE = 'Observatrice',
+}
+
+interface EntityEngagementStatusData {
+  color: EngagementStatusColor;
+  label: EngagementStatusLabel;
+}
+
+interface EntityEngagementStatusProps {
+  entityType: EntityType;
+  entityEngagementStatus: EngagementStatus;
+}
+
+const EntityEngagementStatus = ({ entityType, entityEngagementStatus }: EntityEngagementStatusProps) => {
+  const engagementStatusData: EntityEngagementStatusData = getEngagementStatusData(entityEngagementStatus, entityType);
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <p style={{ fontWeight: 600, marginLeft: '.75rem', marginRight: '.25rem' }}>Statut : {engagementStatusData.label}</p>
+      <CircleIcon sx={{ color: engagementStatusData.color, verticalAlign: 'middle', height: '.90rem', margin: 'auto 0' }} />
+    </Box>
+  );
+};
+
+export default EntityEngagementStatus;
+
+function getEngagementStatusData(engagementStatus: EngagementStatus, entityType: EntityType): EntityEngagementStatusData {
+  return {
+    color: entityStatusColor[engagementStatus],
+    label: entityStatusLabel[engagementStatus](entityType),
+  };
+}
