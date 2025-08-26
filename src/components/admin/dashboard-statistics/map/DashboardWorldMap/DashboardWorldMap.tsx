d1fc6e7bf903d8f3ed4b215c822592d0c579f5fc
Carla Faboumy <carla.faboumy@carbon-it.com>
2025-08-26 14:22:20 +0200

    feat(VIL-10): connexion du planisphère au backend

--- src/components/admin/dashboard-statistics/map/DashboardWorldMap/DashboardWorldMap.tsx
+++ src/components/admin/dashboard-statistics/map/DashboardWorldMap/DashboardWorldMap.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
 
 import CircleIcon from '@mui/icons-material/Circle';
 
 import TooltipMouseTracker from '../TooltipMouseTracker/TooltipMouseTracker';
 import styles from './DashboardWorldMap.module.css';
-import { EngagementStatusColor } from 'types/statistics.type';
+import type { GeoJSONCountryData } from 'src/components/WorldMap/world/objects/country';
+import type { CountryEngagementStatus } from 'types/statistics.type';
+import { EngagementStatus, EngagementStatusColor } from 'types/statistics.type';
 
-const DashboardWorldMap = () => {
-  const [isTooltipVisible, setIsTooltipVisible] = React.useState(false);
-  const [tooltipData, setTooltipData] = React.useState('');
+interface DashboardWorldMapProps {
+  countriesEngagementStatuses: CountryEngagementStatus[];
+}
+
+const DashboardWorldMap = ({ countriesEngagementStatuses }: DashboardWorldMapProps) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+  const [tooltipData, setTooltipData] = useState<GeoJSONCountryData | null>(null);
 
   return (
     <div className={styles.mapContainer}>
@@ -20,7 +26,7 @@ const DashboardWorldMap = () => {
               geographies.map((geo) => (
                 <Geography
                   onMouseOver={() => {
-                    setTooltipData(geo.properties.name);
+                    setTooltipData(geo);
                     setIsTooltipVisible(true);
                   }}
                   onMouseLeave={() => setIsTooltipVisible(false)}
@@ -28,16 +34,16 @@ const DashboardWorldMap = () => {
                   geography={geo}
                   style={{
                     default: {
-                      fill: '#FFF',
+                      fill: getCountryEngagementColor(countriesEngagementStatuses, geo),
                       stroke: '#000',
                       strokeWidth: '.2',
                       outline: 'none',
                       transition: 'ease .2s',
                     },
                     hover: {
-                      fill: '#edf2fb',
+                      fill: getCountryEngagementColor(countriesEngagementStatuses, geo),
                       stroke: '#000',
-                      strokeWidth: '.2',
+                      strokeWidth: '.9',
                       outline: 'none',
                       cursor: 'pointer',
                     },
@@ -54,27 +60,44 @@ const DashboardWorldMap = () => {
           </Geographies>
         </ZoomableGroup>
       </ComposableMap>
-      <TooltipMouseTracker isVisible={isTooltipVisible}>{tooltipData}</TooltipMouseTracker>
+      {tooltipData && (
+        <TooltipMouseTracker isVisible={isTooltipVisible}>
+          <>
+            {tooltipData?.properties.nameFR}
+            {getCountryEngagementColor(countriesEngagementStatuses, tooltipData) !== '#FFF' && (
+              <CircleIcon
+                fontSize="small"
+                sx={{
+                  color: getCountryEngagementColor(countriesEngagementStatuses, tooltipData),
+                  verticalAlign: 'middle',
+                  marginLeft: '.3rem',
+                  paddingBottom: '.125rem',
+                }}
+              />
+            )}
+          </>
+        </TooltipMouseTracker>
+      )}
       <div className={styles.legendContainer}>
         <h2>Légende:</h2>
         <ul>
           <li>
-            <p style={{ verticalAlign: 'middle' }}>
+            <div style={{ verticalAlign: 'middle', display: 'inline' }}>
               <CircleIcon fontSize="small" sx={{ color: EngagementStatusColor.ACTIVE, verticalAlign: 'middle' }} /> <span>Actif</span>: la majorité
               des classes ont posté ces 3 dernières semaines
-            </p>
+            </div>
           </li>
           <li>
-            <p>
+            <div style={{ verticalAlign: 'middle', display: 'inline' }}>
               <CircleIcon fontSize="small" sx={{ color: EngagementStatusColor.OBSERVER, verticalAlign: 'middle' }} /> <span>Observateur</span>: la
               majorité des classes ont pas posté depuis 3 semaines
-            </p>
+            </div>
           </li>
           <li>
-            <p>
+            <div style={{ verticalAlign: 'middle', display: 'inline' }}>
               <CircleIcon fontSize="small" sx={{ color: EngagementStatusColor.GHOST, verticalAlign: 'middle' }} /> <span>Fantôme</span>: la majorité
               des classes ne se sont pas connectées depuis 3 semaines
-            </p>
+            </div>
           </li>
         </ul>
       </div>
@@ -83,3 +106,14 @@ const DashboardWorldMap = () => {
 };
 
 export default DashboardWorldMap;
+
+function getCountryEngagementColor(countriesData: CountryEngagementStatus[], geography: GeoJSONCountryData): EngagementStatusColor | string {
+  const countryToColor = countriesData.find((country) => country.countryCode === geography.properties.iso2);
+
+  if (!countryToColor) return '#FFF';
+  const countryStatusKey = (Object.keys(EngagementStatus) as Array<keyof typeof EngagementStatus>).find(
+    (key) => EngagementStatus[key] === countryToColor.status,
+  );
+
+  return countryStatusKey ? EngagementStatusColor[countryStatusKey] : '#FFF';
+}
