fa76418802a297475788ec8d0f348d70e698d5fd
Samuel Marques Antunes <samuel.mantunes@gmail.com>
2025-11-06 17:29:53 +0100

    feat(VIL-838): WIP

--- src/components/admin/dashboard-statistics/DataDetailsStats.tsx
+++ src/components/admin/dashboard-statistics/DataDetailsStats.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
 const DataDetailsStats = () => {
-  return <h1>Données</h1>;
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '50vh', gap: 2 }}>
+      <Button variant="contained" color="primary">
+        Télécharger les Données Classe
+      </Button>
+      <Button variant="outlined" color="primary">
+        Télécharger les Données Famille
+      </Button>
+    </Box>
+  );
 };
 
 export default DataDetailsStats;
