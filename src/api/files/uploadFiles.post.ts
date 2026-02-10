1e597cb75e19359cc1e0ef38fad54036e3240cb5
mathieu-vibert <mathieu.vibert@meritis.fr>
2026-02-10 17:53:57 +0100

    fix(VIL-921): Correction de configuration pour l'upload de fichier

--- src/api/files/uploadFiles.post.ts
+++ src/api/files/uploadFiles.post.ts
@@ -8,23 +8,25 @@ async function uploadFiles(params: { files: File[] }): Promise<string[]> {
   for (const file of files) {
     formData.append('files', file);
   }
-  return (
-    await axiosRequest({
-      method: 'POST',
-      baseURL: '/api',
-      url: `/files`,
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      data: formData,
-    })
-  ).data as Promise<string[]>;
+  const result = await axiosRequest({
+    method: 'POST',
+    baseURL: '/api',
+    url: `/files`,
+    data: formData,
+  });
+  if (!Array.isArray(result.data)) {
+    throw new Error('Invalid API response for file import !');
+  }
+  return result.data;
 }
 
 export const useUploadFiles = () => {
   return useMutation({
     mutationFn: (files: File[]) => {
       return uploadFiles({ files });
+    },
+    onError: (error) => {
+      console.error('File upload failed', error);
     },
   });
 };
